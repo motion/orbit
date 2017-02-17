4e650c790b3758938dcb4c86d9d0fe0868029fbb
natew <natewienert@gmail.com>
2017-02-16 21:57:36 -0800

    :new: baseStyles

--- packages/view/src/index.js
+++ packages/view/src/index.js
@@ -1,19 +1,20 @@
 import view from 'motion-view'
 import { StoreHMR } from 'motion-hmr'
 import gloss from 'gloss'
-import reactMixin from 'react-mixin'
+import mixin from 'react-mixin'
 import Helpers from 'motion-class-helpers'
 import React from 'react'
+import baseStyles from './styles'
 
-const styled = gloss()
+const styled = gloss({ baseStyles })
 
 // @view decorator
 export default view(Klass => {
-  // auto-extend React.Component
+  // auto React.Component
   Object.setPrototypeOf(Klass.prototype, React.Component.prototype)
   // mixins
-  reactMixin(Klass.prototype, StoreHMR)
-  reactMixin(Klass.prototype, Helpers)
+  mixin(Klass.prototype, StoreHMR)
+  mixin(Klass.prototype, Helpers)
   // gloss
   return styled(Klass)
 })
---
+++ packages/view/src/styles.js
@@ -0,0 +1,81 @@
+// reference with <tag $$name />
+
+export default {
+  style: styles => styles,
+  flex: flex => ({ flex: flex === true ? 1 : flex }),
+  absolute: ([top,right,bottom,left]) => ({ top, left, bottom, right, position: 'absolute' }),
+  // direct
+  opacity: opacity => ({ opacity }),
+  zIndex: zIndex => ({ zIndex }),
+  color: color => ({ color }),
+  top: top => ({ top }),
+  right: right => ({ right }),
+  bottom: bottom => ({ bottom }),
+  left: left => ({ left }),
+  position: position => ({ position }),
+  padding: padding => ({ padding }),
+  paddingLeft: paddingLeft => ({ paddingLeft }),
+  paddingRight: paddingRight => ({ paddingRight }),
+  paddingTop: paddingTop => ({ paddingTop }),
+  paddingBottom: paddingBottom => ({ paddingBottom }),
+  margin: margin => ({ margin }),
+  marginLeft: marginLeft => ({ marginLeft }),
+  marginRight: marginRight => ({ marginRight }),
+  marginTop: marginTop => ({ marginTop }),
+  marginBottom: marginBottom => ({ marginBottom }),
+  borderLeft: borderLeft => ({ borderLeft }),
+  borderRight: borderRight => ({ borderRight }),
+  borderTop: borderTop => ({ borderTop }),
+  borderBottom: borderBottom => ({ borderBottom }),
+  align: alignItems => ({ alignItems }),
+  alignItems: alignItems => ({ alignItems }),
+  minHeight: minHeight => ({ minHeight }),
+  maxHeight: maxHeight => ({ maxHeight }),
+  minWidth: minWidth => ({ minWidth }),
+  maxWidth: maxWidth => ({ maxWidth }),
+  width: width => ({ width }),
+  height: height => ({ height }),
+  background: background => ({ background }),
+  hide: val => ({ display: val ? 'none' : 'auto' }),
+  alignSelf: alignSelf => ({ alignSelf }),
+  justify: justifyContent => ({ justifyContent }),
+  row: {
+    flexFlow: 'row',
+  },
+  col: {
+    flexFlow: 'column',
+  },
+  centered: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  alignEnd: {
+    alignItems: 'flex-end',
+  },
+  alignStart: {
+    alignItems: 'flex-start',
+  },
+  justifyEnd: {
+    justifyContent: 'flex-end',
+  },
+  justifyStart: {
+    justifyContent: 'flex-start',
+  },
+  spaceBetween: {
+    justifyContent: 'space-between',
+  },
+  fullscreen: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+  ellipse: {
+    display: 'block',
+    whiteSpace: 'pre',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
+}

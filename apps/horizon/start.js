10ad14c468a4738fd5d7605af0883b991a859e0a
natew <natewienert@gmail.com>
2017-02-17 15:35:17 -0800

    :bug: fix backend ports

--- apps/horizon/start.js
+++ apps/horizon/start.js
@@ -3,14 +3,17 @@
 process.argv.push('serve', '--permissions', 'no')
 process.chdir(__dirname)
 
-require('wait-for-port')('my-rethink', 28010, function(error) {
-  if (error) {
-    console.error('Timed out waiting for hz to boot up')
+require('wait-for-port')('my-rethink', 28010,
+  { retryInterval: 1000, numRetries: 20 },
+  function(error) {
+    if (error) {
+      console.error('Timed out waiting for hz to boot up')
+    }
+    else {
+      require('macro-horizon/src/main')
+    }
   }
-  else {
-    require('macro-horizon/src/main')
-  }
-})
+)
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)

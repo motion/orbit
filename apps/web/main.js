015cba890c88bcda3e37b7214dcef2552ec83e8e
natew <natewienert@gmail.com>
2017-02-16 19:58:55 -0800

    :art: use nice new motion-view

--- apps/web/main.js
+++ apps/web/main.js
@@ -6,15 +6,21 @@ class Store {
   x = 1
 }
 
-@view
+@view.inject({
+  test: 10
+})
 export default class Main {
   componentDidMount() {
     // this.addEvent(window, 'mousemove', console.log)
+    console.log(this)
   }
 
   render(a) {
     return (
-      <h1>Hello {this.test}</h1>
+      <h1>
+        Hello2222 {this.test}
+        <Sub />
+      </h1>
     )
   }
 
@@ -22,5 +28,14 @@ export default class Main {
     h1: {
       background: 'blue'
     }
+  }
+}
+
+
+@view.provide('test')
+class Sub {
+  render() {
+    console.log('gpot', this.test)
+    return null
   }
 }
--- packages/view/src/index.js
+++ packages/view/src/index.js
@@ -1,3 +1,4 @@
+import view from 'motion-view'
 import gloss from 'gloss'
 import { mix } from 'mixwith'
 import { addHelpers } from 'motion-class-helpers'
@@ -8,14 +9,12 @@ const styled = gloss()
 
 const HelpfulComponent = mix(React.Component)
   .with(
-    addHelpers
+    addHelpers()
   )
 
-// @view decorator
-export default Klass => {
-  // auto extend HelpfulComponent
+export default view(Klass => {
+  // add HelpfulComponent
   Object.setPrototypeOf(Klass.prototype, HelpfulComponent.prototype)
-
   // add gloss
   return styled(Klass)
-}
+})

c8d9fc7ce679a6612910ab64f231594f5edcb2d1
natew <natewienert@gmail.com>
2017-02-20 14:01:34 -0800

    :art: cleanup decorators

--- packages/decorators/src/index.js
+++ packages/decorators/src/index.js
@@ -14,14 +14,22 @@ export const glossy = gloss({ baseStyles })
 // view helpers
 const { provide, inject } = viewHelpers()
 
-// @view decorator
+const classHelpers = {
+  addEvent,
+  setTimeout,
+  setInterval,
+  ref,
+  watch,
+  react,
+}
+
+// @view
 export function view(View) {
   // extends React.Component
   Object.setPrototypeOf(View.prototype, React.Component.prototype)
   // mixins
-  mixin(View.prototype, { componentWillMount })
-  mixin(View.prototype, { addEvent, setTimeout, setInterval, ref })
-  mixin(View.prototype, { watch, react })
+  mixin(View.prototype, classHelpers)
+  mixin(View.prototype { componentWillMount })
   // gloss, mobx
   return glossy(observer(View))
 }
@@ -29,9 +37,8 @@ export function view(View) {
 view.provide = provide(view)
 view.inject = inject(view)
 
-// @store decorator
+// @store
 export function store(Store) {
-  mixin(Store.prototype, Helpers)
-  mixin(Store.prototype, { watch, react })
+  mixin(Store.prototype, classHelpers)
   return Store
 }

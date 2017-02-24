c36986e8a802d8c501259e373a07cecf789778e4
natew <natewienert@gmail.com>
2017-02-23 16:04:37 -0800

    :art: cleanups

--- packages/decorators/src/view.js
+++ packages/decorators/src/view.js
@@ -7,21 +7,23 @@ import { observer } from 'mobx-react'
 import baseStyles from './baseStyles'
 import * as ClassHelpers from './classHelpers'
 
-// mobx decorators
-export * from 'mobx'
 // gloss
 export const glossy = gloss({ baseStyles })
+
 // @view
 export const view = motionView(View => {
   // extends React.Component
   Object.setPrototypeOf(View.prototype, React.Component.prototype)
+
   // mixins
   mixin(View.prototype, ClassHelpers)
+
   // render({ props }, { state })
   const render = View.prototype.render
   View.prototype.render = function() {
     return render.call(this, this.props, this.state, this.context)
   }
-  // autobin, gloss, mobx (order important)
+
+  // autobind, gloss, mobx (order important)
   return autobind(glossy(observer(View)))
 })

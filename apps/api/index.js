6272713fa735c12721b36327a1701f93db446496
natew <natewienert@gmail.com>
2017-02-24 20:18:40 -0800

    :new: working towards working docker

--- apps/api/index.js
+++ apps/api/index.js
@@ -3,45 +3,47 @@ import http from 'http'
 import bodyParser from 'body-parser'
 import logger from 'morgan'
 import SuperLogin from 'superlogin'
+import config from './superlogin.config'
+import { FacebookStrategy } from 'passport-facebook'
+import { GitHubStrategy } from 'passport-github'
+import { GoogleStrategy } from 'passport-google-oauth'
 
 const app = express()
-const port = process.env.PORT || 3000
-console.log('starting on', port)
-app.set('port', port)
+
+app.set('port', process.env.PORT || 3000)
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const config = {
-  dbServer: {
-    protocol: 'http://',
-    host: 'localhost:5984',
-    user: '',
-    password: '',
-    userDB: 'users',
-    couchAuthDB: '_users'
-  },
-  mailer: {
-    fromEmail: 'gmail.user@gmail.com',
-    options: {
-      service: 'Gmail',
-        auth: {
-          user: 'gmail.user@gmail.com',
-          pass: 'userpass'
-        }
-    }
-  },
-  userDBs: {
-    defaultDBs: {
-      private: ['supertest']
-    }
-  }
-}
-
-// Initialize SuperLogin
 const superlogin = new SuperLogin(config)
 
-// Mount SuperLogin's routes to our app
+const strategies = [
+  ["facebook", FacebookStrategy],
+  ["github", GitHubStrategy],
+  ["google", GoogleStrategy],
+]
+
+strategies.forEach(([name, strategy]) => {
+  if(superlogin.config.getItem(`providers.${name}.credentials.clientID`)) {
+    superlogin.registerOAuth2(name, strategy)
+  }
+})
+
+// https redirect unless using localhost
+app.use((req, res, next) => {
+  if (
+    req.protocol === 'https'
+    || req.header('X-Forwarded-Proto') === 'https'
+    || req.hostname === 'localhost'
+  ) {
+    return next()
+  }
+  res.status(301).redirect(`https://${req.headers['host']}${req.url}`)
+})
+
+// superlogin routes
 app.use('/auth', superlogin.router)
+
+app.use(express.static('client'))
 
 http.createServer(app).listen(app.get('port'))

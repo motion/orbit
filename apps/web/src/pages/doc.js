0ee2b7db32c10e05a69d8a0c716a1230764b8cef
natew <natewienert@gmail.com>
2017-04-18 15:50:02 -0700

    :fire: cleanup

--- apps/web/src/pages/doc.js
+++ apps/web/src/pages/doc.js
@@ -3,29 +3,25 @@ import { view } from 'helpers'
 import { Page } from 'views'
 import Router from 'router'
 import Editor, { Raw } from 'views/editor'
-import { debounce } from 'lodash'
+import { throttle } from 'lodash'
 
 @view.provide({
   store: class {
     doc = Doc.get(Router.params.id.replace('-', ':'))
-    update = debounce(async val => {
+    update = throttle(async val => {
       const [doc] = this.doc.current
       doc.content = Raw.serialize(val)
       doc.save()
-    }, 300)
+    }, 100)
   },
 })
 export default class DocPage {
   render({ store }) {
-    window.store = store
     const [active] = store.doc.current || []
 
     if (!active) {
       return null
     }
-
-    delete active.key
-    delete active.data
 
     return (
       <Page>

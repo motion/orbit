3bb9d12917341ea51cc7d41027d042b8cb5f2d8e
natew <natewienert@gmail.com>
2017-04-18 17:15:49 -0700

    :art: editable titles

--- apps/web/src/pages/doc.js
+++ apps/web/src/pages/doc.js
@@ -3,31 +3,48 @@ import { view } from 'helpers'
 import { Page } from 'views'
 import Router from 'router'
 import Editor, { Raw } from 'views/editor'
-import { throttle } from 'lodash'
+import { throttle } from 'lodash-decorators'
 
 @view.provide({
   store: class {
-    doc = Doc.get(Router.params.id.replace('-', ':'))
-    update = throttle(async val => {
-      const [doc] = this.doc.current
-      doc.content = Raw.serialize(val)
-      doc.save()
-    }, 100)
+    docs = Doc.get(Router.params.id.replace('-', ':'))
+    get doc() {
+      return this.docs && this.docs.current && this.docs.current[0]
+    }
+    @throttle(100)
+    update = val => {
+      this.doc.content = Raw.serialize(val)
+      this.doc.save()
+    }
+    editTitle = () => {
+      this.doc.title = this.title.innerHTML
+      this.doc.save()
+    }
   },
 })
 export default class DocPage {
   render({ store }) {
-    const [active] = store.doc.current || []
+    const doc = store.doc
 
-    if (!active) {
+    if (!doc) {
       return null
     }
 
     return (
       <Page>
         <Page.Main>
-          <h2>Doc: {active.title}</h2>
-          <Editor content={active.content} onChange={store.update} />
+          <h2>
+            Doc:
+            {' '}
+            <title
+              ref={ref => store.title = ref}
+              contentEditable
+              onInput={store.editTitle}
+            >
+              {doc.title}
+            </title>
+          </h2>
+          <Editor content={doc.content} onChange={store.update} />
         </Page.Main>
       </Page>
     )

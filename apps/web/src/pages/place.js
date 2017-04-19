86502d6334b609437cc7a14b0438f7e4c668edbe
natew <natewienert@gmail.com>
2017-04-19 16:11:23 -0700

    :art: figure out multiple relations

--- apps/web/src/pages/place.js
+++ apps/web/src/pages/place.js
@@ -1,10 +1,12 @@
-import { view } from 'helpers'
-import { Place } from 'models'
+import { view, autorun } from 'helpers'
+import { Place, Doc } from 'models'
 import { Title, Text, Page, Link } from 'views'
 import Router from 'router'
+import DocItem from '~/src/views/doc/item'
 
 class PlaceStore {
   place = Place.get(Router.params.name)
+  docs = Doc.forBoard(Router.params.name)
 }
 
 @view.provide({
@@ -18,6 +20,10 @@ export default class PlacePage {
       <Page if={active}>
         <Page.Main>
           {active.title}
+          <docs if={store.docs.current}>
+            {(store.docs.current || [])
+              .map(doc => <DocItem key={doc._id} doc={doc} />)}
+          </docs>
         </Page.Main>
       </Page>
     )

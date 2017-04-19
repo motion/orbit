e3d41f7974e4846e0fca2d9995293b3cbc040a4b
natew <natewienert@gmail.com>
2017-04-19 09:56:46 -0700

    :minibus: basemodel

---
+++ apps/models/helpers/baseModel.js
@@ -0,0 +1,76 @@
+import { compile } from 'kontur'
+
+export default class BaseModel {
+  constructor({ defaultSchema, defaultProps }) {
+    this.defaultSchema = defaultSchema
+    this.compiledSchema = {
+      ...this.defaultSchema,
+      ...this.settings,
+      ...compile(this.constructor.props),
+    }
+  }
+
+  get defaultProps() {
+    const { defaultProps } = this.constructor
+    return typeof defaultProps === 'function'
+      ? defaultProps()
+      : defaultProps || {}
+  }
+
+  get title() {
+    return ((this.settings && this.settings.title) || this.constructor.name)
+      .toLowerCase()
+  }
+
+  get compiledMethods() {
+    return {
+      ...this.methods,
+      delete() {
+        return this.collection
+          .findOne(this._id)
+          .exec()
+          .then(doc => doc.remove())
+      },
+    }
+  }
+
+  async connect(db) {
+    this.db = db
+    this.collection = await db.collection({
+      name: this.title,
+      schema: this.compiledSchema,
+      statics: this.statics,
+      methods: this.compiledMethods,
+    })
+
+    if (this.hooks) {
+      Object.keys(this.hooks).forEach(hook => {
+        console.log('adding hook', this.title, hook)
+        this.collection[hook](this.hooks[hook])
+      })
+    }
+  }
+
+  timestamps(...props) {
+    const defined = props.filter(x => !!this.compiledSchema.properties[x])
+    const values = defined.reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur]: new Date().toISOString(),
+      }),
+      {}
+    )
+    return values
+  }
+
+  // helpers
+
+  create(object) {
+    const properties = {
+      ...object,
+      ...this.defaultProps,
+      ...this.timestamps('created_at', 'updated_at'),
+    }
+    return this.collection.insert(properties)
+  }
+}

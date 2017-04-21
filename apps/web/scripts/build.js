da8038914686436eb9becd0669c785e184c8c29f
natew <natewienert@gmail.com>
2017-04-21 13:50:50 -0700

    :art: build assets

--- apps/web/scripts/build.js
+++ apps/web/scripts/build.js
@@ -2,5 +2,9 @@ process.env.NODE_ENV = 'production'
 
 const webpack = require('webpack')
 const config = require('./config/webpack')
+const paths = require('./config/paths')
+const ncp = require('ncp')
 
 webpack(config, console.log.bind(console))
+
+ncp(paths.appPublic, paths.appBuild)

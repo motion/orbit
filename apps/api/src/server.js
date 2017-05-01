c2fdc174c8731a9c0e7862421741c4ca6570f9c8
natew <natewienert@gmail.com>
2017-04-30 19:21:37 -0700

    :new: server isolate, ignore eng

---
+++ apps/api/src/server.js
@@ -0,0 +1,73 @@
+import http from 'http'
+import proxy from 'express-http-proxy'
+import logger from 'morgan'
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import SuperLogin from 'superlogin'
+import { GitHubStrategy } from 'passport-github'
+import { GoogleStrategy } from 'passport-google-oauth'
+import { FacebookStrategy } from 'passport-facebook'
+import config from './superlogin.config.js'
+import { DB_PROTOCOL, DB_HOST } from './keys'
+
+export default class Server {
+  constructor() {
+    const app = express()
+    const port = process.env.PORT || 3000
+
+    console.log('Couch url:', COUCH_URL)
+
+    app.set('port', port)
+    app.use(logger('dev'))
+
+    // CORS
+    app.use(
+      cors({
+        origin: 'http://localhost:3001',
+        credentials: true,
+      })
+    )
+
+    app.use('/', proxy(`http://${DB_HOST}`))
+
+    // middleware
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: false }))
+
+    // // SUPERLOGIN
+    // const superlogin = new SuperLogin(config)
+    // const strategies = [
+    //   ['facebook', FacebookStrategy],
+    //   ['github', GitHubStrategy],
+    //   ['google', GoogleStrategy],
+    // ]
+    // strategies.forEach(([name, strategy]) => {
+    //   if (superlogin.config.getItem(`providers.${name}.credentials.clientID`)) {
+    //     superlogin.registerOAuth2(name, strategy)
+    //   }
+    // })
+
+    // // https redirect unless using localhost
+    // app.use((req, res, next) => {
+    //   if (
+    //     req.protocol === 'https' ||
+    //     req.header('X-Forwarded-Proto') === 'https' ||
+    //     req.hostname === 'localhost'
+    //   ) {
+    //     return next()
+    //   }
+    //   res.status(301).redirect(`https://${req.headers['host']}${req.url}`)
+    // })
+
+    // superlogin routes
+    // app.use('/couch/auth', superlogin.router)
+
+    this.server = app
+  }
+
+  start() {
+    console.log('Server starting on port', port)
+    http.createServer(this.server).listen(this.server.get('port'))
+  }
+}

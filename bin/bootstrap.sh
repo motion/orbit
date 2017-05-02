f639134c42fab5e17d47612ce476666f4ebf0294
natew <natewienert@gmail.com>
2017-05-02 13:04:44 -0700

    :new: starting on kubes prod stuff

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -5,3 +5,9 @@ sudo mv $PWD/deis /usr/local/bin/deis
 # install helm
 brew install kubernetes-helm
 helm init
+
+# kompose
+curl -L https://github.com/kubernetes-incubator/kompose/releases/download/v0.6.0/kompose-darwin-amd64 -o kompose
+chmod +x kompose
+sudo mv ./kompose /usr/local/bin/kompose
+

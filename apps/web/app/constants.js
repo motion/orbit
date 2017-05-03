f0d9d67736ed5ab17b8b323336043dc150209596
natew <natewienert@gmail.com>
2017-05-03 13:07:25 -0700

    :bug: fix some stuff

--- apps/web/app/constants.js
+++ apps/web/app/constants.js
@@ -3,9 +3,7 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 export const DB_CONFIG = {
   name: 'username',
   password: 'password',
-  couchUrl: IS_PROD
-    ? 'https://mirai.cloudant.com'
-    : 'http://localhost:3000/couch', //'https://jot-app-api.herokuapp.com/couch',
+  couchUrl: IS_PROD ? 'http://couch.write.sexy' : 'http://couch.jot.dev',
 }
 
 export const HEADER_HEIGHT = 38

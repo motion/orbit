73e83278ac28fe7107304b97b22f0d0d92ef6cd0
natew <natewienert@gmail.com>
2017-05-04 18:35:39 -0700

    :bug:

--- apps/web/app/constants.js
+++ apps/web/app/constants.js
@@ -3,7 +3,7 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 export const DB_CONFIG = {
   name: 'username',
   password: 'password',
-  couchUrl: `${window.location.protocol}//couch.${window.location.hostname}`,
+  couchUrl: `${window.location.protocol}//couch.${window.location.host}`,
 }
 
 export const HEADER_HEIGHT = 38

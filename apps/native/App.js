c14124943aee3a7619e16e2b770c8c69af8dee89
natew <natewienert@gmail.com>
2017-05-06 00:16:09 -0700

    :minibus: native

--- apps/native/App.js
+++ apps/native/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text if={false}>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make wi2ll automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -20,4 +20,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})

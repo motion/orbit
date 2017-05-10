cbef77477e630cd05e682f77a66b33319f9c8614
natew <natewienert@gmail.com>
2017-05-09 20:44:43 -0700

    :bug: fix superlogin conf

--- apps/api/src/keys.js
+++ apps/api/src/keys.js
@@ -1,10 +1,11 @@
 import invariant from 'invariant'
 
-export const DB_HOST = process.env.DB_HOST
+export const DB_HOSTNAME = process.env.DB_HOSTNAME
 export const DB_PORT = process.env.DB_PORT
+export const DB_HOST = `${DB_HOSTNAME}:${DB_PORT}`
 export const DB_PROTOCOL = process.env.DB_PROTOCOL
-export const DB_USER = process.env.DB_USER
-export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_USER = process.env.DB_USER || ''
+export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 
 invariant(DB_HOST, 'must set DB_HOST')
 invariant(DB_PORT, 'must set DB_PORT')

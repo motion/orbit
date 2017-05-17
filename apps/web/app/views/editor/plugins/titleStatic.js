c896908821ce62885f51c2cf29a148f7611ab6ff
natew <natewienert@gmail.com>
2017-05-17 10:29:11 -0700

    :art: behavior around meta

--- apps/web/app/views/editor/plugins/titleStatic.js
+++ apps/web/app/views/editor/plugins/titleStatic.js
@@ -2,22 +2,34 @@
 
 export default {
   onKeyDown: (e, data, state) => {
-    const { startBlock } = state
     const isEnter = e.which === 13
 
+    // only start blocks and enter event
     if (!isEnter) return
-    if (!/^(title|meta)$/.test(startBlock.type)) {
+    if (!/^(title|meta)$/.test(state.startBlock.type)) {
       return
+    }
+
+    // if at end of block
+    if (
+      state.startOffset === state.endOffset &&
+      state.endOffset === state.startBlock.length
+    ) {
+      if (state.startBlock.type === 'meta') {
+        // allow insert
+        return
+      }
     }
 
     e.preventDefault()
 
-    const firstBodyNode = state.document.nodes.get(2)
+    const currentNodeIndex = state.document.nodes.indexOf(state.startBlock)
+    const nextNode = state.document.nodes.get(currentNodeIndex + 1)
 
     return state
       .transform()
-      .collapseToEndOf(firstBodyNode)
-      .moveOffsetsTo(firstBodyNode.length)
+      .collapseToEndOf(nextNode)
+      .moveOffsetsTo(nextNode.length)
       .apply()
   },
 }

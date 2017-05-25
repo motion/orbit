890766318010c489eae89421ea96bc71c6cc2d75
natew <natewienert@gmail.com>
2017-05-24 23:13:08 -0700

    :art:

--- apps/web/app/views/editor/nodes/column.js
+++ apps/web/app/views/editor/nodes/column.js
@@ -12,20 +12,38 @@ export default class Column {
 
   render({ node, attributes, children }) {
     const flex = node.data.get('flex') || 1
-    console.log('change', onChange)
+    console.log('flex', flex)
 
     return (
       <column $$flex={flex} {...attributes}>
         {children}
-        <Icon name="add" onClick={() => this.bump(node)} />
+        <Icon
+          $icon
+          name="expand"
+          onClick={() => this.bump(node)}
+          size={10}
+          color={[0, 0, 0, 0.6]}
+        />
       </column>
     )
   }
 
   static style = {
     column: {
-      border: [1, '#555'],
       flex: 1,
+      position: 'relative',
+    },
+    icon: {
+      position: 'absolute',
+      top: '50%',
+      marginTop: -5,
+      right: 0,
+      bottom: 0,
+      cursor: 'default',
+      opacity: 0.3,
+      '&:hover': {
+        opacity: 1,
+      },
     },
   }
 }

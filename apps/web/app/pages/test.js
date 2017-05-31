97f6b51397e633f0a7bd110f0ae4b64f16a8d7eb
Nick Cammarata <cammarata.nick@gmail.com>
2017-05-31 13:10:20 -0700

    less test page

--- apps/web/app/pages/test.js
+++ apps/web/app/pages/test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { view } from '~/helpers'
 import { Document } from '@jot/models'
 import Page from '~/views/page'
-import Overdrive from 'react-overdrive'
 import { flatten } from 'lodash'
 
 class TestStore {
@@ -17,21 +16,7 @@ export default class Test {
     return (
       <Page header title="Todo" actions={[]}>
         <test style={{ padding: 20 }}>
-          <Overdrive id="boo" key={'boo'} if={store.align == 'left'}>
-            <h1>big text</h1>
-          </Overdrive>
-          <Overdrive
-            id="boo"
-            key={'boo'}
-            style={{ marginLeft: 800 }}
-            if={store.align == 'center'}
-          >
-            <h3>small</h3>
-          </Overdrive>
-
-          <up onClick={() => store.align = 'left'}>left</up>
-          <up onClick={() => store.align = 'center'}>center</up>
-          <up onClick={() => store.align = 'right'}>right</up>
+          <h5>just a page for testing</h5>
         </test>
       </Page>
     )

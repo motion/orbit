7e07893c4da128d80278f8a7fd4d4ac7909c8d06
Nick Cammarata <cammarata.nick@gmail.com>
2017-06-05 01:52:55 -0700

    properly use node state

--- apps/web/app/editor/plugins/email.js
+++ apps/web/app/editor/plugins/email.js
@@ -1,16 +1,15 @@
 import { replacer } from '~/editor/helpers'
 import React from 'react'
-import { Editor, Raw } from 'slate'
 import { view, observable } from '~/helpers'
 import node from '~/editor/node'
 import { Button } from '~/ui'
 import { BLOCKS } from '~/editor/constants'
 
 class EmailStore {
-  contents = null
   address = 'forward@jot.dev'
 
   onFill = () => {
+    const { node: { data }, setData } = this.props
     const nick = {
       from: { email: 'cammarata.nick@gmail.com', name: 'Nick Cammarata' },
     }
@@ -19,7 +18,7 @@ class EmailStore {
       from: { email: 'nate.wienert@gmail.com', name: 'Nate Wienert' },
     }
 
-    this.contents = [
+    const contents = [
       {
         ...nick,
         content: 'Nate, what do you think about Jot so far?',
@@ -37,6 +36,13 @@ class EmailStore {
         content: "I don't know, there's really too much blur for me to see anything",
       },
     ]
+
+    setData(data.set('contents', contents))
+  }
+
+  onReset = () => {
+    const { node: { data }, setData } = this.props
+    setData(data.set('contents', null))
   }
 }
 
@@ -46,24 +52,31 @@ class EmailStore {
 })
 class EmailNode {
   render({ store, node: { data } }) {
+    const contents = data.get('contents') || null
+
+    console.log('contents are', contents)
+
     return (
       <emailNode contentEditable="false">
-        <none if={store.contents === null}>
+        <none if={contents === null}>
           <noneText>Forward to {store.address}</noneText>
           <Button $button onClick={store.onFill}>
             Fake it till you make it
           </Button>
         </none>
-        <emails if={store.contents !== null}>
-          {store.contents.map(email => (
-            <email>
-              <from $$row>
-                <name>{email.from.name}</name>
-                <address>{email.from.email}</address>
-              </from>
-              <content>{email.content}</content>
-            </email>
-          ))}
+        <emails if={contents !== null}>
+          <inner>
+            {contents.map(email => (
+              <email>
+                <from $$row>
+                  <name>{email.from.name}</name>
+                  <address>{email.from.email}</address>
+                </from>
+                <content>{email.content}</content>
+              </email>
+            ))}
+          </inner>
+          <Button $button onClick={store.onReset}>reset</Button>
         </emails>
       </emailNode>
     )
@@ -110,5 +123,5 @@ export default class Email {
   nodes = {
     [BLOCKS.EMAIL]: EmailNode,
   }
-  plugins = [replacer(/^(\-email)$/, 'email', {})]
+  plugins = [replacer(/^(\-email)$/, 'email', { contents: null })]
 }

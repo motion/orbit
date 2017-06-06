4b2213a53763bad858ee045f9a5695a85a57f565
Nick Cammarata <cammarata.nick@gmail.com>
2017-06-06 13:28:18 -0700

    gradients

--- apps/web/app/index.js
+++ apps/web/app/index.js
@@ -1,6 +1,7 @@
 // for a fast splash screen to show
 // this just requires react + <Splash />
 
+import 'regenerator-runtime/runtime'
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'

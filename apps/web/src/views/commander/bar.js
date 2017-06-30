4e4214f59ca7a412989d8abcc3caf2b25d36a3c6
Nick Cammarata <cammarata.nick@gmail.com>
2017-06-29 18:20:34 -0700

    sizing

--- apps/web/src/views/commander/bar.js
+++ apps/web/src/views/commander/bar.js
@@ -9,17 +9,26 @@ export default class CommanderInput {
     return (
       <bar $$align="center" $$row $$flex>
         <Input
-          size={1.4}
+          $input
+          size={1.1}
           onChange={commanderStore.onChange}
           onKeyDown={commanderStore.onKeyDown}
           onFocus={commanderStore.onFocus}
           placeholder={commanderStore.place}
           placeholderColor={[0, 0, 0, 0.1]}
-          borderColor="transparent"
           getRef={commanderStore.ref('input').set}
           value={commanderStore.value}
         />
       </bar>
     )
+  }
+
+  static style = {
+    bar: {},
+    input: {
+      width: '100%',
+      marginLeft: 10,
+      marginTop: 0,
+    },
   }
 }

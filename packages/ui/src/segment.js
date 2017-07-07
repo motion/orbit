95b39f71a24e89505e339d00ecffa5dd959a6b69
natew <natewienert@gmail.com>
2017-07-07 16:04:09 -0700

    :bug: popovers enforce pointerevents

--- packages/ui/src/segment.js
+++ packages/ui/src/segment.js
@@ -130,7 +130,6 @@ export default class Segment implements ViewType<Props> {
 
   static style = {
     segment: {
-      pointerEvents: 'auto',
       flexFlow: 'row',
       alignItems: 'center',
       userSelect: 'none',

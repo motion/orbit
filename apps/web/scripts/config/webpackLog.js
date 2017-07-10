aa65e7358399b3549466d4f623b5f9739b4a58ec
natew <natewienert@gmail.com>
2017-07-10 13:17:41 -0700

    try webpack

---
+++ apps/web/scripts/config/webpackLog.js
@@ -0,0 +1,26 @@
+module.exports = (fatalError, stats) => {
+  if (fatalError) {
+    console.error(fatalError)
+    return
+  }
+
+  const jsonStats = stats.toJson('verbose')
+  const buildError = fatalError || jsonStats.errors[0] || jsonStats.warnings[0]
+
+  if (buildError) {
+    console.log(buildError)
+  } else {
+    console.log(
+      stats.toString({
+        assets: true,
+        chunks: true,
+        colors: true,
+        hash: false,
+        providedExports: true,
+        timings: true,
+        usedExports: true,
+        version: false,
+      })
+    )
+  }
+}

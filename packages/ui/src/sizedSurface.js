a46ba3aff0731b32c192d93ad2f9b9b3b2600b94
natew <natewienert@gmail.com>
2017-07-17 17:59:40 -0700

    save

--- packages/ui/src/sizedSurface.js
+++ packages/ui/src/sizedSurface.js
@@ -4,12 +4,12 @@ import Surface from './surface'
 
 const LINE_HEIGHT = 30
 const replaceWith = (into, outof) => obj => (obj === into ? outof : obj)
-const apply = (obj, fn) => (Array.isArray(obj) ? obj.map(fn) : fn(obj))
-const applyMultiply = (x, y) => apply(x, z => z * y)
-const multiply = (val: any, size: number, replaceVal: any = 1) =>
-  applyMultiply(replaceWith(true, replaceVal)(val), size)
-
 const default1 = replaceWith(true, 1)
+
+const apply = (obj: any, fn: Function) =>
+  Array.isArray(obj) ? obj.map(fn) : fn(obj)
+const applyMultiply = (x, y) => apply(x, z => z * y)
+const multiply = (val: any, size: number) => applyMultiply(default1(val), size)
 
 export default class SizedSurface extends React.Component {
   static defaultProps = {
@@ -30,18 +30,21 @@ export default class SizedSurface extends React.Component {
 
     // sizes
     let height = sizeHeight && props.size * LINE_HEIGHT * default1(sizeHeight)
-    const fontSize =
-      props.fontSize || (sizeFont && height * 0.45 * default1(sizeFont))
+    const fontSize = sizeFont && height * 0.45 * default1(sizeFont)
+
+    // do after fontSize calc
     if (props.inline) {
       height = height * 0.8
     }
+
     const borderRadius =
       sizeRadius && height / 3.4 * multiply(props.radius, sizeRadius)
-    const padWithWrap = props.wrapElement ? 0 : height / 3.5
+
+    const sizePadding2 = props.wrapElement ? 0 : height / 3.5
     const padding =
-      props.padding ||
-      (sizePadding && [0, padWithWrap * multiply(props.padding, sizePadding)])
-    const margin = sizeMargin && multiply(props.margin, sizeMargin) * 0.25
+      sizePadding && multiply(props.padding, sizePadding2 * sizePadding)
+
+    const margin = sizeMargin && multiply(props.margin, sizeMargin * 0.25)
 
     const pass = {
       borderRadius,

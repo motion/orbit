68adf762b554d64a0ec10ed2a8363aacdd742cba
natew <natewienert@gmail.com>
2017-08-08 18:02:44 -0700

    :fire: old

--- apps/electron/src/window.js
+++ apps/electron/src/window.js
@@ -9,25 +9,11 @@ export default class Window extends React.Component {
 
   componentDidUpdate() {
     if (this.props.show) {
-      console.log('show this window')
       if (!this.state.showing) {
         this.setState({ showing: true })
       }
     }
-
-    if (this.state.showing) {
-      this.startAnimate()
-    }
   }
-
-  startAnimate = once(() => {
-    console.log('startanimate')
-    this.interval = setInterval(() => {
-      console.log('setstate')
-      // this.setState({ position: this.state.position + 10 })
-      clearInterval(this.interval)
-    }, 1000)
-  })
 
   getPublicInstance() {
     return this.ref

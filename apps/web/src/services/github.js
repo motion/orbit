4fccb9ee7d30dad2e4d9065f7ebc2283f2d3df90
natew <natewienert@gmail.com>
2017-08-24 20:52:24 -0700

    save

--- apps/web/src/services/github.js
+++ apps/web/src/services/github.js
@@ -1,34 +1,36 @@
-// @flow
 import Octokat from 'octokat'
 import { store, watch } from '@mcro/black'
-import type CurrentUser from '../stores/currentUserStore'
-
-type GithubRepo = {
-  id: number,
-  owner: Object,
-  name: string,
-  url: string,
-  default_branch: string,
-  permissions: Object,
-  created_at: string,
-  updated_at: string,
-}
 
 @store
 export default class GithubService {
-  user: ?CurrentUser = null
+  user = null
 
   @watch
   orgs = () => this.setting && this.github && this.github.user.orgs.fetchAll()
 
   @watch
-  repos: Array<GithubRepo> = () =>
+  allRepos = () =>
     this.github &&
     this.activeOrgs &&
-    this.activeOrgs.map(org => this.github.orgs(org).repos.fetchAll())
+    Promise.all(
+      this.activeOrgs.map(org =>
+        this.github.orgs(org).repos.fetchAll().then(repos => ({ org, repos }))
+      )
+    )
 
   @watch
-  github: Octokat = () =>
+  repos = () =>
+    this.allRepos &&
+    this.allRepos.reduce(
+      (acc, { org, repos }) => ({
+        ...acc,
+        [org]: repos.reduce((acc, cur) => ({ ...acc, [cur.name]: cur }), {}),
+      }),
+      {}
+    )
+
+  @watch
+  github = () =>
     this.token &&
     new Octokat({
       token: this.token,
@@ -46,7 +48,7 @@ export default class GithubService {
     return this.user && this.user.setting.github
   }
 
-  get activeOrgs(): { [string]: boolean } {
+  get activeOrgs() {
     return (
       this.setting && Object.keys(this.setting.values.orgs).filter(x => !!x)
     )

d96404ae8215bfead871579a21197eaa39b48dac
natew <natewienert@gmail.com>
2017-09-21 15:28:55 -0700

    save

--- apps/web/src/sync/syncers/github/issueSync.js
+++ apps/web/src/sync/syncers/github/issueSync.js
@@ -6,6 +6,7 @@ import { omit, flatten } from 'lodash'
 export default class GithubIssueSync {
   setting: Setting
   token: string
+
   constructor(setting: Setting, token: string) {
     this.setting = setting
     this.token = token
@@ -16,7 +17,7 @@ export default class GithubIssueSync {
       throw new Error('User hasnt selected any orgs in settings')
     }
     const createdIssues = flatten(
-      await Promise.all(this.setting.activeOrgs.map(this.syncIssues))
+      await Promise.all(this.setting.activeOrgs.map(this.createIssuesForOrg))
     ).filter(Boolean)
     console.log(
       'Created',
@@ -26,50 +27,41 @@ export default class GithubIssueSync {
     )
   }
 
-  syncIssues = async (orgLogin: string): Promise<?Array<Object>> => {
-    const results = await this.graphFetch({
-      query: `
-      query AllIssues {
-        organization(login: "${orgLogin}") {
-          repositories(first: 50) {
-            edges {
-              node {
-                id
-                name
-                issues(first: 100) {
-                  edges {
-                    node {
-                      id
-                      title
-                      number
-                      body
-                      bodyText
-                      updatedAt
-                      createdAt
-                      author {
-                        avatarUrl
-                        login
-                      }
-                      labels(first: 10) {
-                        edges {
-                          node {
-                            name
-                          }
-                        }
-                      }
-                      comments(first: 100) {
-                        edges {
-                          node {
-                            author {
-                              avatarUrl
-                              login
-                            }
-                            createdAt
-                            body
-                          }
-                        }
-                      }
+  graphQuery = {
+    repo: `edges {
+      node {
+        id
+        name
+        issues(first: 100) {
+          edges {
+            node {
+              id
+              title
+              number
+              body
+              bodyText
+              updatedAt
+              createdAt
+              author {
+                avatarUrl
+                login
+              }
+              labels(first: 10) {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+              comments(first: 100) {
+                edges {
+                  node {
+                    author {
+                      avatarUrl
+                      login
                     }
+                    createdAt
+                    body
                   }
                 }
               }
@@ -77,26 +69,52 @@ export default class GithubIssueSync {
           }
         }
       }
-    `,
+    }`,
+  }
+
+  getIssuesFromRepo = (repository: Object) => {
+    return repository.issues.edges.map(edge => ({ ...edge.node, repository }))
+  }
+
+  unwrapIssue = (obj: Object) => {
+    obj.labels = obj.labels.edges.map(edge => edge.node)
+    obj.comments = obj.comments.edges.map(edge => edge.node)
+    return obj
+  }
+
+  createIssue = async (issue: Object, orgLogin: string) => {
+    const data = this.unwrapIssue(omit(issue, ['bodyText']))
+    const id = `${issue.id}`
+    // ensure if one is set, the other gets set too
+    const created = issue.createdAt || issue.updatedAt || ''
+    const updated = issue.updatedAt || created
+    // stale removal
+    const stale = await Thing.get({ id, created: { $ne: created } })
+    if (stale) {
+      console.log('Removing stale event', id)
+      await stale.remove()
+    }
+    // already exists
+    if (updated && (await Thing.get({ id, updated }))) {
+      return false
+    }
+    return await Thing.update({
+      id,
+      integration: 'github',
+      type: 'task',
+      title: issue.title,
+      body: issue.bodyText,
+      data,
+      orgName: orgLogin,
+      parentId: issue.repository,
+      created,
+      updated,
     })
+  }
 
-    if (results.message) {
-      console.error('Error doing fetch', results)
-      return null
-    }
-
-    const repositories = results.data.organization.repositories.edges
-    if (!repositories || !repositories.length) {
-      console.log('no repos found in response', repositories)
-      return null
-    }
-
-    const unwrap = obj => {
-      obj.labels = obj.labels.edges.map(edge => edge.node)
-      obj.comments = obj.comments.edges.map(edge => edge.node)
-      return obj
-    }
-
+  createIssuesForOrg = async (orgLogin: string): Promise<?Array<Object>> => {
+    const repositories = await this.getRepositoriesForOrg(orgLogin)
+    const issues = flatten(repositories.map(this.getIssuesFromRepo))
     let finished = []
     let creating = []
 
@@ -106,57 +124,41 @@ export default class GithubIssueSync {
       creating = []
     }
 
-    for (const repositoryNode of repositories) {
-      const repository = repositoryNode.node
-      const issues = repository.issues.edges.map(edge => edge.node)
-
-      if (!issues || !issues.length) {
-        continue
+    for (const issue of issues) {
+      // pause for every 10 to finish
+      if (creating.length === 10) {
+        await waitForCreating()
       }
-
-      for (const issue of issues) {
-        // pause for every 10 to finish
-        if (creating.length === 10) {
-          await waitForCreating()
-        }
-
-        const create = async () => {
-          const data = unwrap(omit(issue, ['bodyText']))
-          const id = `${issue.id}`
-          // ensure if one is set, the other gets set too
-          const created = issue.createdAt || issue.updatedAt || ''
-          const updated = issue.updatedAt || created
-          // stale removal
-          const stale = await Thing.get({ id, created: { $ne: created } })
-          if (stale) {
-            console.log('Removing stale event', id)
-            await stale.remove()
-          }
-          // already exists
-          if (updated && (await Thing.get({ id, updated }))) {
-            return false
-          }
-          return await Thing.update({
-            id,
-            integration: 'github',
-            type: 'task',
-            title: issue.title,
-            body: issue.bodyText,
-            data,
-            orgName: orgLogin,
-            parentId: repository.name,
-            created,
-            updated,
-          })
-        }
-
-        creating.push(create())
-      }
+      creating.push(this.createIssue(issue, orgLogin))
     }
 
     await waitForCreating()
-
     return finished
+  }
+
+  getRepositoriesForOrg = async (orgLogin: string): ?Array<Object> => {
+    const results = await this.graphFetch({
+      query: `
+      query AllIssues {
+        organization(login: "${orgLogin}") {
+          repositories(first: 50) {
+            ${this.graphQuery.repo}
+          }
+        }
+      }
+    `,
+    })
+    if (results.message) {
+      console.error('Error doing fetch', results)
+      return null
+    }
+    let repositories = results.data.organization.repositories.edges
+    if (!repositories || !repositories.length) {
+      console.log('no repos found in response', repositories)
+      return null
+    }
+    repositories = repositories.map(r => r.node)
+    return repositories
   }
 
   graphFetch = createApolloFetch({

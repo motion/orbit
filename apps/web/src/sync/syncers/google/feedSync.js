cc740f4668d83bc0f769bcd1c02f884944ac2f85
natew <natewienert@gmail.com>
2017-09-21 21:38:24 -0700

    :fire:

--- apps/web/src/sync/syncers/google/feedSync.js
+++ apps/web/src/sync/syncers/google/feedSync.js
@@ -1,11 +1,8 @@
 // @flow
 import { Event } from '~/app'
-import GAPI from './api'
 import SyncerAction from '../syncerAction'
 
 export default class GoogleFeedSync extends SyncerAction {
-  api = new GAPI({ key: this.token })
-
   run = async () => {
     console.log('run', this.api)
   }
--- apps/web/src/sync/syncers/google/googleSync.js
+++ apps/web/src/sync/syncers/google/googleSync.js
@@ -2,6 +2,7 @@
 import { store } from '@mcro/black/store'
 import Syncer from '../syncer'
 import GoogleFeedSync from './feedSync'
+import * as Constants from '~/constants'
 
 @store
 export default class GoogleSync extends Syncer {
@@ -11,5 +12,27 @@ export default class GoogleSync extends Syncer {
   }
   static syncers = {
     feed: GoogleFeedSync,
+  }
+
+  clientId = Constants.GOOGLE_CLIENT_ID
+
+  constructor(...args) {
+    super(...args)
+
+    // setTimeout(() => {
+    //   const gapi = window.gapi
+    //   gapi.load('client', async () => {
+    //     const opts = {
+    //       apiKey: this.token,
+    //       scope: ['profile'], // , 'drive'
+    //       clientId: Constants.GOOGLE_CLIENT_ID,
+    //     }
+    //     console.log('init with opts', opts)
+    //     await gapi.client.init(opts)
+
+    //     this.api = gapi.client
+    //     console.log('set it up', this.api)
+    //   })
+    // })
   }
 }

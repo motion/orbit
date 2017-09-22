46726051c23849fccf55ec372ef2040b6af79c1f
natew <natewienert@gmail.com>
2017-09-21 18:32:11 -0700

    :bug: actually working

--- packages/hmr-view/lib/index.js
+++ packages/hmr-view/lib/index.js
@@ -82,7 +82,7 @@ function proxyReactComponents({
 
 setInterval(() => {
   if (reloaded.length) {
-    console.log(`[HMR] views: ${reloaded.join(', ')}, ${reloadedInstances} instances`);
+    console.log(`[HMR] ${reloaded.join(', ')}, ${reloadedInstances} instances`);
     if (window.Black) {
       window.Black.view.emit('hmr');
     }

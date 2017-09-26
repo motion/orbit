8b13700264c6040089651e787800695b2fec8d57
natew <natewienert@gmail.com>
2017-09-26 12:16:15 -0700

    save

--- apps/api/src/server/oauth.js
+++ apps/api/src/server/oauth.js
@@ -1,7 +1,8 @@
 import Passport from 'passport'
+import Refresh from 'passport-oauth2-refresh'
 
-export default class OAuth {
-  passport = Passport
+export default class Oauth {
+  cache = {}
 
   constructor({ strategies, onSuccess }) {
     if (!strategies) {
@@ -10,33 +11,61 @@ export default class OAuth {
     if (!onSuccess) {
       throw Error(`Need to provide onSuccess callback`)
     }
+    this.setupStrategies(strategies, onSuccess)
+    this.setupSerialization()
+  }
 
+  setupStrategies(strategies, onSuccess) {
     for (const name of Object.keys(strategies)) {
       const { strategy: Strategy, config } = strategies[name]
       if (!config.credentials) {
         console.log('No credentials found for strategy', name)
         continue
       }
-      Passport.use(
-        new Strategy(config.credentials, async function(
-          accessToken,
-          refreshToken,
-          profile,
-          cb
-        ) {
-          try {
-            const user = await onSuccess(
-              name,
-              accessToken,
-              refreshToken,
-              profile
-            )
-            cb(null, user)
-          } catch (err) {
-            cb(err)
-          }
-        })
-      )
+
+      const strategy = new Strategy(config.credentials, async function(
+        accessToken,
+        refreshToken,
+        profile,
+        cb
+      ) {
+        try {
+          const user = await onSuccess(name, accessToken, refreshToken, profile)
+          cb(null, user)
+        } catch (err) {
+          cb(err)
+        }
+      })
+
+      Passport.use(strategy)
+      Refresh.use(strategy)
     }
+  }
+
+  refreshToken(userId, service) {
+    return new Promise((resolve, reject) => {
+      Refresh.requestNewAccessToken(
+        service,
+        this.cache[userId].refreshToken,
+        (err, accessToken) => {
+          if (err || !accessToken) {
+            reject(err || 'No access token')
+          }
+          resolve(accessToken)
+        }
+      )
+    })
+  }
+
+  setupSerialization() {
+    Passport.serializeUser((user, done) => {
+      const id = user.info.id
+      this.cache[id] = user.info
+      done(null, id)
+    })
+
+    Passport.deserializeUser((id, done) => {
+      done(null, this.cache[id])
+    })
   }
 }

51d696184a61fac454a7c2ac6771b92c6db5c5b5
natew <natewienert@gmail.com>
2017-09-27 13:11:23 -0700

    save

--- apps/web/src/helpers/electron.js
+++ apps/web/src/helpers/electron.js
@@ -1,5 +1,9 @@
-const electron = (window.require && window.require('electron')) || {}
+import electron, { ipcRenderer } from 'electron'
 
-export const OS = electron.ipcRenderer
+export const OS = ipcRenderer || {
+  send() {
+    console.log('Avoid OS.send, not in electron')
+  },
+}
 
 export default electron

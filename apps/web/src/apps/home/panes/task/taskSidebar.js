48130e5745017083924a63e8338f87dfbf46d49d
natew <natewienert@gmail.com>
2017-10-11 20:13:09 -0700

    :bug: fix paths

--- apps/web/src/apps/home/panes/task/taskSidebar.js
+++ apps/web/src/apps/home/panes/task/taskSidebar.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react'
 import { view } from '@mcro/black'
-import Pane from '~/apps/pane'
+import Pane from '../pane'
 import { SidebarTitle, getItem } from '../helpers'
 
 class SidebarTaskStore {

86a9556d3cd383e8d6f56a33eebcf0ab763676de
natew <natewienert@gmail.com>
2017-10-14 21:39:07 -0700

    :new: etc hosts setup

--- apps/api/src/api.js
+++ apps/api/src/api.js
@@ -1,38 +1,38 @@
 // @flow
 import Server from './server'
-// import Database, { Models } from '@mcro/models'
-// import PouchAdapterMemory from 'pouchdb-adapter-memory'
 import pouch from 'pouchdb'
+import hostile_ from 'hostile'
+import * as Constants from '~/constants'
+import { promisifyAll } from 'sb-promisify'
+import sudoPrompt_ from 'sudo-prompt'
+
+const hostile = promisifyAll(hostile_)
+const sudoPrompt = promisifyAll(sudoPrompt_)
 
 export default class API {
   server: Server
-  // database: Database
 
   constructor() {
-    // this.database = new Database(
-    //   {
-    //     name: 'username',
-    //     password: 'password',
-    //     adapter: PouchAdapterMemory,
-    //     adapterName: 'memory',
-    //   },
-    //   Models
-    // )
-    // const pouch = this.database.pouch
     this.server = new Server({ pouch })
   }
 
   async start() {
+    this.setupHosts()
     const port = this.server.start()
-    // await this.database.start({
-    //   modelOptions: {
-    //     debug: true,
-    //   },
-    // })
     console.log('API on port', port)
   }
 
   dispose() {
     this.server.dispose()
+  }
+
+  async setupHosts() {
+    const lines = await hostile.get(true)
+    const exists = lines.map(line => line[1]).indexOf(Constants.APP_HOST) > -1
+    if (!exists) {
+      await sudoPrompt.exec(`npx hostile set 127.0.0.1 ${Constants.APP_HOST}`, {
+        name: 'Orbit',
+      })
+    }
   }
 }

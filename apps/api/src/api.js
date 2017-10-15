d30e2114c4f1ae30bca280ea56b92f9f70f0588a
natew <natewienert@gmail.com>
2017-10-14 22:11:25 -0700

    :bug: fix working memory pouch

--- apps/api/src/api.js
+++ apps/api/src/api.js
@@ -1,11 +1,17 @@
 // @flow
 import Server from './server'
-import PouchDB from 'pouchdb'
+import PouchDBBrowser from 'pouchdb-browser'
+import PouchHttp from 'pouchdb-adapter-http'
+import PouchMapReduce from 'pouchdb-mapreduce'
+import PouchMemory from 'pouchdb-adapter-memory'
 import hostile_ from 'hostile'
 import * as Constants from '~/constants'
 import { promisifyAll } from 'sb-promisify'
 import sudoPrompt_ from 'sudo-prompt'
-import memdown from 'memdown'
+
+const PouchDB = PouchDBBrowser.plugin(PouchHttp)
+  .plugin(PouchMapReduce)
+  .plugin(PouchMemory)
 
 const hostile = promisifyAll(hostile_)
 const sudoPrompt = promisifyAll(sudoPrompt_)
@@ -14,7 +20,7 @@ export default class API {
   server: Server
 
   constructor() {
-    const pouch = PouchDB.defaults({ db: memdown })
+    const pouch = PouchDB.defaults({ adapter: 'memory' })
     this.server = new Server({ pouch })
   }
 

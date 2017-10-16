68498ba1ed0145e00ab126a7358807bc99e36f6c
natew <natewienert@gmail.com>
2017-10-15 19:46:59 -0700

    :bug: orbit

--- apps/electron/src/constants.js
+++ apps/electron/src/constants.js
@@ -1,4 +1,4 @@
 export const IS_PROD = process.env.NODE_ENV === 'production'
-export const APP_URL = IS_PROD ? 'http://seemirai.com' : 'http://jot.dev'
+export const APP_URL = IS_PROD ? 'http://orbit.dev' : 'http://jot.dev'
 export const APP_HOME = '/'
 export const IS_MAC = process.platform === 'darwin'

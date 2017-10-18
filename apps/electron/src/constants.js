78953267e5cc3283659657ebdfa16ec5327b024f
natew <natewienert@gmail.com>
2017-10-18 11:24:25 -0700

    :bug: use seemirai

--- apps/electron/src/constants.js
+++ apps/electron/src/constants.js
@@ -1,6 +1,4 @@
 export const IS_PROD = process.env.NODE_ENV === 'production'
-export const APP_URL = IS_PROD
-  ? 'http://app.seemirai.com:3001'
-  : 'http://jot.dev'
+export const APP_URL = 'http://app.seemirai.com:3001'
 export const APP_HOME = '/'
 export const IS_MAC = process.platform === 'darwin'

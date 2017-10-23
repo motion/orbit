9d41632e0dca7fd0c619f8f4f35d456fdd348bfe
Nick Cammarata <cammarata.nick@gmail.com>
2017-10-23 16:11:46 -0700

    remove extra files

--- apps/electron/es5/constants.js
+++
@@ -1,5 +0,0 @@
-export const IS_PROD = process.env.NODE_ENV === 'production';
-export const APP_URL = 'http://app.seemirai.com:3001';
-export const APP_HOME = '/';
-export const IS_MAC = process.platform === 'darwin';
-//# sourceMappingURL=constants.js.map

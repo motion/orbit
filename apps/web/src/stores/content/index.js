6707d1d963d267930a1ccd7026948ff3e6cfe68b
Nick Cammarata <cammarata.nick@gmail.com>
2017-12-11 19:35:56 -0800

    fix urls

--- apps/web/src/stores/content/index.js
+++ apps/web/src/stores/content/index.js
@@ -3,15 +3,36 @@ import rawDocs from './docs'
 
 const tasks = rawTasks.map(title => ({
   title,
+  id: title,
   body: '# ' + title,
   integration: 'github',
 }))
 
 const mdToTitle = md => md.split('\n')[0].slice(2)
 const docs = rawDocs.map(body => ({
+  id: mdToTitle(body),
   title: mdToTitle(body),
   integration: 'docs',
   body,
 }))
 
-export default [...tasks, ...docs]
+const deliverx = [
+  {
+    id: 'deliverx.website',
+    title: 'Late deliveries map',
+    type: 'website',
+    integration: 'website',
+    body: '# Late Deliveries Map',
+    url: 'http://deliverx.dev/map',
+  },
+  {
+    id: 'deliverx.map',
+    title: 'Drivers',
+    body: '# Drivers',
+    type: 'website',
+    integration: 'website',
+    url: 'http://deliverx.dev/drivers',
+  },
+]
+
+export default [...tasks, ...docs, ...deliverx]

bc26103a9e13aa60ce821faefc687c966a6b633b
Nick Cammarata <cammarata.nick@gmail.com>
2017-12-12 18:11:18 -0800

    Improved focus (#114)
    * site specific focuses
    
    * only use titlte as backup to contenteditable

---
+++ apps/electron/src/helpers/focus/index.js
@@ -0,0 +1,96 @@
+export default () => {
+  const first = (xs, test) => {
+    for (const x of xs) {
+      if (test(x)) {
+        return x
+      }
+    }
+    return null
+  }
+
+  const getFavicon = () => {
+    if (window.__oraFavicon) {
+      return window.__oraFavicon
+    }
+    function getDataUri(url, callback) {
+      try {
+        var image = new Image()
+        image.onload = function() {
+          try {
+            var canvas = document.createElement('canvas')
+            canvas.width = this.naturalWidth
+            canvas.height = this.naturalHeight
+            canvas.getContext('2d').drawImage(this, 0, 0)
+            callback(canvas.toDataURL('image/png'))
+          } catch (err) {} /* fail silent */
+        }
+        image.src = url
+      } catch (err) {} /* fail silent */
+    }
+    try {
+      const faviurl = document
+        .querySelector('link[rel~=icon]')
+        .getAttribute('href')
+      getDataUri(faviurl, dataUri => {
+        window.__oraFavicon = dataUri
+      })
+    } catch (err) {} /* fail silently */
+    return ''
+  }
+
+  const contentEditableValue = () =>
+    document.getSelection().toString()
+      ? document.getSelection().toString()
+      : document.getSelection().anchorNode
+        ? document.getSelection().anchorNode.textContent
+        : null
+
+  // for sites where it is common to reply to items
+  const contentEditableOr = fn =>
+    contentEditableValue().length ? contentEditableValue() : fn()
+
+  const rules = [
+    {
+      name: 'Zendesk',
+      regex: /.+zendesk.com.+\/tickets\/.+/,
+      script: () => {
+        return {
+          title: contentEditableOr(
+            () => document.querySelector('input[name=subject]').value
+          ),
+          selection: '',
+        }
+      },
+    },
+    {
+      name: 'Gmail',
+      regex: /.+mail.google.com.+inbox\/.+/,
+      script: () => {
+        return {
+          title: contentEditableOr(
+            () => document.querySelector('h2.hP').innerText
+          ),
+          selection: '',
+        }
+      },
+    },
+  ]
+
+  const url = document.location + ''
+  const rule = first(rules, ({ regex }) => url.match(regex))
+
+  const vals = rule
+    ? rule.script()
+    : {
+        title: document.title,
+        selection: contentEditableValue(),
+      }
+
+  return JSON.stringify(
+    Object.assign({}, vals, {
+      url,
+      crawlerInfo: document.__oraCrawlerAnswer,
+      favicon: getFavicon(),
+    })
+  )
+}

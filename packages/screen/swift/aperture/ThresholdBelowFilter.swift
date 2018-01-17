bb5b2111e4baac729bff24fcb99a099bb9155f0d
Nate Wienert <natewienert@gmail.com>
2018-01-17 13:46:39 -0800

    cleanup character finding (#132)
    * save
    
    * ignore swindler
    
    * :bug: fix lots of connected components and image saving
    
    * :fire: remove unused code
    
    * save
    
    * finish

---
+++ packages/screen/swift/aperture/ThresholdBelowFilter.swift
@@ -0,0 +1,24 @@
+import AppKit
+
+class ThresholdBelowFilter: CIFilter {
+  @objc dynamic var inputImage : CIImage?
+  var threshold: CGFloat = 0.4
+  
+  var colorKernel = CIColorKernel(source:
+    "kernel vec4 color(__sample pixel, float threshold)" +
+      "{" +
+      "    float luma = dot(pixel.rgb, vec3(0.2126, 0.7152, 0.0722));" +
+      "    return (luma < threshold) ? pixel : vec4(1.0, 1.0, 1.0, 1.0);" +
+    "}"
+  )
+  
+  override var outputImage: CIImage!
+  {
+    let inputImage = self.inputImage!
+    let colorKernel = self.colorKernel!
+    let extent = inputImage.extent
+    let arguments = [inputImage, threshold] as [Any]
+    return colorKernel.apply(extent: extent, arguments: arguments)
+  }
+}
+
---
+++ packages/screen/swift/aperture/ThresholdFilter.swift
@@ -0,0 +1,24 @@
+import AppKit
+
+class ThresholdFilter: CIFilter {
+  @objc dynamic var inputImage : CIImage?
+  var threshold: CGFloat = 0.5
+  
+  var colorKernel = CIColorKernel(source:
+    "kernel vec4 color(__sample pixel, float threshold)" +
+      "{" +
+      "    float luma = dot(pixel.rgb, vec3(0.2126, 0.7152, 0.0722));" +
+      //      "    float threshold = smoothstep(inputEdgeO, inputEdge1, luma);" +
+      "    return (luma > threshold) ? vec4(1.0, 1.0, 1.0, 1.0) : vec4(0.0, 0.0, 0.0, 1.0);" +
+    "}"
+  )
+  
+  override var outputImage: CIImage!
+  {
+    let inputImage = self.inputImage!
+    let colorKernel = self.colorKernel!
+    let extent = inputImage.extent
+    let arguments = [inputImage, threshold] as [Any]
+    return colorKernel.apply(extent: extent, arguments: arguments)
+  }
+}

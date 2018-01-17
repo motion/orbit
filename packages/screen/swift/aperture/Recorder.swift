9f23a7e4804c6c0a1e34a0d5c37c54a3c9e9e6ea
natew <natewienert@gmail.com>
2018-01-17 13:05:05 -0800

    save

--- packages/screen/swift/aperture/Recorder.swift
+++ packages/screen/swift/aperture/Recorder.swift
@@ -254,13 +254,15 @@ final class Recorder: NSObject {
       ratio = maxHeight / imageHeight
     }
     // Calculate new size based on the ratio
-    if ratio > 1 {
-      ratio = 1
-    }
+    // this would prevent sizing it up but we want that
+    //    if ratio > 1 {
+    //      ratio = 1
+    //    }
     let mWidth = imageWidth * ratio
     let mHeight = imageHeight * ratio
     guard let colorSpace = image.colorSpace else { return nil }
     guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: image.bitsPerComponent, bytesPerRow: image.bytesPerRow, space: colorSpace, bitmapInfo: image.alphaInfo.rawValue) else { return nil }
+    // white background
     context.clear(CGRect(x: 0, y: 0, width: width, height: height))
     context.setFillColor(CGColor(gray: 1, alpha: 1))
     context.fill(CGRect(x: 0, y: 0, width: width, height: height))

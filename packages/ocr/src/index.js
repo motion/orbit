dbccc4bea3e2ece0d31e2794a82fa6c9a42a1cc8
Nate Wienert <natewienert@gmail.com>
2018-01-22 21:50:42 -0800

    merge ocr + screenchars (#136)
    * working ocr loop test
    
    * save

--- packages/ocr/src/index.js
+++ packages/ocr/src/index.js
@@ -1,115 +1,26 @@
-// @flow
-import NLP from 'google-nlp'
-import cheerio from 'cheerio'
-import { sortBy } from 'lodash'
-import { exec } from 'child_process'
+import execa from 'execa'
+import Fs from 'fs-extra'
 import Path from 'path'
-import promisify from 'sb-promisify'
-// import { screen } from '@mcro/screendump'
 
-type OCROptions = {
-  inputFile?: 'string',
-  takeScreenshot?: boolean,
-  bounds?: [number, number], // width, height
-  offset?: [number, number], // left, top
-}
+const pyDir = Path.join(__dirname, '..', 'python')
+const ocrFilePath = Path.join(pyDir, 'data', 'ocr.csv')
+const ocrScriptPath = Path.join(pyDir, 'src', 'test.py')
 
-//	Google Cloud API key
-const apiKey = 'AIzaSyDl_JoYndPs9gDWzbldcvx0th0E5d2iQu0'
-const nlp = new NLP(apiKey)
-const ocrPath = (...path) => Path.resolve(__dirname, '..', ...path)
-
-const ocrFile = async file => {
-  console.log('ocr file', file)
-  const tesseractHocr = ocrPath('tmp/tesseractOutput')
-  const tess = `TESSDATA_PREFIX=${ocrPath(
-    'tessdata',
-  )} OMP_THREAD_LIMIT=1 tesseract ${file} ${tesseractHocr} --oem 1 -l eng ${ocrPath(
-    'hocr',
-  )}`
-
-  console.time('tesseract')
-  const cmd = `${tess} && cat ${tesseractHocr}.hocr`
-  console.log('running', cmd)
-  const stdout = await promisify(exec)(cmd)
-  console.timeEnd('tesseract')
-
-  console.time('parseWords')
-  const $ = cheerio.load(stdout)
-  const bboxMatch = /bbox ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)/
-  const parseBox = s => {
-    const box = s.match(bboxMatch)
-    if (box && box.length) {
-      const [_, x, y, x1, y1] = box.map(x => parseInt(x, 10))
-      return {
-        x,
-        y,
-        width: x1 - x,
-        height: y1 - y,
-      }
-    }
-    return null
+export default async function(inputFile) {
+  console.log('copy', inputFile, ocrFilePath)
+  try {
+    await Fs.copy(inputFile, ocrFilePath)
+  } catch (err) {
+    throw err
   }
-
-  const texts = []
-  $('span.ocrx_word').each(function() {
-    const text = $(this)
-      .text()
-      .trim()
-    if (text.length === 0) {
-      return
-    }
-    const box = parseBox($(this).attr('title'))
-    if (!box) {
-      return
-    }
-    texts.push({ text, box })
+  const cmd = `python3 ./src/test.py`
+  console.log('$', cmd)
+  const shell = execa.shell(`pwd && ${cmd}`, {
+    cwd: pyDir,
   })
-
-  const text = texts.map(_ => _.text).join(' ')
-
-  const { entities } = await nlp.analyzeEntities(text)
-  if (!entities) {
-    return { text, boxes: [] }
-  }
-  const formattedEntities = sortBy(
-    entities.map(({ name, salience }) => ({ name, weight: salience })),
-  )
-  const boxes = formattedEntities
-    .map(({ name, weight }) => {
-      const fits = texts.filter(({ text }) => name.indexOf(text.trim()) > -1)
-      const box = fits.length > 0 ? fits[0].box : null
-      if (!box) {
-        return null
-      }
-      return {
-        name,
-        weight,
-        box,
-      }
-    })
-    .filter(x => !!x)
-
-  console.timeEnd('parseWords')
-  return { text, boxes }
-}
-
-async function takeScreenshot(options: OCROptions) {
-  console.time('screenshot')
-  const destination = ocrPath('tmp', 'screenshot.png')
-  console.log('tmp screen to', destination)
-  if (options.alt) {
-    await screen(...options.offset, ...options.bounds, destination)
-  } else {
-    await screen({ destination, ...options })
-  }
-  console.timeEnd('screenshot')
-  return destination
-}
-
-export default async function ocr(options: OCROptions) {
-  // if (options.takeScreenshot) {
-  //   options.inputFile = await takeScreenshot(options)
-  // }
-  return await ocrFile(options.inputFile)
+  shell.stdout.setEncoding('utf8')
+  // shell.stdout.on('data', x => console.log(x))
+  // shell.stderr.on('data', x => console.log(x))
+  const answer = await shell
+  return answer.stdout
 }
--- packages/ocr/src/test.js
+++ packages/ocr/src/test.js
@@ -1,17 +1,52 @@
-import ocr from './index'
+import ocr from '.'
+import Screen from '@mcro/screen'
+import execa from 'execa'
 import Path from 'path'
 
-const opts = {
-  inputFile: Path.join(__dirname, '..', 'test-screen.png'),
-}
+process.on('unhandledRejection', function(error, p) {
+  console.log('Screen PromiseFail:')
+  if (error.stack) {
+    console.log(error.message)
+    console.log(error.stack)
+  } else {
+    console.log(error)
+  }
+})
 
-console.log(opts)
+let screenDir = Path.join(__dirname, '..', 'tmp')
 
 async function test() {
-  console.time('rust')
-  const res = await ocr(opts)
-  console.timeEnd('rust')
-  console.log(JSON.stringify(res, 0, 2))
+  const video = new Screen()
+  console.log('start')
+
+  video.startRecording({
+    fps: 2,
+    sampleSpacing: 10,
+    sensitivity: 2,
+    showCursor: true,
+    boxes: [
+      {
+        id: 'screen',
+        x: 0,
+        y: 24,
+        width: 1166,
+        height: 980,
+        screenDir,
+        initialScreenshot: true,
+        findContent: true,
+      },
+    ],
+  })
+
+  video.onChangedFrame(async data => {
+    console.log('Wrote characters, do ocr', data)
+    console.log(await ocr(Path.join(screenDir, 'characters.txt')))
+    process.exit(0)
+  })
 }
 
-test()
+try {
+  test()
+} catch (err) {
+  console.log('error', err)
+}

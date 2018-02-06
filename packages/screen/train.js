81f3b1859b1f0cad293236e15120cfe0b25eb123
natew <natewienert@gmail.com>
2018-02-05 17:46:16 -0800

    start on train script

---
+++ packages/screen/train.js
@@ -0,0 +1,140 @@
+const Screen = require('./es6').default
+const Fs = require('fs')
+const Path = require('path')
+const execa = require('execa')
+const express = require('express')
+
+const tmpDir = Path.join(__dirname, 'tmp')
+const trainDir = Path.join(__dirname, 'train')
+const sleep = ms => new Promise(res => setTimeout(res, ms))
+
+const server = express()
+server.get('/:font', (req, res) => {
+  console.log('got req', req.params)
+  res.send(getPage(req.params.font))
+})
+server.listen(3003)
+
+function getPage(fontName) {
+  console.log('getting', fontName)
+  return `
+<html>
+  <head>
+    <style>
+      body {
+        padding: 20px;
+        font-family: ${fontName};
+        font-size: 28px;
+        letter-spacing: 2px;
+      }
+    </style>
+  </head>
+  <body>
+    <p>
+      The quick brown fox*
+    </p>
+    <p>
+      jumped over the lazy dog.
+    </p>
+    <p>
+      Cwm fjord BANK GLyphs Vext Quiz!
+    </p>
+    <p>
+      pack my box with? #!@$*=+[]"';:
+    </p>
+    <p>
+      PACK MY BOX WITH? #!@$*=+[]"';:
+    </p>
+    <p>
+      (five dozen liquor jugs) #!@$*=+[]"';:
+    </p>
+    <p>
+      Jackdaws' love,
+    </p>
+    <p>
+      JACKDAWS' LOVE,
+    </p>
+    <p>
+      my big sphinx of quartz
+    </p>
+    <p>
+      MY BIG SPHINX of quartz
+    </p>
+  </body>
+</html>
+  `
+}
+
+const screen = new Screen({ debugBuild: false })
+
+async function train() {
+  // reset train dir
+  if (Fs.existsSync(trainDir)) {
+    await execa('rm', ['-r', trainDir])
+  }
+  await execa('mkdir', [trainDir])
+
+  await screen.start()
+
+  const fonts = ['arial', 'georgia']
+
+  while (fonts.length) {
+    const font = fonts.pop()
+    await execa(`open`, [`http://localhost:3003/${font}`])
+    await sleep(400)
+    await new Promise(resolve => {
+      screen.onWords(async data => {
+        console.log('finished ocr', data.length)
+        screen.pause()
+        await sleep(350) // wait for fs to write all files
+        const fontDir = Path.join(trainDir, font)
+        await execa('mkdir', [fontDir])
+        await execa('cp', ['-r', Path.join(tmpDir, '.'), fontDir])
+        resolve()
+      })
+      screen.resume()
+      screen.watchBounds({
+        fps: 1,
+        sampleSpacing: 1,
+        sensitivity: 1,
+        showCursor: false,
+        debug: true,
+        boxes: [
+          {
+            id: 0,
+            x: 0,
+            y: 24,
+            width: 800,
+            height: 1200,
+            initialScreenshot: true,
+            findContent: true,
+            screenDir: tmpDir,
+          },
+        ],
+      })
+    })
+  }
+}
+
+try {
+  train()
+} catch (err) {
+  console.log('error', err)
+}
+
+process.on('unhandledRejection', function(error, p) {
+  console.log('OCR PromiseFail:')
+  if (error.stack) {
+    console.log(error.message)
+    console.log(error.stack)
+  } else {
+    console.log(error)
+  }
+})
+
+process.on('SIGINT', async () => {
+  console.log('stopping screen')
+  await screen.stop()
+  console.log('stoped')
+  process.exit(0)
+})

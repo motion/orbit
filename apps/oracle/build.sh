c4b2befb3699193b9642bb0138a995d3ea79205d
natew <natewienert@gmail.com>
2018-02-15 14:12:32 -0800

    save

--- apps/oracle/build.sh
+++ apps/oracle/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+(cd swindler && carthage update --platform mac)
+
 cd orbit
+carthage update --platform mac
 xcodebuild -configuration Release -derivedDataPath $(mktemp -d) -scheme orbit
-touch ../apps/api/src/index.js
+touch ../apps/desktop/src/index.js

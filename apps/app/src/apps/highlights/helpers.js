51c1d1adf571dd6e73b8a69d989ad5c96f73017a
natew <natewienert@gmail.com>
2018-02-17 09:32:18 -0800

    save

--- apps/app/src/apps/highlights/helpers.js
+++ apps/app/src/apps/highlights/helpers.js
@@ -2,3 +2,14 @@ export const HL_PAD = 2
 export const TOP_BAR_PAD = 22
 export const LINE_Y_ADJ = -5
 export const getKey = ([x, y, w, h]) => `${x}${y}${w}${h}`
+
+export function toTarget(quadTreeItem) {
+  if (!quadTreeItem) return null
+  return {
+    left: quadTreeItem.x,
+    top: quadTreeItem.y,
+    width: quadTreeItem.w,
+    height: quadTreeItem.h,
+    key: quadTreeItem.string,
+  }
+}

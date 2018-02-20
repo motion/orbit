f636ca3f113283925861a151187d68234b6c376c
natew <natewienert@gmail.com>
2018-02-20 13:28:45 -0800

    save

--- apps/dev/start-apps.sh
+++ apps/dev/start-apps.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-tmux new-session -d -s orbit 'build --watch'
-tmux split-window -h 'npm run start-desktop'
+export TERM=xterm-256color
+
+tmux source-file ./tmux.conf
+
+tmux new-session -d -s orbit 'npm run start-desktop'
+tmux split-window -h 'cd ../.. && build --watch'
 tmux split-window -v 'npm run start-app'
 tmux split-window -h 'npm run start-electron'
 tmux split-window -h 'sleep 3 && npm run start-debug'

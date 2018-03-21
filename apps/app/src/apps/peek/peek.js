3329cc70c88165dc0d7a5c0afabe6df7c2e5415b
natew <natewienert@gmail.com>
2018-03-21 15:58:09 -0700

    some body design

--- apps/app/src/apps/peek/peek.js
+++ apps/app/src/apps/peek/peek.js
@@ -1,70 +1,47 @@
-// @flow
 import * as React from 'react'
 import { view, watch } from '@mcro/black'
 import * as UI from '@mcro/ui'
 import { App, Electron } from '@mcro/all'
 import PeekHeader from './peekHeader'
 import PeekContents from './peekContents'
+import { Thing } from '@mcro/models'
 
-// const keyParam = (window.location.search || '').match(/key=(.*)/)
-// const KEY = keyParam && keyParam[1]
 const SHADOW_PAD = 15
 const background = '#fff'
 const peekShadow = [
   [0, 0, SHADOW_PAD, [0, 0, 0, 0.2]],
   [0, 0, 0, 1, [0, 0, 0, 0.05]],
 ]
-// const log = debug('peek')
 const borderRadius = 8
 
-@view
+@view({
+  peek: class PeekStore {
+    @watch
+    peekItem = () =>
+      Thing.get({ bucket: 'pg', title: 'General and Surprising' })
+  },
+})
 export default class PeekPage {
-  render() {
-    const { orbit } = Electron
-    const arrowTowards = (orbit && orbit.arrowTowards) || 'right'
-    const arrowSize = 24
-    let arrowStyle
-    let peekStyle
-    switch (arrowTowards) {
-      case 'right':
-        peekStyle = {
-          // marginRight: -4,
-        }
-        arrowStyle = {
-          top: 53,
-          right: SHADOW_PAD - arrowSize,
-        }
-        break
-      case 'left':
-        peekStyle = {
-          // marginLeft: -2,
-        }
-        arrowStyle = {
-          top: 53,
-          left: -3,
-        }
-        break
-    }
-    if (!Electron.currentPeek) {
-      console.log('no peek')
+  render({ peek }) {
+    const { currentPeek, onLeft } = Electron
+    if (!currentPeek) {
       return null
     }
+    let peekStyle = {}
     if (Electron.orbitState.fullScreen) {
       peekStyle.paddingLeft = 0
     }
-    const towardsRight = Electron.currentPeek.arrowTowards === 'left'
     return (
       <UI.Theme name="light">
         <peek css={peekStyle} $peekVisible={App.isShowingPeek}>
           <content
             css={{
-              borderRightRadius: !towardsRight ? 0 : borderRadius,
-              borderLeftRadius: towardsRight ? 0 : borderRadius,
+              borderRightRadius: !onLeft ? 0 : borderRadius,
+              borderLeftRadius: onLeft ? 0 : borderRadius,
             }}
           >
-            <PeekHeader />
-            {arrowTowards}
-            <PeekContents if={Electron.currentPeek} />
+            <PeekHeader peek={peek} />
+            <PeekContents if={Electron.currentPeek} peek={peek} />
           </content>
         </peek>
       </UI.Theme>

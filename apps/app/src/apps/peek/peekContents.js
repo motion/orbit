3a3722738df4e2b5b40ef62de1f5bc4fb9912652
Nate Wienert <natewienert@gmail.com>
2018-03-21 20:10:10 -0700

    fix automagic bug for returned values (#190)

--- apps/app/src/apps/peek/peekContents.js
+++ apps/app/src/apps/peek/peekContents.js
@@ -27,23 +27,25 @@ const EmptyContents = ({ item }) => (
   </pane>
 )
 
-@view({
-  store: class PeekContentsStore {
-    // this.selectedPath
-    //   ? r2.get(`/contents${this.selectedPath}`).json.then(x => x.file)
-    //   : null
+// ({
+//   store: class PeekContentsStore {
+//     // this.selectedPath
+//     //   ? r2.get(`/contents${this.selectedPath}`).json.then(x => x.file)
+//     //   : null
 
-    get selectedPath() {
-      const selected = App.state.selectedItem
-      if (!selected) return null
-      if (!selected.id || (selected.id && selected.id.indexOf('.txt') === -1)) {
-        return null
-      }
-      console.log('selected.id', selected.id)
-      return selected.id
-    }
-  },
-})
+//     get selectedPath() {
+//       const selected = App.state.selectedItem
+//       if (!selected) return null
+//       if (!selected.id || (selected.id && selected.id.indexOf('.txt') === -1)) {
+//         return null
+//       }
+//       console.log('selected.id', selected.id)
+//       return selected.id
+//     }
+//   },
+// })
+
+@view
 export default class PeekContents {
   render({ peek: { peekItem } }) {
     const { selectedItem } = App.state

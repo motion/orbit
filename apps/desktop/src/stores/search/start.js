3108eaa651479dcdb4313fb59d171390483e1186
Nick Cammarata <cammarata.nick@gmail.com>
2018-03-22 16:03:46 -0700

    Search db (#195)
    * initial work
    
    * merge back in data
    
    * move to reactions
    
    * remove migrations
    
    * run python in run desktop
    
    * starts python properly now

---
+++ apps/desktop/src/stores/search/start.js
@@ -0,0 +1,40 @@
+import execa from 'execa'
+import { flatten } from 'lodash'
+import path from 'path'
+import { readFileSync } from 'fs'
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const PYTHON_API = 'http://localhost:5000'
+const startPython = async () => {
+  const cwd = path.resolve(__dirname, '..', '..', '..', './python')
+  console.log('cwd is', cwd)
+  const script = execa(`python3`, [`run_embedding.py`], { cwd })
+
+  let tries = 0
+  while (true) {
+    await sleep(450)
+    try {
+      const res = await fetch(PYTHON_API)
+      if (res.status === 200) {
+        return true
+      }
+    } catch (err) {}
+    tries += 1
+    console.log('connecting to python, try', tries)
+  }
+}
+
+const readData = file => {
+  const filePath = path.resolve(__dirname, `../../../data/${file}`)
+  return JSON.parse(readFileSync(filePath, 'utf8'))
+}
+
+export default async () => {
+  await startPython()
+  const dataset = flatten(
+    ['datasets/books.json', 'datasets/pg.json'].map(readData),
+  )
+
+  return dataset
+}
--- apps/search/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npx kill-port 3003
-until ../../node_modules/.bin/parcel -p 3003 src/index.js; do
-  echo "SearchError: parcel crash, restarting..." >&2
-  sleep 1
-done

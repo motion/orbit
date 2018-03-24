ab3a5327931b0f1768e02689bb72d17cf79cd431
natew <natewienert@gmail.com>
2018-03-23 17:18:37 -0700

    fix bugs in keynav

--- apps/app/src/apps/orbit/orbitStore.js
+++ apps/app/src/apps/orbit/orbitStore.js
@@ -1,42 +1,38 @@
 import { react } from '@mcro/black'
-import { App } from '@mcro/all'
+import { App, Desktop } from '@mcro/all'
 import KeyboardStore from './keyboardStore'
-
-// const log = debug('OrbitStore')
 
 export default class OrbitStore {
   keyboardStore = new KeyboardStore()
-  searchResults = []
   showSettings = false
-  indexingStatus = ''
   selectedIndex = 0
 
   @react({ delay: 64 })
   setAppSelectedIndex = [() => this.selectedIndex, App.setSelectedIndex]
 
   willMount() {
-    // start app reactions
     App.runReactions()
+    this.on(this.keyboardStore, 'keydown', this.handleKeyDown)
+  }
 
-    this.on(this.keyboardStore, 'keydown', code => {
-      switch (code) {
-        case 40: // down
-          this.selectedIndex = Math.min(
-            this.results.length - 1,
-            this.selectedIndex + 1,
-          )
-          return
-        case 38: //up
-          this.selectedIndex = Math.max(0, this.selectedIndex - 1)
-          return
-        case 13: //enter
-          App.setOpenResult(this.results[this.selectedIndex])
-          // so hitting enter on a previous app works
-          setTimeout(() => {
-            App.setOpenResult(null)
-          }, 100)
-      }
-    })
+  handleKeyDown = code => {
+    switch (code) {
+      case 40: // down
+        this.selectedIndex = Math.min(
+          Desktop.results.length - 1,
+          this.selectedIndex + 1,
+        )
+        return
+      case 38: //up
+        this.selectedIndex = Math.max(0, this.selectedIndex - 1)
+        return
+      case 13: //enter
+        App.setOpenResult(Desktop.results[this.selectedIndex])
+        // so hitting enter on a previous app works
+        setTimeout(() => {
+          App.setOpenResult(null)
+        }, 100)
+    }
   }
 
   onChangeQuery = e => {

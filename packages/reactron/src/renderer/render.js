5375bbd75d5ae423fdaf9501f8fb336998fb28b2
natew <natewienert@gmail.com>
2018-03-29 18:17:35 -0700

    revert reac tupdate

--- packages/reactron/src/renderer/render.js
+++ packages/reactron/src/renderer/render.js
@@ -5,9 +5,11 @@ import { validateElement } from '../utils/renderUtils'
 export async function render(element) {
   const container = createElement('ROOT')
   validateElement(element)
-  await new Promise((resolve, reject) => {
-    container.app.once('ready', resolve, reject)
-  })
+  console.log('start', container.app)
+  // await new Promise((resolve, reject) => {
+  //   container.app.once('ready', resolve, reject)
+  // })
   const node = Renderer.createContainer(container)
+  console.log('rendersadsa')
   Renderer.updateContainer(element, node, null)
 }

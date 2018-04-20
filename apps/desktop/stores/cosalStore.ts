fc560c8ff13ac8086cf54250e4b68c9da862a34e
Nick Cammarata <cammarata.nick@gmail.com>
2018-04-19 17:55:44 -0700

    better indexing

--- apps/desktop/stores/cosalStore.ts
+++ apps/desktop/stores/cosalStore.ts
@@ -9,19 +9,38 @@ export default class CosalStore {
   query = ''
   loading = true
   cosal = new Cosal()
+  lastIndex = 0
+
+  bitToDoc = ({ id, title, createdAt }) => ({
+    id,
+    fields: [{ weight: 1, content: title }],
+    createdAt: createdAt,
+  })
 
   async willMount() {
-    const bits: any = uniqBy(await Bit.find({ take: 1500 }), 'title')
+    const startingHistory = 3000
+
+    const bits: any = uniqBy(await Bit.find({ take: startingHistory }), 'id')
+    this.lastIndex = +Date.now()
     await this.cosal.warm()
-    await this.cosal.addDocuments(
-      bits.map(({ id, title }) => ({
-        id,
-        fields: [{ weight: 1, content: title }],
-        createdAt: +Date.now(),
-      })),
-    )
+    const start = +Date.now()
+    await this.cosal.addDocuments(bits.map(this.bitToDoc))
     this.cosal.docsVersion += 1
+    // @ts-ignore
+    this.setTimeout(this.updateSince, 10000)
     this.loading = false
+  }
+
+  updateSince = async () => {
+    const bitsSince: any = await Bit.createQueryBuilder()
+      .where(`createdAt > '${new Date(this.lastIndex).toISOString()}'`)
+      .getMany()
+    const bits: any = uniqBy(bitsSince, 'id')
+    await this.cosal.addDocuments(bits.map(this.bitToDoc))
+    this.cosal.docsVersion += 1
+    this.lastIndex = +Date.now()
+    // @ts-ignore
+    this.setTimeout(this.updateSince)
   }
 
   // @ts-ignore

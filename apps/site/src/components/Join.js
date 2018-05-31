5dc27e09f00c7555d9025f965cd33c9a4536dd55
natew <natewienert@gmail.com>
2018-05-31 12:24:53 -0700

    save

--- apps/site/src/components/Join.js
+++ apps/site/src/components/Join.js
@@ -2,11 +2,38 @@ import * as React from 'react'
 import { view } from '@mcro/black'
 import { P, P2, Callout } from '~/views'
 import * as UI from '@mcro/ui'
-import { BrandLogo } from '~/components'
+import * as r2 from '@mcro/r2'
 
 @view
 export class Join extends React.Component {
-  render({ ...props }) {
+  state = {
+    error: null,
+    success: null,
+  }
+
+  form = React.createRef()
+
+  submit = async e => {
+    e.preventDefault()
+    try {
+      this.setState({ error: null, success: null })
+      const form = this.form.current
+      const json = new URLSearchParams(new FormData(form)).toString()
+      const { result, msg } = await r2.post(form.getAttribute('action'), {
+        json,
+        mode: 'no-cors',
+      }).json
+      if (result === 'error') {
+        throw msg
+      } else {
+        this.setState({ error: null, success: `${msg}` })
+      }
+    } catch (error) {
+      this.setState({ error: 'Error submitting, did you enter an email?' })
+    }
+  }
+
+  render({ ...props }, { success, error }) {
     return (
       <section id="join" {...props}>
         <Callout>
@@ -18,12 +45,14 @@ export class Join extends React.Component {
             first, along with interesting development updates.
           </P2>
           <form
-            action="https://tryorbit.us18.list-manage.com/subscribe/post?u=019909d3efb283014d35674e5&amp;id=015e5a3442"
+            ref={this.form}
+            action="https://tryorbit.us18.list-manage.com/subscribe/post-json?u=019909d3efb283014d35674e5&amp;id=015e5a3442"
             method="post"
             id="mc-embedded-subscribe-form"
             name="mc-embedded-subscribe-form"
             target="_blank"
             noValidate
+            onSubmit={this.submit}
           >
             <input
               type="email"
@@ -39,16 +68,25 @@ export class Join extends React.Component {
                 value=""
               />
             </div>
-            <UI.Theme theme="rgb(10.8%, 34.7%, 81.2%)">
-              <UI.Button
-                size={1.1}
-                sizeRadius={3}
-                margin={[0, 0, 0, 'auto']}
-                type="submit"
+            <end $$row $$align="center">
+              <message
+                if={success || error}
+                $success={success && !error}
+                css={{ maxWidth: '70%' }}
               >
-                Signup
-              </UI.Button>
-            </UI.Theme>
+                {success || error || ''}
+              </message>
+              <UI.Theme theme="rgb(10.8%, 34.7%, 81.2%)">
+                <UI.Button
+                  size={1.1}
+                  sizeRadius={3}
+                  margin={[0, 0, 0, 'auto']}
+                  type="submit"
+                >
+                  Signup
+                </UI.Button>
+              </UI.Theme>
+            </end>
           </form>
         </Callout>
       </section>
@@ -72,6 +110,13 @@ export class Join extends React.Component {
       margin: [10, 0, 20],
       fontSize: 18,
       border: [1, 'red'],
+    },
+    message: {
+      paddingRight: 40,
+      color: 'darkred',
+    },
+    success: {
+      color: 'darkgreen',
     },
   }
 

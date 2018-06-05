2d66e0b78da0ca9b8718f9058288fa836bd10e04
natew <natewienert@gmail.com>
2018-06-04 17:58:17 -0700

    save

--- apps/site/surge-fix.sh
+++ apps/site/surge-fix.sh
@@ -3,4 +3,7 @@
 cp dist/index.html dist/about.html
 cp dist/index.html dist/features.html
 cp dist/index.html dist/use-cases.html
+cp dist/index.html dist/about.html
+cp dist/index.html dist/privacy.html
+cp dist/index.html dist/terms.html
 echo '*' > dist/CORS

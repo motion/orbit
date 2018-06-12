63cbf77fc3ee1fc7f523d77b682d047fa9445337
natew <natewienert@gmail.com>
2018-06-11 18:00:06 -0700

    save

--- apps/app/src/apps/orbit/orbitSettings.js
+++ apps/app/src/apps/orbit/orbitSettings.js
@@ -1,8 +1,17 @@
 import { view, react } from '@mcro/black'
 import { partition } from 'lodash'
 import { OrbitSettingCard } from './orbitSettingCard'
+import { OrbitCard } from './orbitCard'
 import { OrbitDockedPane } from './orbitDockedPane'
 import { SubTitle } from '~/views'
+import * as UI from '@mcro/ui'
+
+const CheckBoxRow = ({ children, checked }) => (
+  <row css={{ flexFlow: 'row', padding: [8, 0], alignItems: 'center' }}>
+    <input checked={checked} css={{ margin: ['auto', 4] }} type="checkbox" />{' '}
+    <label css={{ padding: [0, 4], fontWeight: 400 }}>{children}</label>
+  </row>
+)
 
 class OrbitSettingsStore {
   isPaneActive = false
@@ -99,6 +108,20 @@ export class OrbitSettings {
     )
     return (
       <OrbitDockedPane name={name}>
+        <SubTitle>Settings</SubTitle>
+        <OrbitCard>
+          <UI.Text css={{ marginBottom: 10 }}>
+            You've added {activeIntegrations.length} integration{activeIntegrations.length ===
+            '1'
+              ? ''
+              : 's'}.{' '}
+            {activeIntegrations.length === 0
+              ? 'Add some integrations below to get started with Orbit.'
+              : ''}
+          </UI.Text>
+          <CheckBoxRow checked>Start on Login</CheckBoxRow>
+          <CheckBoxRow>Automatically manage disk space</CheckBoxRow>
+        </OrbitCard>
         <section if={activeIntegrations.length}>
           <SubTitle>Integrations</SubTitle>
           <cards>

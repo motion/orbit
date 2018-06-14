85d4233f87b2b9f343685c39975e0886027025b7
natew <natewienert@gmail.com>
2018-06-14 10:36:57 -0700

    save

--- apps/desktop/sync/syncers/google/googleMailSync.ts
+++ apps/desktop/sync/syncers/google/googleMailSync.ts
@@ -48,7 +48,7 @@ export default class GoogleMailSync {
   updateSetting = async (setting?) => {
     this.setting = setting || (await Setting.findOne({ type: 'gmail' }))
     this.service = new DriveService(this.setting)
-    this.gmail = new Gmail(setting.token),
+    this.gmail = new Gmail(setting.token)
   }
 
   run = async () => {

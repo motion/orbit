8ed79924290b9be0e832f49668191de20311dbfe
natew <natewienert@gmail.com>
2018-06-15 09:26:51 -0700

    save

--- apps/site/src/pages/HomePage.js
+++ apps/site/src/pages/HomePage.js
@@ -89,11 +89,10 @@ class HomeStore {
 const Pitch = ({ isLarge }) => (
   <>
     <Title italic size={3.2} margin={[0, 0, 15, 0]} alpha={1} color="#222">
-      A better way to coordinate teams
+      A better way to organize knowledge
     </Title>
     <P size={1.35} sizeLineHeight={1.2} fontWeight={300}>
-      Unify your team knowledge to make it useful and easy to find. A completely
-      secure, no 😅 intranet that takes just seconds to setup.
+      Coming soon, a smarter, faster more flexible way to unify your knowledge.
     </P>
     <homeJoin css={{ margin: [20, -15, -30] }}>
       <Join />

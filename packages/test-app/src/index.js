054679b9f580b9cdcf58d9c103169d2a3683a56d
Nate Wienert <natewienert@gmail.com>
2018-06-27 00:19:08 -0700

    Nate/dev (#283)
    * nucleo in test
    
    * save
    
    * save

--- packages/test-app/src/index.js
+++ packages/test-app/src/index.js
@@ -1,10 +1,11 @@
+import '~/../public/styles/nucleo.css'
 import '~/../public/testBase.css'
 import './createElement'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Themes from '~/themes'
 import * as UI from '@mcro/ui'
-import Root from './root'
+import { Root } from './root'
 
 export function render() {
   const RootNode = document.querySelector('#app')

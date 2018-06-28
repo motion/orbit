9ef4945b2dc70e7a347685da220111abed8a2d0d
jongleberry <me@jongleberry.com>
2018-06-27 23:02:13 -0700

    apps/all fix

--- packages/css/_/test.js
+++ packages/css/_/test.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const css_1 = __importDefault(require("./css"));
 const styler = css_1.default();
-console.log(styler({
-    color: 'red',
-    alpha: 0.5,
-    background: [0, 0, 0, 0.5],
-    border: [1, 'red'],
-    boxShadow: [0, 0, 0, [0, 0, 0, 0.5]],
-}));
+test('styler runs', () => {
+    styler({
+        color: 'red',
+        alpha: 0.5,
+        background: [0, 0, 0, 0.5],
+        border: [1, 'red'],
+        boxShadow: [0, 0, 0, [0, 0, 0, 0.5]],
+    });
+});
 //# sourceMappingURL=test.js.map

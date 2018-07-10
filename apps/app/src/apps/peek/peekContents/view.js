64b78d608edad4e11fb7816934dcf60c90f8c417
natew <natewienert@gmail.com>
2018-07-09 21:43:59 -0700

    really bugfix

--- apps/app/src/apps/peek/peekContents/view.js
+++ apps/app/src/apps/peek/peekContents/view.js
@@ -11,11 +11,11 @@ const views = {
 @view
 export class View extends React.Component {
   render({ bit, children }) {
-    const View = views[bit.id]
-    if (!View || !bit) {
-      console.error('no view or bit', bit.toJS(), View)
+    if (!bit || !views[bit.id]) {
+      console.error('no view or bit', bit.toJS(), views)
       return children({})
     }
+    const View = views[bit.id]
     return children({
       title: bit.title,
       content: <View bit={bit} />,

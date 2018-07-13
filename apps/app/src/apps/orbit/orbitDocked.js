aea328af356a657bff49850cf33b752dacf3136f
natew <natewienert@gmail.com>
2018-07-12 21:45:18 -0700

    some fixes

--- apps/app/src/apps/orbit/orbitDocked.js
+++ apps/app/src/apps/orbit/orbitDocked.js
@@ -8,11 +8,62 @@ import { OrbitHeader } from './orbitHeader'
 import { OrbitSearchResults } from './orbitSearchResults'
 import { OrbitDirectory } from './orbitDirectory'
 import { App } from '@mcro/stores'
-import { OrbitDockedPaneStore } from './orbitDockedPaneStore'
+import { OrbitDockedPaneStore } from './OrbitDockedPaneStore'
 
 const borderRadius = 14
 const SHADOW_PAD = 120
 const DOCKED_SHADOW = [0, 0, SHADOW_PAD, [0, 0, 0, 0.55]]
+
+const Frame = view(UI.Col, {
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  bottom: 100,
+  borderRadius,
+  zIndex: 2,
+  flex: 1,
+  pointerEvents: 'none',
+  width: App.dockedWidth,
+  opacity: 0,
+  transform: {
+    x: 6,
+  },
+})
+
+Frame.theme = ({ theme, visible, willAnimate }) => ({
+  background: theme.base.background,
+  ...(visible && {
+    pointerEvents: 'auto',
+    opacity: 1,
+    transform: {
+      x: 0,
+    },
+  }),
+  ...(willAnimate && {
+    willChange: 'transform, opacity',
+    transition: `
+      transform ease-in ${App.animationDuration}ms,
+      opacity ease-in ${App.animationDuration}ms
+    `,
+  }),
+})
+
+const Border = view(UI.FullScreen, {
+  zIndex: Number.MAX_SAFE_INTEGER,
+  pointerEvents: 'none',
+})
+
+Border.theme = ({ theme }) => {
+  const borderColor = theme.base.background.darken(0.25).desaturate(0.6)
+  const borderShadow = ['inset', 0, 0, 0, 0.5, borderColor]
+  const borderGlow = ['inset', 0, 0, 0, 1, [255, 255, 255, 0.5]]
+  return {
+    border: {
+      borderRadius: borderRadius,
+      boxShadow: [borderShadow, borderGlow, DOCKED_SHADOW],
+    },
+  }
+}
 
 @attachTheme
 @view.attach('appStore', 'orbitStore')
@@ -23,14 +74,14 @@ const DOCKED_SHADOW = [0, 0, SHADOW_PAD, [0, 0, 0, 0.55]]
 class OrbitDockedInner extends React.Component {
   render({ paneStore, appStore, theme }) {
     const { animationState } = paneStore
-    // log('DOCKED ------------', App.orbitState.docked)
+    log('DOCKED ------------', App.orbitState.docked)
     return (
       <>
-        <frame
-          $visible={animationState.visible}
-          $willAnimate={animationState.willAnimate}
+        <Frame
+          visible={animationState.visible}
+          willAnimate={animationState.willAnimate}
         >
-          <border $$fullscreen />
+          <Border fullscreen />
           <container>
             <OrbitHeader
               borderRadius={borderRadius}
@@ -59,65 +110,16 @@ class OrbitDockedInner extends React.Component {
               </orbitRelativeInner>
             </orbitInner>
           </container>
-        </frame>
+        </Frame>
       </>
     )
   }
 
-  static theme = ({ theme }) => {
-    const background = theme.base.background
-    const borderColor = theme.base.background.darken(0.25).desaturate(0.6)
-    const borderShadow = ['inset', 0, 0, 0, 0.5, borderColor]
-    const borderGlow = ['inset', 0, 0, 0, 1, [255, 255, 255, 0.5]]
-    return {
-      frame: {
-        background,
-      },
-      border: {
-        borderRadius: borderRadius,
-        boxShadow: [borderShadow, borderGlow, DOCKED_SHADOW],
-      },
-    }
-  }
-
   static style = {
-    frame: {
-      position: 'absolute',
-      top: 10,
-      right: 10,
-      bottom: 100,
-      borderRadius,
-      zIndex: 2,
-      flex: 1,
-      pointerEvents: 'none',
-      width: App.dockedWidth,
-      opacity: 0,
-      transform: {
-        x: 6,
-      },
-    },
     container: {
       borderRadius: borderRadius + 1,
       // overflow: 'hidden',
       flex: 1,
-    },
-    border: {
-      zIndex: Number.MAX_SAFE_INTEGER,
-      pointerEvents: 'none',
-    },
-    willAnimate: {
-      willChange: 'transform, opacity',
-      transition: `
-        transform ease-in ${App.animationDuration}ms,
-        opacity ease-in ${App.animationDuration}ms
-      `,
-    },
-    visible: {
-      pointerEvents: 'auto',
-      opacity: 1,
-      transform: {
-        x: 0,
-      },
     },
     // having this have -20 margin on sides
     // means we have nice shadows on inner content

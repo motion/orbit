2c0dea8b506e6023dced61e0647f1aec535b74c9
natew <natewienert@gmail.com>
2018-07-17 14:26:22 -0700

    date range

--- apps/app/src/apps/orbit/OrbitSearchFilters.tsx
+++ apps/app/src/apps/orbit/OrbitSearchFilters.tsx
@@ -1,12 +1,25 @@
+import 'react-date-range/dist/styles.css' // main style file
+import '../../../public/styles/calendar.css' // theme css file
 import * as React from 'react'
 import { view, compose } from '@mcro/black'
 import * as UI from '@mcro/ui'
 import { RoundButton } from '../../views'
 import { OrbitIcon } from './OrbitIcon'
+import { DateRangePicker } from 'react-date-range'
 
 const Select = view('select', {})
 
 class SearchFilterStore {
+  dateState = {
+    ranges: [
+      {
+        startDate: Date.now(),
+        endDate: Date.now(),
+        key: 'selection',
+      },
+    ],
+  }
+
   get filters() {
     const { settingsList } = this.props.integrationSettingsStore
     if (!settingsList) {
@@ -15,6 +28,13 @@ class SearchFilterStore {
     return settingsList
       .filter(x => x.type !== 'setting')
       .map(setting => ({ icon: setting.type }))
+  }
+
+  onChangeDate = ranges => {
+    this.dateState = {
+      ranges: [ranges.selection],
+    }
+    // this.dateState = ranges
   }
 }
 
@@ -36,13 +56,9 @@ const x = ({ store, ...props }) => {
   return (
     <SearchFilters width="100%" alignItems="center">
       {/* <UI.Icon name="ui-2_filter" size={12} opacity={0.6} marginRight={12} /> */}
-      <Select>
-        <option>Last month</option>
-      </Select>
-      <div style={{ width: 10 }} />
-      <Select>
-        <option>Everyone</option>
-      </Select>
+      <div className="calendar-dom">
+        <DateRangePicker onChange={store.onChangeDate} {...store.dateState} />
+      </div>
       <UI.Col flex={1} />
       {store.filters.map((filter, i) => {
         return (

bd9c6fc0392f8b2115e3f4f316b98195c703c358
natew <natewienert@gmail.com>
2018-07-21 08:51:37 -0700

    get working pyocr

--- apps/oracle/build.sh
+++ apps/oracle/build.sh
@@ -10,6 +10,16 @@ if [ "$1" = "--if-empty" ]; then
   fi
 fi
 
+. $(dirname $0)/../../bin/__/common
+
+# have to install with shared cython library
+ensure-dep "pyenv" -- env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -v 3.6.1
+
+# symlink from multicore shared lib to non
+# i needed this but not sure if py2app is bundling this properly later or just using shared lib?
+ln -s ~/.pyenv/versions/3.6.1/lib/libpython3.6m.dylib ~/.pyenv/versions/3.6.1/lib/libpython3.6.dylib
+ln -s ~/.pyenv/versions/3.6.1/lib/libpython3.6m.a ~/.pyenv/versions/3.6.1/lib/libpython3.6.a
+
 pip3 install -U pyobjc
 (cd swindler && carthage update --platform mac)
 (cd pyocr && ./build.sh)

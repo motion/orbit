01519acf9af22e029f6f605a72719c3887e4275f
natew <natewienert@gmail.com>
2018-08-02 18:10:42 -0700

    fixes

--- apps/app/src/apps/peek/views/PeekRelated.tsx
+++ apps/app/src/apps/peek/views/PeekRelated.tsx
@@ -14,10 +14,17 @@ export const PeekRelated = decorator(({ relatedStore, ...props }) => (
     <Carousel
       items={relatedStore.relatedBits}
       cardWidth={180}
-      cardHeight={78}
+      cardHeight={62}
+      cardSpace={5}
+      verticalPadding={5}
       cardProps={{
+        titleProps: {
+          size: 1,
+        },
+        padding: 10,
         hide: {
           subtitle: true,
+          body: true,
         },
       }}
       {...props}

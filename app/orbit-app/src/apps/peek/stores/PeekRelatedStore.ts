2ab0b6fe5f1460d66a238f43a0944c6d8689a739
natew <natewienert@gmail.com>
2018-08-04 23:31:37 -0700

    perf for now

--- app/orbit-app/src/apps/peek/stores/PeekRelatedStore.ts
+++ app/orbit-app/src/apps/peek/stores/PeekRelatedStore.ts
@@ -1,9 +1,13 @@
-import { react } from '@mcro/black'
 import { BitRepository } from '../../../repositories'
 
 export class PeekRelatedStore {
-  relatedBits = react(
-    () => BitRepository.find({ take: 6, skip: 2, relations: ['people'] }),
-    { defaultValue: [], delay: 250, debug: true },
-  )
+  relatedBits = null
+
+  async didMount() {
+    this.relatedBits = await BitRepository.find({
+      take: 6,
+      skip: 2,
+      relations: ['people'],
+    })
+  }
 }

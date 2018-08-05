3cad2bb144622d3038777f555d5d11650c6501f1
natew <natewienert@gmail.com>
2018-08-04 23:42:02 -0700

    save

--- app/orbit-app/src/apps/peek/stores/PeekRelatedStore.ts
+++ app/orbit-app/src/apps/peek/stores/PeekRelatedStore.ts
@@ -1,9 +1,11 @@
 import { BitRepository } from '../../../repositories'
+import { sleep } from '@mcro/black'
 
 export class PeekRelatedStore {
   relatedBits = null
 
   async didMount() {
+    await sleep(50)
     this.relatedBits = await BitRepository.find({
       take: 6,
       skip: 2,

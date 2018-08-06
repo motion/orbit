63acc776ba59bbce11fd411bcb24f1d4b2a260ac
natew <natewienert@gmail.com>
2018-08-06 13:39:57 -0700

    save

--- app/orbit-app/src/apps/orbit/orbitDocked/orbitOnboard/OrbitOnboard.tsx
+++ app/orbit-app/src/apps/orbit/orbitDocked/orbitOnboard/OrbitOnboard.tsx
@@ -1,10 +1,15 @@
 import { SubPane } from '../../SubPane'
-import { view } from '@mcro/black'
+import { view, compose } from '@mcro/black'
 import { Text, Button, Theme, View } from '@mcro/ui'
+import { ORBIT_WIDTH } from '@mcro/constants'
 
 const controlsHeight = 40
+const numFrames = 3
+const frameWidth = ORBIT_WIDTH
 
 const OnboardFrame = view({
+  position: 'relative',
+  width: frameWidth,
   height: 300,
   paddingBottom: controlsHeight,
   alignItems: 'center',
@@ -12,29 +17,61 @@ const OnboardFrame = view({
 })
 
 const Controls = view({
+  flexFlow: 'row',
   position: 'absolute',
   bottom: 0,
   right: 0,
+  alignItems: 'center',
 })
 
-export const OrbitOnboard = () => {
+const FrameAnimate = view({
+  margin: [0, -16],
+  flexFlow: 'row',
+  width: frameWidth * numFrames,
+  transition: 'all ease-in 300ms',
+})
+
+FrameAnimate.theme = ({ curFrame }) => ({
+  transform: {
+    x: -frameWidth * curFrame,
+  },
+})
+
+const decorator = compose(
+  view.attach({
+    store: class {
+      curFrame = 0
+      nextFrame = () => this.curFrame++
+      lastFrame = () => this.curFrame--
+    },
+  }),
+)
+
+export const OrbitOnboard = decorator(({ store }) => {
   return (
     <SubPane name="onboard">
-      <OnboardFrame>
-        <Text size={2.5} fontWeight={600}>
-          Hello
-        </Text>
-        <View height={10} />
-        <Text size={1.5} alpha={0.5}>
-          Welcome to Orbit.
-        </Text>
-
-        <Controls>
-          <Theme name="orbit">
-            <Button size={1.2}>Let's get setup</Button>
-          </Theme>
-        </Controls>
-      </OnboardFrame>
+      <FrameAnimate curFrame={store.curFrame}>
+        <OnboardFrame>
+          <Text size={2.5} fontWeight={600}>
+            Hello
+          </Text>
+          <View height={10} />
+          <Text size={1.5} alpha={0.5}>
+            Welcome to Orbit.
+          </Text>
+        </OnboardFrame>
+      </FrameAnimate>
+      <Controls>
+        <Button chromeless onClick={store.lastFrame}>
+          Back
+        </Button>
+        <View width={10} />
+        <Theme name="orbit">
+          <Button size={1.2} onClick={store.nextFrame}>
+            Let's get setup
+          </Button>
+        </Theme>
+      </Controls>
     </SubPane>
   )
-}
+})

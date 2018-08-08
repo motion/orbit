c8f4ae5d514cd2b41139da2a6ef1f0bf84923c2d
natew <natewienert@gmail.com>
2018-08-08 11:06:10 -0700

    build improvements

--- app/build-orbit/scripts/build.sh
+++ app/build-orbit/scripts/build.sh
@@ -6,8 +6,8 @@ set -e
 cd $(dirname $0)
 
 # --resume
-FLAGs=$@
-if [[ "$@" =~ "--resume" ]]; then
+FLAGS=$@
+if [ "$1" = "--resume" ]; then
   FLAGS=`cat .lastbuild`
   echo "resuming with options $FLAGS"
 fi
@@ -18,61 +18,79 @@ cd ..
 
 # BUILD
 
-if [ "$1" = "--debug" ]; then
+if [[ "$FLAGS" =~ "--debug" ]]; then
   DEBUG="electron-packager,electron-build:*"
 fi
 
 # iohook
-if [[ "$@" =~ "--no-iohook" ]]; then
+if [[ "$FLAGS" =~ "--no-iohook" ]]; then
   echo "not building iohook..."
 else
   echo "installing iohook..."
   (cd build-resources/iohook && npm install)
-  echo -n "--no-iohook " >> ./scripts/.lastbuild
 fi
+echo -n "--no-iohook " >> ./scripts/.lastbuild
 
 # version
-if [[ "$@" =~ "--no-version" ]]; then
+if [[ "$FLAGS" =~ "--no-version" ]]; then
   echo "not versioning..."
 else
   echo "bump version..."
   npm version patch
-  echo -n "--no-version " >> ./scripts/.lastbuild
 fi
+echo -n "--no-version " >> ./scripts/.lastbuild
 
-#publish
-if [[ "$@" =~ "--no-install" ]]; then
+# bundle
+if [[ "$FLAGS" =~ "--no-bundle" ]]; then
+  echo "not bundling..."
+else
+  echo "bundling..."
+  (cd ../orbit-app && npm run build-app)
+fi
+echo -n "--no-bundle " >> ./scripts/.lastbuild
+
+function publish-packages() {
+  # run verdaccio
+  kill $(lsof -t -i:4343) || true
+  npx verdaccio -c ./scripts/verdaccio/publish-config.yaml --listen 4343 &
+  while ! nc -z localhost 4343; do sleep 0.1; done
+  # publish
+  (cd ../.. && npx lerna exec --ignore "@mcro/orbit" --ignore "@mcro/build-orbit" -- npm publish --registry http://localhost:4343 --force)
+  # kill verdaccio
+  kill %-
+  kill $(lsof -t -i:4343) || true
+}
+
+# publish
+if [[ "$FLAGS" =~ "--no-publish" ]]; then
   echo "not publishing..."
 else
-  echo "publishing packages for prod install..."
-
-  # verdaccio
-  echo "running verdaccio private registry..."
-  kill $(lsof -t -i:4343) || true
-  npx verdaccio -c ./scripts/verdaccio/config.yaml --listen 4343 &
-  sleep 1
-  echo "making you log in..."
-  npm login --registry=http://localhost:4343/ --scope=@mcro
-
-  # orbit-app
-  echo "bundle orbit-app..."
-  (cd ../orbit-app && npm run build-app)
-
-  # publish packages
-  function publish-all() {
-    npx lerna exec --stream --ignore "@mcro/orbit" -- npm publish --force
-  }
-  (cd ../.. && publish-all)
-
-  # install packages
-  echo "installing for prod... $(pwd)"
-  (cd app && yarn install --production --registry http://localhost:4343)
-
-  # cleanup
-  echo "killing verdaccio..."
-  kill %-
-  echo -n "--no-install " >> ./scripts/.lastbuild
+  echo "publishing packages..."
+  publish-packages
 fi
+echo -n "--no-publish " >> ./scripts/.lastbuild
+
+function install-packages() {
+  # run verdaccio
+  npx verdaccio -c ./scripts/verdaccio/install-config.yaml --listen 4343 &
+  while ! nc -z localhost 4343; do sleep 0.1; done
+  # install
+  echo "installing for prod... $(pwd)"
+  (cd ../orbit && npm install --production --registry http://localhost:4343)
+  # kill verdaccio
+  kill %-
+  kill $(lsof -t -i:4343) || true
+  echo -n "--no-install " >> ./scripts/.lastbuild
+}
+
+# install
+if [[ "$FLAGS" =~ "--no-install" ]]; then
+  echo "not installing..."
+else
+  echo "installing packages..."
+  install-packages
+fi
+echo -n "--no-install " >> ./scripts/.lastbuild
 
 # bundle
 echo "running electron-bundler..."
@@ -83,16 +101,16 @@ DEBUG=electron-packager node -r esm --trace-warnings ./scripts/bundle.js
 # cp -r ./build-resources/iohook/node_modules/iohook dist/Orbit-darwin-x64/Orbit.app/Contents/Resources/app/node_modules/@mcro/orbit-desktop/node_modules
 
 # sign
-if [[ "$@" =~ "--no-sign" ]]; then
+if [[ "$FLAGS" =~ "--no-sign" ]]; then
   echo "not signing"
 else
   echo "signing app..."
   npx electron-osx-sign --ignore puppeteer/\\.local-chromium ./dist/Orbit-darwin-x64/Orbit.app
-  echo -n "--no-sign " >> ./scripts/.lastbuild
 fi
+echo -n "--no-sign " >> ./scripts/.lastbuild
 
 # pack
-if [[ "$@" =~ "--no-pack" ]]; then
+if [[ "$FLAGS" =~ "--no-pack" ]]; then
   echo "not signing"
 else
   echo "packing app into .dmg..."

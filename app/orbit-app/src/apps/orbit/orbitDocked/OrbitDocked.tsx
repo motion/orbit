1af51a8d4b4c5f6a87ba688b342075ae51f0fca7
natew <natewienert@gmail.com>
2018-08-09 13:38:18 -0700

    save

--- app/orbit-app/src/apps/orbit/orbitDocked/OrbitDocked.tsx
+++ app/orbit-app/src/apps/orbit/orbitDocked/OrbitDocked.tsx
@@ -38,7 +38,7 @@ const OrbitDockedFrame = view(UI.Col, {
   opacity: 0,
   // transition: 'bottom ease 80ms',
   transform: {
-    x: 6,
+    x: 12,
   },
   visible: {
     pointerEvents: 'auto',
@@ -111,11 +111,16 @@ class OrbitDockedStore {
 @view
 export class OrbitDocked extends React.Component<Props> {
   render() {
-    const { paneManagerStore, appStore, searchStore } = this.props
+    const { paneManagerStore, appStore, searchStore, store } = this.props
     // log('DOCKED ------------', store.animationState)
     return (
       <UI.Theme name="dark">
-        <OrbitDockedFrame visible={App.orbitState.docked}>
+        <OrbitDockedFrame
+          visible={App.orbitState.docked ? store.animationState.visible : false}
+          willAnimate={
+            App.orbitState.docked ? store.animationState.willAnimate : false
+          }
+        >
           <OrbitDockedChrome appStore={appStore} />
           <UI.View borderBottomRadius={BORDER_RADIUS} flex={1}>
             <OrbitHeader

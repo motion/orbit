bcac61943b79b2a3dd70c8f427e494e285dbd8ca
natew <natewienert@gmail.com>
2018-08-16 17:37:19 -0700

    kill children

--- app/orbit/src/main.ts
+++ app/orbit/src/main.ts
@@ -1,6 +1,8 @@
 import { findContiguousPorts } from './findContiguousPorts'
 import { setConfig } from '@mcro/config'
 import killPort from 'kill-port'
+import psTree from 'ps-tree'
+import childProcess from 'child_process'
 
 type OrbitOpts = {
   version: string
@@ -33,4 +35,18 @@ export async function main({ version }: OrbitOpts) {
   // require apps after config
   const ElectronApp = require('@mcro/orbit-electron')
   ElectronApp.main({ port: ports[0] })
+
+  // handle exits gracefully
+  process.on('exit', () => {
+    console.log('Orbit exiting...')
+    psTree(process.getuid(), (err, children) => {
+      if (err) {
+        console.log('error getting children', err)
+        return
+      }
+      const pids = children.map(x => x.PID)
+      console.log('exiting children', pids)
+      childProcess.spawn('kill', ['-9', ...pids])
+    })
+  })
 }

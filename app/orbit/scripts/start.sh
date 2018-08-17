4282373abbf8d12ad30c3464fb163c7dc41b6055
natew <natewienert@gmail.com>
2018-08-16 17:44:44 -0700

    save

--- app/orbit/scripts/start.sh
+++ app/orbit/scripts/start.sh
@@ -2,20 +2,15 @@
 
 cd $(dirname $0)/..
 
-# test prod app
-if [ "$1" = "start-prod" ]; then
-  ./scripts/start-debug-browser.sh &
-  echo "starting app in prod..."
-  NODE_ENV=production ../build-orbit/node_modules/.bin/electron --inspect=9001 --remote-debugging-port=9002 ./_/main
-  wait
-  kill %-
-  exit 0
-fi
-
 ./scripts/start-debug-browser.sh &
 ../orbit-electron/scripts/start.sh &
 ../orbit-desktop/scripts/start.sh &
-../orbit-app/scripts/start.sh &
+if [ "$1" = "start-prod" ]; then
+  ../orbit-app/scripts/start.sh start-prod &
+else
+  ../orbit-app/scripts/start.sh &
+fi
+
 
 wait
 

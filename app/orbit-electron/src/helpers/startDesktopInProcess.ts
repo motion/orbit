9cc8e1191a62bb2d23c97519accf0a352194f0a7
natew <natewienert@gmail.com>
2018-08-20 14:02:35 -0700

    save

--- app/orbit-electron/src/helpers/startDesktopInProcess.ts
+++ app/orbit-electron/src/helpers/startDesktopInProcess.ts
@@ -1,26 +1,33 @@
-// import exec from 'execa'
-import { execFile } from 'child_process'
+import { fork } from 'child_process'
 // @ts-ignore
 import { promisify } from 'util'
 import { logger } from '@mcro/logger'
 import * as Path from 'path'
 import { getConfig } from '@mcro/config'
+import electronUtil from 'electron-util/node'
 
 const log = logger('electron')
-const exec = promisify(execFile)
+const exec = promisify(fork)
 
 export async function startDesktopInProcess(port) {
-  const desktopRoot = require.resolve('@mcro/orbit-desktop')
+  const desktopRoot = electronUtil.fixPathForAsarUnpack(
+    require.resolve('@mcro/orbit-desktop'),
+  )
   const productionRoot = Path.join(desktopRoot, '..', 'main-production.js')
   log(`Desktop main file: ${productionRoot}`)
-  const { stdout, stderr } = await exec(productionRoot, [], {
-    cwd: Path.join(desktopRoot, '..'),
-    env: {
-      DESKTOP_PORT: port,
-      NODE_ENV: process.env.NODE_ENV,
-      ORBIT_CONFIG: JSON.stringify(getConfig()),
-    },
-  })
-  stdout.pipe(process.stdout)
-  stderr.pipe(process.stderr)
+  try {
+    const child = await exec(productionRoot, [], {
+      cwd: Path.join(desktopRoot, '..'),
+      env: {
+        DESKTOP_PORT: port,
+        NODE_ENV: process.env.NODE_ENV,
+        ORBIT_CONFIG: JSON.stringify(getConfig()),
+        PATH: process.env.PATH,
+      },
+    })
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stderr)
+  } catch (err) {
+    console.log('error starting desktop', err)
+  }
 }

d7151eeaf0d7af2ecd3b541b3620c51e30f60926
natew <natewienert@gmail.com>
2018-08-21 22:22:16 -0700

    save

--- packages/automagical/src/ensure.ts
+++ packages/automagical/src/ensure.ts
@@ -7,7 +7,9 @@ export const ensure = (message: string, condition: boolean) => {
   if (!condition) {
     // TODO we can make this throw a custom error type and catch it in automagic
     // that would let us log out the parent react name along with message
-    log(`Reaction cancelled: ${message}`)
+    if (process.env.DEBUG === 'automagical') {
+      log(`Reaction cancelled: ${message}`)
+    }
     throw cancel
   }
 }

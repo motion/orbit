143805d9be494b9c9ac731c7efdb616740289faf
natew <natewienert@gmail.com>
2018-08-23 22:13:46 -0700

    slower syncs

--- app/orbit-desktop/src/syncer/index.ts
+++ app/orbit-desktop/src/syncer/index.ts
@@ -12,22 +12,66 @@ import { MailWhitelisterSyncer } from './mail-whitelister/MailWhitelisterSyncer'
 import { SlackIssuesSyncer } from './slack/SlackIssuesSyncer'
 import { SlackPeopleSyncer } from './slack/SlackPeopleSyncer'
 
+const ONE_MINUTE = 1000 * 60 * 60
+const FIVE_MINUTES = ONE_MINUTE * 5
+const TEN_MINUTES = ONE_MINUTE * 10
+
 export const Syncers = [
-  new Syncer({ type: 'jira', constructor: JiraIssueSyncer, interval: 1000 * 120 }),
-  new Syncer({ type: 'jira', constructor: JiraPeopleSyncer, interval: 1000 * 120 }),
+  new Syncer({
+    type: 'jira',
+    constructor: JiraIssueSyncer,
+    interval: TEN_MINUTES,
+  }),
+  new Syncer({
+    type: 'jira',
+    constructor: JiraPeopleSyncer,
+    interval: TEN_MINUTES,
+  }),
   new SyncerGroup('GithubSyncers', [
-    new Syncer({ type: 'confluence', constructor: ConfluencePeopleSyncer, interval: 1000 * 120 }),
-    new Syncer({ type: 'confluence', constructor: ConfluenceContentSyncer, interval: 1000 * 120 }),
+    new Syncer({
+      type: 'confluence',
+      constructor: ConfluencePeopleSyncer,
+      interval: TEN_MINUTES,
+    }),
+    new Syncer({
+      type: 'confluence',
+      constructor: ConfluenceContentSyncer,
+      interval: TEN_MINUTES,
+    }),
   ]),
-  new Syncer({ type: 'gdocs' as any, constructor: GDriveSyncer, interval: 1000 * 120 }), // todo: fix gdocs as any - to fix that we need to change all gdocs to gdrive
-  new Syncer({ type: 'gmail', constructor: GMailSyncer, interval: 1000 * 120 }),
+  new Syncer({
+    type: 'gdocs' as any,
+    constructor: GDriveSyncer,
+    interval: FIVE_MINUTES,
+  }), // todo: fix gdocs as any - to fix that we need to change all gdocs to gdrive
+  new Syncer({
+    type: 'gmail',
+    constructor: GMailSyncer,
+    interval: FIVE_MINUTES,
+  }),
   new SyncerGroup('GithubSyncers', [
-    new Syncer({ type: 'github', constructor: GithubPeopleSyncer, interval: 1000 * 120 }),
-    new Syncer({ type: 'github', constructor: GithubIssueSyncer, interval: 1000 * 120 }),
+    new Syncer({
+      type: 'github',
+      constructor: GithubPeopleSyncer,
+      interval: TEN_MINUTES,
+    }),
+    new Syncer({
+      type: 'github',
+      constructor: GithubIssueSyncer,
+      interval: TEN_MINUTES,
+    }),
   ]),
   new SyncerGroup('SlackSyncers', [
-    new Syncer({ type: 'slack', constructor: SlackPeopleSyncer, interval: 1000 * 120 }),
-    new Syncer({ type: 'slack', constructor: SlackIssuesSyncer, interval: 1000 * 200 }),
+    new Syncer({
+      type: 'slack',
+      constructor: SlackPeopleSyncer,
+      interval: TEN_MINUTES,
+    }),
+    new Syncer({
+      type: 'slack',
+      constructor: SlackIssuesSyncer,
+      interval: FIVE_MINUTES,
+    }),
   ]),
-  new Syncer({ constructor: MailWhitelisterSyncer, interval: 1000 * 120 }),
+  new Syncer({ constructor: MailWhitelisterSyncer, interval: TEN_MINUTES }),
 ]

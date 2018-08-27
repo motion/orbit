88faaa663bd2b97743224342f7f4471a7e26cc75
natew <natewienert@gmail.com>
2018-08-27 13:41:06 -0700

    3rd party app dist

--- app/build-orbit/scripts/zip-app.sh
+++ app/build-orbit/scripts/zip-app.sh
@@ -21,6 +21,6 @@ echo "zipping app..."
   # -y preserve symlinks
   # cd in because zip compressed the full path
   (cd ./dist/Orbit-darwin-x64 && zip -y -r9 -q $OUT_FILE Orbit.app)
-  codesign -vfs "Developer ID Application: Nathan Wienert (399WY8X9HY)" --keychain login.keychain $OUT_FILE
+  codesign -vfs "3rd Party Mac Developer Application: Nathan Wienert (399WY8X9HY)" --keychain login.keychain $OUT_FILE
   scp -r $OUT_FILE root@get.tryorbit.com:/updates/Orbit.app.zip
 fi

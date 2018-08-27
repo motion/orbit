da80ae6a8fdd87cfac3c4acafc98e72149f55b44
natew <natewienert@gmail.com>
2018-08-26 23:22:01 -0700

    save

--- app/build-orbit/scripts/publish.sh
+++ app/build-orbit/scripts/publish.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
+set -e
+
 # publish to our github for auto updates
 
 # root
 cd $(dirname $0)/..
-
-DIST="./dist"
 
 if [ "$npm_package_version" = "" ]; then
   echo "to run: npm run publish"
   exit 0
 fi
 
+# publish .app.zip
+zip -r9 -q ./dist/Orbit.app.zip ./dist/Orbit-darwin-x64/Orbit.app
+codesign -vfs "Developer ID Application: Nathan Wienert (399WY8X9HY)" --keychain login.keychain ./dist/Orbit.app.zip
+scp -r ./dist/Orbit.app.zip root@get.tryorbit.com:/updates/Orbit.app.zip
+
 # prepare for publish by version
-if [ ! -f "$DIST/Orbit-$npm_package_version.dmg" ]; then
+if [ ! -f "./dist/Orbit-$npm_package_version.dmg" ]; then
   echo "zipping into versioned file..."
-  cp $DIST/Orbit.dmg $DIST/Orbit-$npm_package_version.dmg
-  zip -r -X -8 -q $DIST/Orbit-$npm_package_version-mac.zip $DIST/Orbit-darwin-x64/Orbit.app
+  cp ./dist/Orbit.dmg ./dist/Orbit-$npm_package_version.dmg
+  zip -r -X -8 -q ./dist/Orbit-$npm_package_version-mac.zip ./dist/Orbit-darwin-x64/Orbit.app
 fi
 
-scp -r $DIST/Orbit-$npm_package_version-mac.zip root@get.tryorbit.com:/updates/Orbit.zip
-
 # publish
-npx publish ./scripts/publish-config.json -p $DIST -d
+npx publish ./scripts/publish-config.json -p ./dist -d
 
 # clean up
-rm -r $DIST/Orbit-$npm_package_version-mac.zip
-mkdir -p $DIST/releases
-mv $DIST/Orbit-$npm_package_version.dmg $DIST/releases
+rm -r ./dist/Orbit-$npm_package_version-mac.zip
+mkdir -p ./dist/releases
+mv ./dist/Orbit-$npm_package_version.dmg ./dist/releases

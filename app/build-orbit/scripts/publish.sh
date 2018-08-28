b1e2000e304bed8183e6f1e23c9385f9b7545f21
natew <natewienert@gmail.com>
2018-08-27 19:24:32 -0700

    save

--- app/build-orbit/scripts/publish.sh
+++ app/build-orbit/scripts/publish.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-# publish to our github for auto updates
-
 # root
 cd $(dirname $0)/..
 
-if [ "$npm_package_version" = "" ]; then
-  echo "to run: npm run publish"
-  exit 0
-fi
-
-scp ./dist/*.json root@get.tryorbit.com:/updates
 scp ./dist/*.yaml root@get.tryorbit.com:/updates
 scp ./dist/*-mac.zip root@get.tryorbit.com:/updates

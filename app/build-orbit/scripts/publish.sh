f360b96b5dbba138dc3cca04b5a9a842a56e647e
natew <natewienert@gmail.com>
2018-08-30 23:26:12 -0700

    only publish latest

--- app/build-orbit/scripts/publish.sh
+++ app/build-orbit/scripts/publish.sh
@@ -5,6 +5,11 @@ set -e
 # root
 cd $(dirname $0)/..
 
+if [ "$npm_package_version" = "" ]; then
+  echo "run with npm run publish-app"
+  exit 0
+fi
+
 scp ./dist/*.yml root@get.tryorbit.com:/updates
 scp ./dist/*.yaml root@get.tryorbit.com:/updates
-scp ./dist/*-mac.zip root@get.tryorbit.com:/updates
+scp ./dist/Orbit-$npm_package_version-mac.zip root@get.tryorbit.com:/updates

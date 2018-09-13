9f3164f81a3e1514a6e5005ff15ea3aa209193f8
natew <natewienert@gmail.com>
2018-09-12 17:03:05 -0700

    save

--- app/orbit-app/src/components/SelectableCarousel.tsx
+++ app/orbit-app/src/components/SelectableCarousel.tsx
@@ -23,18 +23,19 @@ class CarouselStore {
       this.props.isActiveStore ? this.props.isActiveStore.isActive : null,
     ],
     ([index, isActive]) => {
-      ensure('has carousel', !!this.carouselRef.current)
+      const carousel = this.carouselRef.current
+      ensure('has carousel', !!carousel)
       const { items, offset, selectionStore, resetOnInactive } = this.props
       if (isActive == false) {
         if (resetOnInactive) {
-          this.carouselRef.current.scrollTo(0)
+          carousel.scrollTo(0)
         }
         throw cancel
       }
       ensure('has index', typeof index === 'number')
-      ensure('wasnt clicked', selectionStore.selectEvent !== 'click')
       ensure('within bounds', index >= offset && index <= offset + items.length)
-      this.carouselRef.current.scrollTo(index - offset)
+      const wasClicked = selectionStore.selectEvent === 'click'
+      carousel.scrollTo(index - offset, { onlyIfOutside: wasClicked })
     },
   )
 }
@@ -45,19 +46,11 @@ class CarouselStore {
 })
 export class SelectableCarousel extends React.Component<Props> {
   render() {
-    const {
-      store,
-      selectionStore,
-      cardWidth = 180,
-      cardHeight = 95,
-      ...props
-    } = this.props
+    const { store, selectionStore, cardWidth = 180, cardHeight = 95, ...props } = this.props
     return (
       <Carousel
         ref={store.carouselRef}
-        after={
-          <div style={{ width: App.orbitState.size[0] - cardWidth - 20 }} />
-        }
+        after={<div style={{ width: App.orbitState.size[0] - cardWidth - 20 }} />}
         cardWidth={cardWidth}
         cardHeight={cardHeight}
         {...props}

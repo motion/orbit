8004e7382700687259aba48c2a88fb429a6c5fb3
natew <natewienert@gmail.com>
2018-09-14 00:12:58 -0700

    some size reduction

--- app/oracle/pyocr/build.sh
+++ app/oracle/pyocr/build.sh
@@ -10,3 +10,22 @@ rm -r dist/run || true
 pyinstaller src/run.py
 cp src/model.pyt dist/run
 
+# cleanup big unecessary files
+cd dist/run/torch
+rm -r lib/libcaffe2.dylib
+rm -r __pycache__
+rm -r autograd
+rm -r backends
+rm -r contrib
+rm -r cuda
+rm -r distributed
+rm -r distributions
+rm -r legacy
+rm -r optim
+rm -r serialization.py
+rm -r sparse
+rm -r tensor.py
+rm -r nn
+rm -r lib/libcrypto.1.0.0.dylib
+rm -r lib/libssl.1.0.0.dylib
+cd -

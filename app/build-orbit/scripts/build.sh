52d9cef3e9ac15c7ea1946143b16ac8b3b8e0d61
natew <natewienert@gmail.com>
2018-09-23 02:21:27 -0700

    save

--- app/build-orbit/scripts/build.sh
+++ app/build-orbit/scripts/build.sh
@@ -121,8 +121,7 @@ rm -r dist/mac/Orbit.app || true
 (cd stage-app && ../node_modules/.bin/electron-rebuild --version 3.0.0-beta.1)
 # so desktop node subprocess can use it
 rm -r stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64 || true
-# cp -r stage-app/node_modules/sqlite3/lib/binding/electron-v3.0-darwin-x64 stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64
-ln -s stage-app/node_modules/sqlite3/lib/binding/electron-v3.0-darwin-x64 stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64
+cp -r stage-app/node_modules/sqlite3/lib/binding/electron-v3.0-darwin-x64 stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64
 
 # see stage-app/package.json for options
 echo "electron-builder..."

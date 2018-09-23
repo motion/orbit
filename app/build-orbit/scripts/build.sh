9eb93c0f2e19b7af669a39b4703c4857e67e3734
natew <natewienert@gmail.com>
2018-09-22 18:08:08 -0700

    fix script

--- app/build-orbit/scripts/build.sh
+++ app/build-orbit/scripts/build.sh
@@ -116,11 +116,9 @@ rm -r dist/mac/Orbit.app || true
 #
 (cd stage-app && ../node_modules/.bin/electron-rebuild --version 3.0.0-beta.1)
 # so desktop node subprocess can use it
-if [ ! -L "node_modules/sqlite3/lib/binding/node-v64-darwin-x64" ]; then
+if [ ! -d "stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64" ]; then
   cp -r stage-app/node_modules/sqlite3/lib/binding/electron-v3.0-darwin-x64 stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64
 fi
-# remove outdated one
-rm -r stage-app/node_modules/sqlite3/lib/binding/node-v64-darwin-x64 || true
 
 # see stage-app/package.json for options
 echo "electron-builder..."

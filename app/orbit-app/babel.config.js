b332b8283aad1cc96dabc447f655f4853210030e
natew <natewienert@gmail.com>
2018-09-26 10:21:34 -0700

    hardcode versions

--- app/orbit-app/babel.config.js
+++ app/orbit-app/babel.config.js
@@ -1,9 +1,7 @@
 if (process.env.PUNDLE) {
   module.exports = {
     presets: [
-      [
-        '@mcro/babel-preset-motion' /* , { disable: ['react-hot-loader/babel'] } */,
-      ],
+      ['@mcro/babel-preset-motion' /* , { disable: ['react-hot-loader/babel'] } */],
       ['@babel/preset-env', { targets: { node: '7.0' } }],
     ],
   }

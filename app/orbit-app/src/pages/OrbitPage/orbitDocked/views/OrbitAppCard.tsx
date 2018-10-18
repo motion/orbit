228d2738e28fdb2085e2f0226662e1ed8c102877
natew <natewienert@gmail.com>
2018-10-18 11:07:17 -0700

    save

--- app/orbit-app/src/pages/OrbitPage/orbitDocked/views/OrbitAppCard.tsx
+++ app/orbit-app/src/pages/OrbitPage/orbitDocked/views/OrbitAppCard.tsx
@@ -6,7 +6,6 @@ import { OrbitItemProps } from '../../../../views/OrbitItemProps'
 import { SyncStatus } from './SyncStatus'
 import { Text } from '@mcro/ui'
 import { Setting } from '@mcro/models'
-import { NICE_INTEGRATION_NAMES } from '../../../../constants'
 
 type Props = OrbitItemProps<Setting> &
   AppInfoProps & {
@@ -21,38 +20,39 @@ const decorator = compose(
   view,
 )
 
-export const OrbitAppCard = decorator(({ store, model, isActive, subtitle, ...props }: Props) => {
-  const countSubtitle = store.bitsCount >= 0 ? Number(store.bitsCount).toLocaleString() : '...'
-  return (
-    <OrbitCard
-      model={model}
-      settingId={model.id}
-      title={NICE_INTEGRATION_NAMES[model.type]}
-      titleProps={{
-        ellipse: true,
-      }}
-      subtitle={typeof subtitle !== 'undefined' ? subtitle : countSubtitle}
-      subtitleProps={{
-        size: 1.5,
-        fontWeight: 300,
-      }}
-      titleFlex={1}
-      date={store.job && store.job.updatedAt}
-      icon={model.type}
-      iconProps={{
-        size: 20,
-      }}
-      {...props}
-    >
-      <SyncStatus settingId={model.id}>
-        {(syncJobs, removeJobs) => {
-          return (
-            <Text size={0.85} alpha={0.6}>
-              {syncJobs.length ? 'Syncing' : removeJobs.length ? 'Removing' : ''}
-            </Text>
-          )
+export const OrbitAppCard = decorator(
+  ({ store, model, appConfig, isActive, subtitle, ...props }: Props) => {
+    const countSubtitle = store.bitsCount >= 0 ? Number(store.bitsCount).toLocaleString() : '...'
+    return (
+      <OrbitCard
+        direct
+        title={appConfig.title}
+        titleProps={{
+          ellipse: true,
         }}
-      </SyncStatus>
-    </OrbitCard>
-  )
-})
+        subtitle={typeof subtitle !== 'undefined' ? subtitle : countSubtitle}
+        subtitleProps={{
+          size: 1.5,
+          fontWeight: 300,
+        }}
+        titleFlex={1}
+        date={store.job && store.job.updatedAt}
+        icon={appConfig.integration}
+        iconProps={{
+          size: 20,
+        }}
+        {...props}
+      >
+        <SyncStatus settingId={model.id}>
+          {(syncJobs, removeJobs) => {
+            return (
+              <Text size={0.85} alpha={0.6}>
+                {syncJobs.length ? 'Syncing' : removeJobs.length ? 'Removing' : ''}
+              </Text>
+            )
+          }}
+        </SyncStatus>
+      </OrbitCard>
+    )
+  },
+)

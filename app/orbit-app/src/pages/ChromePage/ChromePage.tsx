49fb64ba8bfa4de4f04375bbaeee0dd99bc25a25
natew <natewienert@gmail.com>
2018-11-03 10:11:44 -0700

    save

--- app/orbit-app/src/pages/ChromePage/ChromePage.tsx
+++ app/orbit-app/src/pages/ChromePage/ChromePage.tsx
@@ -3,7 +3,19 @@ import { AppWrapper } from '../../views'
 import { MenuLayer } from './menuLayer/MenuLayer'
 import { Theme } from '@mcro/ui'
 import { App } from '@mcro/stores'
+import { provide, view } from '@mcro/black'
+import { SettingStore } from '../../stores/SettingStore'
+import { SourcesStore } from '../../stores/SourcesStore'
+import { QueryStore } from '../../stores/QueryStore/QueryStore'
 
+@provide({
+  settingStore: SettingStore,
+  sourcesStore: SourcesStore,
+})
+@provide({
+  queryStore: QueryStore,
+})
+@view
 export class ChromePage extends React.Component {
   render() {
     const theme = App.state.darkTheme ? 'clearDark' : 'clearLight'

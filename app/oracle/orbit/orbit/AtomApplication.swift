2f3f583689cf86f90d420c78e7009832c61e3395
natew <natewienert@gmail.com>
2018-11-07 18:24:54 -0800

    save

--- app/oracle/orbit/orbit/AtomApplication.swift
+++ app/oracle/orbit/orbit/AtomApplication.swift
@@ -228,7 +228,7 @@ class AtomApplication: NSObject, NSApplicationDelegate {
         
         let ogTrayRect = button.window!.convertToScreen(rectInWindow)
         // our tray image is a few px too short oops...
-        let trayRect = NSRect.init(x: ogTrayRect.minX, y: ogTrayRect.minY, width: ogTrayRect.width, height: ogTrayRect.height + 4)
+        let trayRect = NSRect.init(x: ogTrayRect.minX, y: ogTrayRect.minY, width: ogTrayRect.width, height: ogTrayRect.height + 8)
         let nextTrayRect = [Int(round(trayRect.minX)), Int(round(trayRect.maxX))]
         // send tray location...
         if (nextTrayRect[0] != lastTrayRect[0] || nextTrayRect[1] != lastTrayRect[1]) {

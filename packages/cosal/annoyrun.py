473d5a5cf350a4ff50e0ab8bb406333e6bf2b035
natew <natewienert@gmail.com>
2018-11-20 16:03:15 -0800

    save

--- packages/cosal/annoyrun.py
+++ packages/cosal/annoyrun.py
@@ -5,7 +5,7 @@ import os
 
 def annoy():
     f = 100  # Length of item vector that will be indexed
-    t = AnnoyIndex(f)
+    t = AnnoyIndex(f, "angular")
 
     if os.environ['DB_FILE']:
         db = json.loads(open(os.environ['DB_FILE']).read())
@@ -18,10 +18,10 @@ def annoy():
         t.build(10)  # 10 trees
         t.save('./app_data/test.ann')
 
-    u = AnnoyIndex(f)
-    u.load('./app_data/test.ann')  # super fast, will just mmap the file
+    # u = AnnoyIndex(f)
+    # u.load('./app_data/test.ann')  # super fast, will just mmap the file
 
     vector = json.loads(os.environ['VECTOR'])
-    print(json.dumps(u.get_nns_by_vector(vector, 10)))
+    print(json.dumps(t.get_nns_by_vector(vector, 10)))
 
     # print(u.get_nns_by_item(0, 2))  # will find the 1000 nearest neighbors

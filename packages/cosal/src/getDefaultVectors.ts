fb9e8f606590c26ce942256aefa976ad9f4bc580
natew <natewienert@gmail.com>
2018-12-10 21:38:18 -0800

    typo

--- packages/cosal/src/getDefaultVectors.ts
+++ packages/cosal/src/getDefaultVectors.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
-import { readJSON } from 'fs-extra';
+import { readJSON } from 'fs-extra'
 
 export async function getDefaultVectors() {
-  return await readJSON(join(__dirname, '..', 'vecs.data.json')
+  return await readJSON(join(__dirname, '..', 'vecs.data.json'))
 }

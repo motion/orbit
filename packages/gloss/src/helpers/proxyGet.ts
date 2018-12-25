8a459022bcf2042f465483f29c425a2c806e118b
natew <natewienert@gmail.com>
2018-12-24 21:07:52 -0800

    save

--- packages/gloss/src/helpers/proxyGet.ts
+++ packages/gloss/src/helpers/proxyGet.ts
@@ -1,14 +1,12 @@
 export function proxyGet<A extends Object>(main: A, ...alternates: A[]): A {
   return new Proxy(main, {
     get(target, key) {
-      console.log('get', target, key)
       const val = Reflect.get(target, key)
       if (typeof val !== 'undefined') {
         return val
       }
       for (const alt of alternates) {
         const altVal = Reflect.get(alt, key)
-        console.log('returning', alt, altVal)
         if (typeof altVal !== 'undefined') {
           return altVal
         }

820950da2e19b3696ea1ffc103d8e8c280cd499a
natew <natewienert@gmail.com>
2019-02-06 18:23:01 -0800

    save

--- packages/css/src/colorHelpers.ts
+++ packages/css/src/colorHelpers.ts
@@ -1,7 +1,9 @@
-import { isColor, toColor } from './toColor'
-import { Color } from './types'
+import { isColor, toColor } from './toColor';
+import { Color } from './types';
 
-export type GradientArg = string | Color
+export type GradientArg = string | Color | { color: string | Color, position?: string }
+
+const makeColor = x => isColor(x) ? toColor(x) : x
 
 export class LinearGradient {
   items: GradientArg[]
@@ -15,8 +17,16 @@ export class LinearGradient {
   }
 
   toString() {
-    const args = this.items.map(item => (isColor(item) ? toColor(item) : item)).join(', ')
-    return `linear-gradient(${args})`
+    const args = this.items.map(item => {
+      if (typeof item === 'string' || !item.color) {
+        return makeColor(item)
+      }
+      if (item.color) {
+        return `${makeColor(item.color)} ${item.position}`
+      }
+    })
+
+    return `linear-gradient(${args.join(', ')})`
   }
 }
 

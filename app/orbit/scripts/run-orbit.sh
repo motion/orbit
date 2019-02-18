2e81a9b8377359b2060d1bd18729d45b933cadd9
Nate Wienert <natewienert@gmail.com>
2019-02-18 11:41:34 -0800

    save (#694)

--- app/orbit/scripts/run-orbit.sh
+++ app/orbit/scripts/run-orbit.sh
@@ -5,13 +5,19 @@ cd $(dirname $0)/..
 npx kill-port 9001
 npx kill-port 9002
 npx kill-port 9003
-export ELECTRON_DISABLE_SECURITY_WARNINGS=true
 export NODE_ENV=development
-print "disabling gpu to prevent white bg bugs"
+export ELECTRON_DISABLE_SECURITY_WARNINGS=true
+
 
 # in development mode we set FIRST_RUN so it auto-runs
 # in prod we have a separate script, see build-orbit/stage-app/index.js
 export FIRST_RUN=true
 
-electron --disable-gpu --async-stack-traces --inspect=9001 --remote-debugging-port=9002 _/main.js
+if [[ "$DISABLE_GPU" =~ "true" ]]; then
+  echo "⚠️ ⚠️ ⚠️ disabling gpu to prevent white bg bugs ⚠️ ⚠️ ⚠️"
+  electron --disable-gpu --async-stack-traces --inspect=9001 --remote-debugging-port=9002 _/main.js
+else
+  electron --async-stack-traces --inspect=9001 --remote-debugging-port=9002 _/main.js
+fi
+
 ``

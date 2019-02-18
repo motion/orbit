e2038e5559c158e632a121adb0c70adcc711f027
natew <natewienert@gmail.com>
2019-02-18 11:07:48 -0800

    sve

--- app/orbit-app/src/apps/useApp.tsx
+++ app/orbit-app/src/apps/useApp.tsx
@@ -1,4 +1,4 @@
-import { isEqual, react } from '@mcro/black'
+import { react } from '@mcro/black'
 import { useHook, useStore } from '@mcro/use-store'
 import { useStoresSimple } from '../hooks/useStores'
 import { AppStore } from './AppStore'
@@ -20,39 +20,27 @@ type AppState = {
 class UseAppStore {
   props: UseAppProps
   stores = useHook(useStoresSimple)
-  state = react(
-    () => {
-      const { stores, props } = this
-      const next: AppState = {
-        appStore: props.appStore || stores.appStore || null,
-        appViews: {},
-        provideStores: null,
+  state = react(() => {
+    const { stores, props } = this
+    const next: AppState = {
+      appStore: props.appStore || stores.appStore || null,
+      appViews: {},
+      provideStores: null,
+    }
+    if (stores.appsStore) {
+      const state = stores.appsStore.appsState
+      if (!state) return next
+      const { appStores, appViews, provideStores } = state
+      // set store
+      if (!next.appStore) {
+        next.appStore = appStores[props.id] || appStores[props.type]
       }
-      if (stores.appsStore) {
-        const state = stores.appsStore.appsState
-        if (!state) return next
-        const { appStores, appViews, provideStores } = state
-        // set store
-        if (!next.appStore) {
-          next.appStore = appStores[props.id] || appStores[props.type]
-        }
-        // set view
-        next.appViews = appViews[props.id] || appViews[props.type] || {}
-        next.provideStores = provideStores[props.id] || provideStores[props.type] || null
-      }
-      return next
-    },
-    next => {
-      if (this.state) {
-        for (const key in next) {
-          if (!isEqual(next[key], this.state[key])) {
-            console.log('ok', key, next[key], this.state[key])
-          }
-        }
-      }
-      return next
-    },
-  )
+      // set view
+      next.appViews = appViews[props.id] || appViews[props.type] || {}
+      next.provideStores = provideStores[props.id] || provideStores[props.type] || null
+    }
+    return next
+  })
 }
 
 export function useApp(props: UseAppProps) {

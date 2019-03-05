d84ea9e7b0c0120d21cb2dcc3ca5e90674470e1a
natew <natewienert@gmail.com>
2019-03-04 18:09:57 -0800

    save

--- app/kit/src/views/ItemView.tsx
+++ app/kit/src/views/ItemView.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { itemViewsApp } from './itemViews'
 
 export function ItemView(props: { item: Bit }) {
+  console.log('props.item', props.item)
+
   const View = itemViewsApp[props.item.type]
 
   if (!View) {

2e10afe33109b1d1e7391de36638971067b7236a
natew <natewienert@gmail.com>
2019-03-06 17:41:18 -0800

    save

--- app/apps/src/apps/search-app/SearchAppMain.tsx
+++ app/apps/src/apps/search-app/SearchAppMain.tsx
@@ -1,9 +1,11 @@
 import { AppProps, AppSubView } from '@o/kit'
-import { Title } from '@o/ui'
 import React from 'react'
+import { SearchHome } from './SearchHome'
 
 export function SearchAppMain({ appConfig }: AppProps) {
-  return <Title>ok {JSON.stringify(appConfig)}</Title>
+  if (appConfig.identifier === 'apps') {
+    return <SearchHome />
+  }
 
   if (appConfig.identifier === 'search') {
     console.warn('todo: search')

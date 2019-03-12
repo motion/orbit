06eda2c80d424e6241b3ab11cf68c57ce8a657cf
Nate Wienert <natewienert@gmail.com>
2019-03-12 13:12:52 -0700

    save (#760)
    merge bugfixes

--- app/apps/src/apps/postgres/index.tsx
+++ app/apps/src/apps/postgres/index.tsx
@@ -1,15 +1,16 @@
 import { createApp } from '@o/kit'
+import React from 'react'
+import PostgresApi from './api'
 import { postgresIcon } from './postgresIcon'
 import { PostgresSettings } from './PostgresSettings'
-import PostgresApi from './api'
 
 export default createApp({
   id: 'postgres',
   name: 'Postgres',
   icon: postgresIcon,
   itemType: 'document', // todo(nate) whats item type for this app?
-  settings: (app) => <PostgresSettings identifier='postgres' app={app}/>,
+  settings: app => <PostgresSettings identifier="postgres" app={app} />,
   setup: PostgresSettings,
   sync: {},
-  API: PostgresApi
+  API: PostgresApi,
 })

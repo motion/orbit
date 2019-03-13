86d4a3f363d13d23ffa7ad7a9ae629883a03428a
natew <natewienert@gmail.com>
2019-03-13 13:07:39 -0700

    save

--- app/orbit-app/src/apps/apps/AppsMain.tsx
+++ app/orbit-app/src/apps/apps/AppsMain.tsx
@@ -1,9 +1,12 @@
-import { AppMainView, AppProps, Icon } from '@o/kit'
-import { Section, Title, TitleRow } from '@o/ui'
+import { AppMainView, AppProps, Icon, useApp } from '@o/kit'
+import { removeApp } from '@o/kit-internal'
+import { Section, Title, TitleBarButton, TitleRow } from '@o/ui'
 import React from 'react'
 import { AppsMainAddApp } from './AppsMainAddApp'
 
 export function AppsMain(props: AppProps) {
+  const [app] = useApp(+props.subId)
+
   // showing settings
   if (props.identifier !== 'apps') {
     if (props.subType === 'sync') {
@@ -11,7 +14,17 @@ export function AppsMain(props: AppProps) {
     } else {
       return (
         <Section>
-          <TitleRow bordered before={<Icon opacity={0.2} name="gear" />}>
+          <TitleRow
+            bordered
+            before={<Icon opacity={0.2} name="gear" />}
+            after={
+              <TitleBarButton
+                icon="boldremove"
+                tooltip={`Remove ${props.title}`}
+                onClick={() => removeApp(app)}
+              />
+            }
+          >
             {props.title}
           </TitleRow>
           <AppMainView {...props} />

a97cb07e02109f03f3d5141c1f671fbd56caa62d
natew <natewienert@gmail.com>
2019-03-19 20:49:51 -0700

    save

--- projects/site/src/pages/HomePage.tsx
+++ projects/site/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { FullScreen, Row, Theme, useDebounce, View } from '@o/ui'
+import { FullScreen, Row, Space, Theme, useDebounce, View } from '@o/ui'
 import { useWaitForFonts } from '@o/wait-for-fonts'
 import { observer } from 'mobx-react-lite'
 import React, { useState } from 'react'
@@ -12,17 +12,27 @@ function HomePage() {
   const [measured, setMeasured] = useState(false)
   const setMeasuredDelayed = useDebounce(setMeasured, 1)
   const fontsLoaded = useWaitForFonts(['Eesti Pro'])
+  const [parSize, setParSize] = useState(16)
 
   if (!fontsLoaded) {
     return null
   }
+
+  const longestText = `Unified search, team vocabulary, and who is good at what.`
 
   return (
     <Parallax pages={3}>
       <ParallaxLayer offset={0} speed={0.5}>
         <Theme name="dark">
           <FullScreen background={theme => theme.background}>
-            <Row height="80%" width="80%" margin="auto" alignItems="center">
+            <Row
+              height="80%"
+              width="70%"
+              maxWidth={900}
+              minWidth={360}
+              margin="auto"
+              alignItems="center"
+            >
               <View width="100%">
                 <FadeDown disable={!measured}>
                   <TitleText>
@@ -31,10 +41,22 @@ function HomePage() {
                     </ViewPortText>
                   </TitleText>
 
-                  <Paragraph debug size={1.65} sizeLineHeight={1.2} textAlign="center" alpha={0.7}>
+                  <Space />
+                  <Space />
+
+                  <ViewPortText style={{ opacity: 0 }} min={16} onReady={setParSize}>
+                    {longestText}
+                  </ViewPortText>
+
+                  <Paragraph
+                    fontSize={parSize - 2}
+                    sizeLineHeight={1.2}
+                    textAlign="center"
+                    alpha={0.7}
+                  >
                     Orbit is a home for team knowledge.
                     <br />
-                    Unified search, team vocabulary, and who is good at what.
+                    {longestText}
                     <br />
                     It's a smart new way to manage your knowledge.
                   </Paragraph>

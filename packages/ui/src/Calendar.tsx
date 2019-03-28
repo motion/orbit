23c825b590cc775a032140965552ab128f4ba064
natew <natewienert@gmail.com>
2019-03-27 20:52:10 -0700

    actually fix

--- packages/ui/src/Calendar.tsx
+++ packages/ui/src/Calendar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DateRangePicker } from 'react-date-range'
 import { memoIsEqualDeep } from './helpers/memoHelpers'
 
-const isNode = typeof process !== 'undefined' && process.release.name === 'node'
+const isNode = typeof process !== 'undefined' && process.release && process.release.name === 'node'
 
 if (isNode === false) {
   require('../Calendar.css')

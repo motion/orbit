70c8d46c452417f74ba125bf8004e7f1fe15c4eb
natew <natewienert@gmail.com>
2019-03-28 19:41:11 -0700

    save

--- packages/ui/src/TreeItems.tsx
+++ packages/ui/src/TreeItems.tsx
@@ -24,6 +24,7 @@ export class TreeItems extends React.PureComponent<
   state = {
     flatTreeItems: [],
     flatKeys: [],
+    maxDepth: -1,
   }
 
   static getDerivedStateFromProps(props, state) {

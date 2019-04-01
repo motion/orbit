f016ed7d0ed9fb054ee23bc182c18a6a561dc05c
natew <natewienert@gmail.com>
2019-04-01 00:29:48 -0700

    almost working w spring

--- packages/ui/src/FloatingView.tsx
+++ packages/ui/src/FloatingView.tsx
@@ -1,7 +1,8 @@
 import { FullScreen } from '@o/gloss'
 import { selectDefined } from '@o/utils'
 import { clamp } from 'lodash'
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useRef } from 'react'
+// @ts-ignore
 import { animated, useSpring } from 'react-spring'
 import { useGesture } from 'react-with-gesture'
 import { Portal } from './helpers/portal'
@@ -9,10 +10,6 @@ import { useGet } from './hooks/useGet'
 import { Interactive, InteractiveProps } from './Interactive'
 import { Omit } from './types'
 import { useVisiblity } from './Visibility'
-
-// TODO:
-// 1. new view <Draggable contain={[0, 0, window.innerWidth, window.innerHeight]} />
-// 2. draggable prop here
 
 export type FloatingViewProps = Omit<InteractiveProps, 'padding'> & {
   padding?: number
@@ -31,56 +28,53 @@ export function FloatingView(props: FloatingViewProps) {
     defaultTop = 0,
     children,
     disableDrag,
-    height,
-    width,
     padding,
     ...restProps
   } = props
-  const controlledSize = typeof height !== 'undefined'
+  const controlledSize = typeof props.height !== 'undefined'
   const controlledPosition = typeof props.top !== 'undefined'
   const isVisible = useVisiblity()
-  const [state, setState] = useState({
-    width: selectDefined(width, defaultWidth),
-    height: selectDefined(height, defaultHeight),
-  })
-  const getState = useGet(state)
   const getProps = useGet(props)
-
-  const [{ xy }, set] = useSpring(() => ({
+  const [{ xy, width, height }, set] = useSpring(() => ({
     xy: [selectDefined(props.left, defaultLeft), selectDefined(props.top, defaultTop)],
+    width: selectDefined(props.width, defaultWidth),
+    height: selectDefined(props.height, defaultHeight),
   }))
+  const getPos = useGet({
+    left: xy.getValue()[0],
+    top: xy.getValue()[1],
+    width: width.getValue(),
+    height: height.getValue(),
+  })
 
   const onResize = useCallback((w, h, desW, desH, sides) => {
     const cb = getProps().onResize
     if (cb) {
       cb(w, h, desW, desH, sides)
     }
-    const ns = getState()
+    const pos = getPos()
     if (sides.right) {
-      ns.width = w
+      set({ width: w })
+    } else if (sides.bottom) {
+      set({ height: h })
+    } else if (sides.top) {
+      const diff = h - pos.height
+      const top = pos.top - diff
+      const height = pos.height + diff
+      set({ xy: [pos.left, top], height })
+    } else if (sides.left) {
+      const diff = w - pos.width
+      const left = pos.left - diff
+      const width = pos.width + diff
+      set({ xy: [left, pos.top], width })
     }
-    if (sides.bottom) {
-      ns.height = h
-    }
-    if (sides.top) {
-      const diff = h - ns.height
-      // top -= diff
-      ns.height += diff
-    }
-    if (sides.left) {
-      const diff = w - ns.width
-      // left -= diff
-      ns.width += diff
-    }
-    // if (top || left) {
-    //   set({ xy: [top || xy.top, left || xy.left] })
-    // }
-
-    setState({ ...ns })
   }, [])
 
   let lastDelta = useRef([props.defaultTop, props.defaultLeft])
   const bind = useGesture(({ down, delta, velocity }) => {
+    if (controlledPosition) {
+      return
+    }
     velocity = clamp(velocity, 1, 8)
     if (!down) {
       lastDelta.current = delta
@@ -88,39 +82,18 @@ export function FloatingView(props: FloatingViewProps) {
     const [x, y] = lastDelta.current
     set({
       xy: down ? [delta[0] + x, delta[1] + y] : lastDelta.current,
-      config: { mass: velocity, tension: 500 * velocity, friction: 50 },
+      config: { duration: 0 },
+      //{ mass: velocity, tension: 500 * velocity, friction: 50 },
     })
   })
-
-  // const { ref } = useDraggable({
-  //   defaultX: defaultLeft,
-  //   defaultY: defaultTop,
-  //   disable: disableDrag,
-  //   // dragBoundaryPad: padding,
-  //   onChange(next) {
-  //     if (controlledPosition) {
-  //       return
-  //     }
-  //     console.log('setting', next.left, next.top)
-  //     set({ xy: [next.left, next.top],
-  //       config: { mass: velocity, tension: 500 * velocity, friction: 50 }
-  //     })
-  //     // const diff = next.top - state.top
-  //     // state.top += diff
-  //     // const diffLeft = next.left - state.left
-  //     // state.left += diffLeft
-  //     // setState({ ...state })
-  //   },
-  // })
 
   return (
     <Portal>
       <FullScreen pointerEvents="none">
         <animated.div
           style={{
-            width: state.width,
-            height: state.height,
-            // @ts-ignore
+            width,
+            height,
             transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
             position: 'relative',
           }}
@@ -132,9 +105,9 @@ export function FloatingView(props: FloatingViewProps) {
             zIndex={12000000}
             disabled={controlledSize}
             padding={padding}
+            {...restProps}
             width="100%"
             height="100%"
-            {...restProps}
             onResize={onResize}
           >
             <FullScreen {...bind()} pointerEvents="inherit">

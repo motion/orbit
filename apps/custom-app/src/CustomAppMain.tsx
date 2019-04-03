773fec4786f194d928ed95736e4da8e23339b15d
natew <natewienert@gmail.com>
2019-04-03 13:22:57 -0700

    save

--- apps/custom-app/src/CustomAppMain.tsx
+++ apps/custom-app/src/CustomAppMain.tsx
@@ -1,19 +1,19 @@
 import { AppProps } from '@o/kit'
-import { Card, MasonryLayout, useFetch } from '@o/ui'
+import { Card, GridItem, GridLayout, useFetch } from '@o/ui'
 import React from 'react'
 
 export function CustomAppMain(_props: AppProps) {
   const items = useFetch('https://jsonplaceholder.typicode.com/photos')
   _props
   return (
-    <MasonryLayout padded>
+    <GridLayout>
       {items.slice(0, 10).map(item => (
-        <div key={item.id} style={{ width: 200, height: 300 }}>
+        <GridItem key={item.id}>
           <Card flex={1} overflow="hidden" title={item.title}>
             <img src={item.url} />
           </Card>
-        </div>
+        </GridItem>
       ))}
-    </MasonryLayout>
+    </GridLayout>
   )
 }

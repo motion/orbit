7cb692cf0242598d9819ef443855a291f7652044
natew <natewienert@gmail.com>
2019-04-04 11:51:48 -0700

    fixing some sizing in flow

--- projects/playground/src/TestUI/TestUIKit.tsx
+++ projects/playground/src/TestUI/TestUIKit.tsx
@@ -1,8 +1,10 @@
-import { List, shallow, Table, useStore } from '@o/kit'
+import { List, shallow, Table, Templates, useStore } from '@o/kit'
 import {
   Card,
   DefinitionList,
   FloatingCard,
+  Flow,
+  FlowStep,
   Form,
   GridItem,
   GridLayout,
@@ -25,8 +27,75 @@ class Store {
 export function TestUIKit() {
   return (
     <Suspense fallback={<Loading />}>
-      <TestGrid />
+      <CustomApp2 />
     </Suspense>
+  )
+}
+
+export function CustomApp2() {
+  return (
+    <Flow
+      initialData={{
+        selected: [],
+      }}
+    >
+      <FlowStep
+        title="Step 1"
+        subTitle="Select your thing"
+        validateFinished={data =>
+          data.rows.length > 0
+            ? true
+            : {
+                rows: 'Need to select rows.',
+              }
+        }
+      >
+        {({ data, setData }) => {
+          return (
+            <Layout type="row">
+              <Pane resizable>
+                <Table
+                  searchable
+                  selectable="multi"
+                  onSelect={rows => setData({ selected: rows })}
+                  rows={[
+                    {
+                      title: 'Hello world Hello world Hello world Hello world Hello world',
+                      date: new Date(Date.now()),
+                    },
+                    { title: 'Hello world', date: new Date(Date.now()) },
+                    { title: 'Hello world', date: new Date(Date.now()) },
+                  ]}
+                />
+              </Pane>
+              <Pane>
+                <List items={data.selected} />
+              </Pane>
+            </Layout>
+          )
+        }}
+      </FlowStep>
+
+      <FlowStep title="Step 2" subTitle="Select other thing">
+        {(/* { data, setData, done } */) => (
+          <Templates.MasterDetail
+            items={[
+              { title: 'Something', group: 'Hello', icon: 'test', subTitle: 'hello' },
+              { title: 'Something', group: 'Hello', icon: 'smoe', subTitle: 'hello' },
+              { title: 'Something', group: 'Hello', icon: 'hi', subTitle: 'hello' },
+              { title: 'Something', group: 'Hello', icon: 'aos', subTitle: 'hello' },
+              { title: 'Something', group: 'Hello', icon: 'dn', subTitle: 'hello' },
+            ]}
+          >
+            {selected => (!selected ? <Loading /> : <Title>{selected.title}</Title>)}
+          </Templates.MasterDetail>
+        )}
+      </FlowStep>
+
+      <FlowStep title="Step 3" subTitle="Select other thing">
+        <Templates.Message title="All set" icon="check" />
+      </FlowStep>
+    </Flow>
   )
 }
 

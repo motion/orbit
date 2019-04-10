718706e4a592ceb4886c367ae6ce084b1b17d2ea
natew <natewienert@gmail.com>
2019-04-09 17:42:14 -0700

    save

--- app/orbit-electron/src/helpers/devTools.ts
+++ app/orbit-electron/src/helpers/devTools.ts
@@ -7,5 +7,5 @@ if (process.env.NODE_ENV === 'development') {
 
   const devToolsReactNew = join(require.resolve('@o/react-devtools'), '..')
 
-  devTools = [tools.mobx /* , tools.react */, devToolsReactNew]
+  devTools = [tools.mobx, tools.react, devToolsReactNew]
 }

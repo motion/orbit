305e7cd2b24da1d7fbfeede86dba921b6a4c6091
natew <natewienert@gmail.com>
2019-04-15 17:02:34 -0700

    save

--- projects/site/src/pages/HomePage/EarlyAccessBetaSection.tsx
+++ projects/site/src/pages/HomePage/EarlyAccessBetaSection.tsx
@@ -16,38 +16,48 @@ export function AbdomenSection(props) {
             <Image
               position="absolute"
               top={0}
-              left={0}
-              right={0}
               width="100%"
+              minWidth={1200}
               height={100}
               src={lightSeparator}
             />
           }
         >
-          <View height={100} />
-          <SpacedPageContent
-            header={
-              <>
-                <PillButton>Beta</PillButton>
-                <Space size="sm" />
-                <TitleText size="xxl">Early Access.</TitleText>
-                <TitleTextSub>
-                  Orbit is now in beta. We're rolling out to small groups.
-                </TitleTextSub>
-                <TitleTextSub>Sign up here to get in queue.</TitleTextSub>
-              </>
-            }
-          >
-            <View height="50%" width="50%" maxWidth={600} minWidth={400} margin="auto">
-              <Form space>
-                <Input flex={0} size={2} sizeRadius={2} />
+          {/* offset header stripe */}
+          <View height={20} />
 
-                <Button alt="confirm" size={2} sizeRadius={2} margin={[0, '20%']}>
-                  Signup
-                </Button>
-              </Form>
-            </View>
-          </SpacedPageContent>
+          <View margin={['auto', 0]}>
+            <SpacedPageContent
+              header={
+                <>
+                  <PillButton>Beta</PillButton>
+                  <Space size="sm" />
+                  <TitleText size="xxl">Early Access.</TitleText>
+                  <TitleTextSub>
+                    Orbit is now in beta and working with teams to solve their internal tools
+                    problems.
+                  </TitleTextSub>
+                  <TitleTextSub>Sign up here to get in queue.</TitleTextSub>
+                </>
+              }
+            >
+              <View width="50%" maxWidth={600} minWidth={360} margin="auto">
+                <Form space>
+                  <Input
+                    placeholder="Email address..."
+                    flex={0}
+                    size={2}
+                    sizeRadius={2}
+                    sizePadding={2}
+                  />
+
+                  <Button alt="confirm" size={2} sizeRadius={2} margin={[0, '20%']}>
+                    Signup
+                  </Button>
+                </Form>
+              </View>
+            </SpacedPageContent>
+          </View>
         </Page.Content>
 
         <Page.Background background={theme => theme.background} top={80} />
--- projects/site/src/pages/HomePage/SecuritySection.tsx
+++ projects/site/src/pages/HomePage/SecuritySection.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import bottomLightSeparator from '../../../public/images/bottom-sep.svg'
 import lightSeparator from '../../../public/images/light-separator.svg'
 import people from '../../../public/images/people.svg'
+import { useScreenSize } from '../../hooks/useScreenSize'
 import { Page } from '../../views/Page'
 import { PillButton } from '../../views/PillButton'
 import { TitleText } from '../../views/TitleText'
@@ -10,6 +11,7 @@ import { TitleTextSub } from './AllInOnePitchDemoSection'
 import { SpacedPageContent } from './SpacedPageContent'
 
 export function WaistSection(props) {
+  const screen = useScreenSize()
   return (
     <Theme name="light">
       <Page {...props}>
@@ -20,9 +22,11 @@ export function WaistSection(props) {
                 transform={{ scaleX: -1 }}
                 position="absolute"
                 top={-120}
+                height={120}
                 left={0}
                 right={0}
                 width="100%"
+                minWidth={1200}
                 src={lightSeparator}
               />
 
@@ -46,24 +50,23 @@ export function WaistSection(props) {
                 <TitleText size="lg" fontWeight={300} maxWidth={400}>
                   Tell Security they can firewall Orbit completely.
                 </TitleText>
-                <Space />
               </>
             }
           >
-            <Grid space="15%" itemMinWidth={350}>
+            <Grid space={screen === 'small' ? 20 : '15%'} itemMinWidth={350}>
               <Col space="xl">
-                <Pitch size={1.2}>
+                <Pitch size="md">
                   Orbit never sends a single bit of data outside your firewall.
                 </Pitch>
 
-                <Pitch>
-                  For the first time you can trust a startup to power all of your internal tools --
-                  from interfacing with sensitive internal databases to visualizing imporant
-                  metrics.
+                <Pitch size="xs">
+                  Get incredibly powerful internal tools, without having to trust a startup with any
+                  of your internal data -- from interfacing with sensitive internal databases to
+                  visualizing imporant metrics, Orbit runs locally so you have control.
                 </Pitch>
               </Col>
 
-              <Image src={people} />
+              <Image margin="auto" padding={20} src={people} />
             </Grid>
           </SpacedPageContent>
         </Page.Content>

f19eba86e1c78f879555f5a7d330bb2799f0fefe
natew <natewienert@gmail.com>
2019-04-16 21:28:27 -0700

    save

--- projects/site/surge-fix.sh
+++ projects/site/surge-fix.sh
@@ -4,10 +4,9 @@ cp dist/index.html dist/about.html
 cp public/stars.html dist/stars.html
 
 # sub-pages
-cp dist/index.html dist/use-cases.html
 cp dist/index.html dist/about.html
+cp dist/index.html dist/apps.html
 cp dist/index.html dist/privacy.html
 cp dist/index.html dist/terms.html
-cp dist/index.html dist/blog.html
 
 echo '*' > dist/CORS

785bdedf89d50fa716f27661d3358ba2d1d3d947
natew <natewienert@gmail.com>
2019-04-19 11:59:15 -0700

    save

--- projects/site/src/views/IntroText.tsx
+++ projects/site/src/views/IntroText.tsx
@@ -1,5 +1,6 @@
 import { TextProps } from '@o/ui'
 import React from 'react'
+
 import { Text } from './Text'
 
-export const IntroText = (props: TextProps) => <Text size={1.65} sizeLineHeight={1.3} {...props} />
+export const IntroText = (props: TextProps) => <Text size={1.8} sizeLineHeight={1.3} {...props} />

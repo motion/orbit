b5caff2c38288c083fc01a00e62c5a569a9c3683
natew <natewienert@gmail.com>
2019-04-21 16:44:20 -0700

    add source

--- projects/site/src/pages/DocsPage/Example.tsx
+++ projects/site/src/pages/DocsPage/Example.tsx
@@ -1,13 +1,41 @@
 import { Section, SectionProps, Space } from '@o/ui'
 import React from 'react'
 
-export function Example({ children, ...props }: SectionProps) {
+import { CodeBlock } from '../../views/CodeBlock'
+
+export function Example({
+  source,
+  examples,
+  id,
+  ...props
+}: SectionProps & { source: string; examples: any; id: string }) {
+  console.log(source, id)
+  if (!source || !id) {
+    return props.children || null
+  }
   return (
     <>
       <Section space {...props}>
-        {children}
+        {examples[id]}
+      </Section>
+      <Space />
+      <Section>
+        <CodeBlock language="typescript">{parseSource(source, id) || ''}</CodeBlock>
       </Section>
       <Space size="xl" />
     </>
   )
+}
+
+function parseSource(source: string, id: string) {
+  const blocks = source.split(/\nexport /g)
+  const keyBlock = blocks.find(x => x.split('\n')[0].indexOf(id) > -1)
+  const allLines = keyBlock.split('\n')
+  const lines = removeIndent(allLines.slice(1, allLines.length - 2))
+  return lines.join('\n')
+}
+
+const removeIndent = (lines: string[]) => {
+  const spacePad = lines.reduce((a, b) => Math.min(a, b.search(/\S/)), 100)
+  return lines.map(line => line.slice(spacePad))
 }
--- projects/site/src/views/CodeBlock.tsx
+++ projects/site/src/views/CodeBlock.tsx
@@ -2,13 +2,13 @@ import { gloss } from '@o/ui'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import React from 'react'
 
-export function CodeBlock(props) {
-  const language = props.className.replace('language-', '')
+export function CodeBlock({ className = 'language-typescript', ...props }) {
+  const language = props.language || className.replace('language-', '')
   return (
     <CodeBlockChrome>
       <Highlight {...defaultProps} code={props.children.trim()} language={language}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={{ ...style, padding: '20px' }}>
+        {({ className: finalClassName, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={finalClassName} style={{ ...style, padding: '20px' }}>
             {tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (

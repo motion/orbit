8170992aaa5a24f6c2d8c6d8d79dee463dd7f063
natew <natewienert@gmail.com>
2019-04-21 15:38:33 -0700

    save

--- projects/site/src/configurations.ts
+++ projects/site/src/configurations.ts
@@ -1,5 +1,6 @@
 import { ThemeMaker } from '@o/gloss'
 import { configureHotKeys, configureUI, toColor } from '@o/ui'
+import { configureUseStore, debugUseStore, IS_STORE } from '@o/use-store'
 import { setConfig } from 'react-hot-loader'
 import ResizeObserver from 'resize-observer-polyfill'
 
@@ -25,17 +26,44 @@ function configure() {
     },
   })
 
-  // just for now since its spitting out so many
-  setConfig({
-    logLevel: 'no-errors-please',
-    pureSFC: true,
-    pureRender: true,
-    // disableHotRenderer: true,
-  })
-
   configureHotKeys({
     ignoreTags: [],
   })
+
+  if (process.env.NODE_ENV === 'development') {
+    // just for now since its spitting out so many
+    setConfig({
+      logLevel: 'no-errors-please',
+      pureSFC: true,
+      pureRender: true,
+      // disableHotRenderer: true,
+    })
+
+    // dev tools for stores
+    configureUseStore({
+      debugStoreState: true,
+    })
+
+    debugUseStore(event => {
+      if (event.type === 'state') {
+        globalizeStores(event.value)
+      }
+    })
+
+    function globalizeStores(stores: Object) {
+      window['Stores'] = stores
+      // if we can, put store right on window
+      for (const key in stores) {
+        if (window[key]) {
+          if (Array.isArray(window[key]) && !window[key][0][IS_STORE]) return
+          if (!window[key][IS_STORE]) return
+          window[key] = stores[key]
+        } else {
+          window[key] = stores[key]
+        }
+      }
+    }
+  }
 }
 
 configure()

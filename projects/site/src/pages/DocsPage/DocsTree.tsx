bc460fe1f84d432e6a45f2b03ea960fb05eb820b
natew <natewienert@gmail.com>
2019-04-24 13:55:32 -0700

    save

--- projects/site/src/pages/DocsPage/DocsTree.tsx
+++ projects/site/src/pages/DocsPage/DocsTree.tsx
@@ -2,40 +2,40 @@ import { Tree } from '@o/ui'
 import produce from 'immer'
 import React, { useState } from 'react'
 
-const treeData = {
-  0: {
-    id: 0,
-    name: 'Root Item',
-    expanded: true,
-    children: [1, 2],
-  },
-  1: {
-    id: 1,
-    name: 'test one',
-    expanded: false,
-    children: [3, 4],
-  },
-  2: {
-    id: 2,
-    name: 'lorem ipsum dolor',
-    expanded: false,
-    children: [],
-  },
-  3: {
-    id: 3,
-    name: 'sit amet',
-    expanded: false,
-    children: [],
-  },
-  4: {
-    id: 4,
-    name: 'test two',
-    expanded: false,
-    children: [],
-  },
-}
-
 export let Simple = () => {
+  const treeData = {
+    0: {
+      id: 0,
+      name: 'Root Item',
+      expanded: true,
+      children: [1, 2],
+    },
+    1: {
+      id: 1,
+      name: 'test one',
+      expanded: false,
+      children: [3, 4],
+    },
+    2: {
+      id: 2,
+      name: 'lorem ipsum dolor',
+      expanded: false,
+      children: [],
+    },
+    3: {
+      id: 3,
+      name: 'sit amet',
+      expanded: false,
+      children: [],
+    },
+    4: {
+      id: 4,
+      name: 'test two',
+      expanded: false,
+      children: [],
+    },
+  }
+
   const [treeState, setTreeState] = useState(treeData)
   const [selected, setSelected] = useState(0)
 

ca25f9bcc6017d25745fc871972552eb9794f947
natew <natewienert@gmail.com>
2019-04-25 13:56:59 -0700

    better code

--- projects/site/src/views/CodeBlock.tsx
+++ projects/site/src/views/CodeBlock.tsx
@@ -1,24 +1,38 @@
 import './duotone-dark.css'
 
-import { gloss } from '@o/ui'
-import { javascript, tsx } from 'illuminate-js/lib/languages'
-import React from 'react'
-import { Illuminate } from 'react-Illuminate'
+import { gloss, useTheme } from '@o/ui'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import lightTheme from 'prism-react-renderer/themes/duotoneLight'
+import darkTheme from 'prism-react-renderer/themes/nightOwl'
+import React, { memo } from 'react'
 
-Illuminate.addLanguage('js', javascript)
-Illuminate.addLanguage('tsx', tsx)
-
-export function CodeBlock(props) {
+export const CodeBlock = memo(props => {
+  const theme = useTheme()
   // const language = props.language || 'tsx'
   // console.log('props.language', props.language)
   return (
     <CodeBlockChrome>
-      <Illuminate lineNumbers lang="tsx">
-        {props.children.trim()}
-      </Illuminate>
+      <Highlight
+        {...defaultProps}
+        code={`${props.children}`}
+        language="jsx"
+        theme={theme.background.isDark() ? darkTheme : lightTheme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     </CodeBlockChrome>
   )
-}
+})
 
 const CodeBlockChrome = gloss({
   fontSize: 14,

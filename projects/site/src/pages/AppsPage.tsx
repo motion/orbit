ed8da3d8b5c0f1a5189781bbf439b1b19d4198c6
natew <natewienert@gmail.com>
2019-04-26 15:10:43 -0700

    save

--- projects/site/src/pages/AppsPage.tsx
+++ projects/site/src/pages/AppsPage.tsx
@@ -3,6 +3,7 @@ import { mount, route } from 'navi'
 import React, { memo, useEffect, useState } from 'react'
 
 import { Header } from '../views/Header'
+import { PillButton } from '../views/PillButton'
 import { SearchInput } from '../views/SearchInput'
 import { SectionContent } from '../views/SectionContent'
 import { BlogFooter } from './BlogPage/BlogLayout'
@@ -21,7 +22,7 @@ export function AppsPage() {
     <Theme name={AppsPage.theme}>
       <FullScreen
         position="fixed"
-        backgroundSize="200%"
+        backgroundSize="cover"
         backgroundRepeat="no-repeat"
         backgroundPosition="bottom center"
         right={-200}
@@ -43,6 +44,73 @@ export function AppsPage() {
           </Col>
         </SectionContent>
 
+        <SectionContent minHeight={400} background="#000">
+          <Col
+            pad={[100, '20%', 100]}
+            space="xxl"
+            spaceAround
+            flex={1}
+            overflow="hidden"
+            className="content"
+          >
+            <PillButton>FAQ</PillButton>
+
+            <Space />
+
+            <FAQItem
+              question="What exactly is an Orbit app?"
+              main="Orbit apps can either provide data, or show data. Any app can then access another apps
+              data, with permission."
+              paragraphs={[
+                <>
+                  This is a new way to think about apps: as small services that can talk to each
+                  other.
+                </>,
+                <>
+                  For example, the Slack app has a `getMessagesInRoom()` method on it's API, along
+                  with every other one from the public Slack API.
+                </>,
+              ]}
+            />
+
+            <FAQItem
+              question="How do apps communicate?"
+              main="There are two ways: either they sync data in, or expose an API."
+              paragraphs={[
+                <>
+                  The API is used for "one time only" access. Any app can use another API to and
+                  then show the results. When you use `useApp()`, you get back an entire API thats
+                  been converted into a loadable, suspense-style call you can make inline in your
+                  views.
+                </>,
+                <>
+                  Meanwhile, sync functionality is more interesting. Any app can create data at
+                  runtime with the `useAppData` hook. But if they want to sync a larger amount of
+                  data for searching, they can define a sync process and save bits. This puts data
+                  into a local SQLite database.
+                </>,
+              ]}
+            />
+
+            <FAQItem
+              question="How do apps sync without a server?"
+              main="A combination of doing local syncing directly with OAuth services, with a light
+              p2p layer that handles coordinating configuration, auth, and simple appData."
+              paragraphs={[
+                <>
+                  Orbit began as a knowledge unification app, and we realized early on that there
+                  was a fundamental misalignment between syncing large amounst of data and user
+                  experience, trust, privacy, and security.
+                </>,
+                <>
+                  So, we developed a system that puts the user first. It lets you write apps that
+                  run behind your firewall.
+                </>,
+              ]}
+            />
+          </Col>
+        </SectionContent>
+
         <View flex={1} />
 
         <BlogFooter />
@@ -54,6 +122,29 @@ export function AppsPage() {
 AppsPage.theme = 'home'
 
 const dim = 180
+
+const FAQItem = ({ main, paragraphs, question }) => {
+  return (
+    <>
+      <Col space="xl">
+        <Title selectable size={1} fontWeight={600}>
+          {question}
+        </Title>
+
+        <Title selectable size={3} fontWeight={100}>
+          {main}
+        </Title>
+
+        {paragraphs.map((p, i) => (
+          <Title key={i} selectable size={1.5} alpha={0.6} fontWeight={100} sizeLineHeight={1.5}>
+            {p}
+          </Title>
+        ))}
+      </Col>
+      <Space size="xxxl" />
+    </>
+  )
+}
 
 const AppSearch = memo(() => {
   let [results, setResults] = useState(appElements)

c3d198fc3548195a6e2a98ea198429a273534504
natew <natewienert@gmail.com>
2019-04-26 18:27:56 -0700

    save

--- projects/site/src/pages/DocsPage/DocsSection.tsx
+++ projects/site/src/pages/DocsPage/DocsSection.tsx
@@ -6,3 +6,9 @@ export let Simple = (
     <Text>Some text goes here</Text>
   </Section>
 )
+
+export let Full = (
+  <Section bordered collapsable defaultCollapsed pad title="Hello World" subTitle="Section">
+    <Text>Some text goes here</Text>
+  </Section>
+)

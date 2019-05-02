ac1a51e821f458ba89dca417084a37836a4a5c69
natew <natewienert@gmail.com>
2019-05-02 10:34:03 -0700

    save

--- projects/site/src/pages/DocsPage/DocsGridLayout.tsx
+++ projects/site/src/pages/DocsPage/DocsGridLayout.tsx
@@ -1,8 +1,17 @@
-import { Card, GridLayout } from '@o/ui'
+import { Card, GridItem, GridLayout, useFetch } from '@o/ui'
 import React from 'react'
 
-export let Basic = (
-  <GridLayout>
-    <Card title="Slack Messages">lorem ipsum</Card>
-  </GridLayout>
-)
+export let Basic = () => {
+  const items = useFetch('https://jsonplaceholder.typicode.com/photos')
+  return (
+    <GridLayout>
+      {items.slice(0, 10).map((item, index) => (
+        <GridItem key={item.id} w={index === 0 ? 4 : 2} h={index === 0 ? 4 : 2}>
+          <Card flex={1} overflow="hidden" title={item.title}>
+            <img src={item.url} />
+          </Card>
+        </GridItem>
+      ))}
+    </GridLayout>
+  )
+}

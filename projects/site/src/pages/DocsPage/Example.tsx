60c56efa12ed2081a9fc099a1553ff8db88c9a30
natew <natewienert@gmail.com>
2019-05-03 15:01:44 -0700

    save

--- projects/site/src/pages/DocsPage/Example.tsx
+++ projects/site/src/pages/DocsPage/Example.tsx
@@ -161,9 +161,6 @@ function parseSource(source: string, id: string) {
         indent(allLines.slice(1, allLines.length - 2))
   // remove empty comment line which forces spacing
   const next = lines[0].trim() === '//' ? lines.slice(1, lines.length) : lines
-
-  console.log(id, next, keyBlock)
-
   return next.join('\n')
 }
 

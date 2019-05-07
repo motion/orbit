23753c3e61cc412c4e73b4bd994f85a77a26e2da
natew <natewienert@gmail.com>
2019-05-06 17:58:27 -0700

    save

--- apps/postgres-app/src/graph.node.ts
+++ apps/postgres-app/src/graph.node.ts
@@ -1,6 +1,21 @@
 import { AppBit } from '@o/kit'
 import postgraphile from 'postgraphile'
 
+import { PostgresAppData } from './PostgresModels'
+
 export async function graph(app: AppBit<PostgresAppData>) {
-  return postgraphile(app.data)
+  const c = app.data.credentials
+  return postgraphile(
+    {
+      user: c.username,
+      host: c.hostname,
+      database: c.database,
+      password: c.password,
+      port: +c.port,
+    },
+    'public',
+    {
+      // watchPg: true
+    },
+  )
 }

ca0b2f15a0b5908cba75e98106ada18c670f9cd7
natew <natewienert@gmail.com>
2019-05-08 19:09:47 -0700

    save

--- app/orbit-app/src/om/actions.ts
+++ app/orbit-app/src/om/actions.ts
@@ -1,4 +1,6 @@
+import { isObject } from 'lodash'
 import { Action } from 'overmind'
+import { isValidElement } from 'react'
 
 export const setNavVisible: Action<boolean> = ({ state }, x) => {
   state.navVisible = x
@@ -9,5 +11,18 @@ export const setNavHovered: Action<boolean> = ({ state }, x) => {
 }
 
 export const setShare: Action<{ key: string; value: any }> = ({ state }, { key, value }) => {
+  filterReactElementObj(value)
+  console.log('setting', value)
   state.share[key] = value
+}
+
+const filterReactElementObj = obj => {
+  for (const key in obj) {
+    if (isValidElement(obj)) {
+      obj[key] = null
+    }
+    if (obj[key] && isObject(obj[key])) {
+      filterReactElementObj(obj[key])
+    }
+  }
 }

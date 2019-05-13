62792537d9583e9d9d77d9997a70b3dd8486243b
natew <natewienert@gmail.com>
2019-05-13 13:26:41 -0700

    save

--- apps/github-app/src/graph.node.ts
+++ apps/github-app/src/graph.node.ts
@@ -1,19 +1,13 @@
 import { AppBit } from '@o/kit'
-import { createHttpLink } from 'apollo-link-http'
-import { introspectSchema, makeRemoteExecutableSchema } from 'graphql-tools'
 
 export async function graph(app: AppBit) {
-  const { token } = app
-  const link = createHttpLink({
-    uri: 'https://api.github.com/graphql',
-    fetch,
-    headers: {
-      Authorization: `bearer ${token}`,
+  return {
+    remoteHttpLink: {
+      uri: 'https://api.github.com/graphql',
+      fetch,
+      headers: {
+        Authorization: `bearer ${app.token}`,
+      },
     },
-  })
-  const introspectionSchema = await introspectSchema(link)
-  return makeRemoteExecutableSchema({
-    schema: introspectionSchema,
-    link,
-  })
+  }
 }

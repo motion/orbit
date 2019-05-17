f3526ff46582040168313a3c0b8d72880756d7bb
natew <natewienert@gmail.com>
2019-05-16 19:22:25 -0700

    save

--- projects/site/src/views/LogoHorizontal.tsx
+++ projects/site/src/views/LogoHorizontal.tsx
@@ -1,13 +1,15 @@
 import orbit from '!raw-loader!../../public/images/logomark-solid.svg'
-import { gloss, useTheme } from 'gloss'
 import { SVG, View, ViewProps } from '@o/ui'
+import { gloss, useTheme } from 'gloss'
 import React, { memo } from 'react'
 
-import { useLink } from "../LinkState";
+import { useLink } from '../LinkState'
 
 export const LogoHorizontal = memo((props: ViewProps & { slim?: boolean }) => {
   const theme = useTheme()
-  const scaleDown = 0.38 + (props.slim ? 0 : 0.075)
+  const scaleDown = 0.08 + (props.slim ? 0 : 0.075)
+  const w = 1441
+  const h = 442
 
   return (
     <View
@@ -18,14 +20,17 @@ export const LogoHorizontal = memo((props: ViewProps & { slim?: boolean }) => {
       padding={[0, 20]}
       margin={[0, 0]}
       transform={{
-        x: '-0.5%',
+        x: -2,
         y: -2,
+        scale: scaleDown,
       }}
+      width={w * scaleDown}
+      height={h * scaleDown}
       zIndex={100000}
       {...useLink('/')}
       {...props}
     >
-      <SVG width={`${313 * scaleDown}px`} height={`${96 * scaleDown}px`} svg={orbit} />
+      <SVG width={`${w}px`} height={`${h}px`} svg={orbit} />
     </View>
   )
 })

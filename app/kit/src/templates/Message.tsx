67fbd2871fae5a2ebaeda8ce38d54fd83d11ddbc
natew <natewienert@gmail.com>
2019-05-21 20:01:43 -0700

    save

--- app/kit/src/templates/Message.tsx
+++ app/kit/src/templates/Message.tsx
@@ -1,26 +1,22 @@
-import { PassProps, Space, SubTitle, Title, View } from '@o/ui'
+import { Col, PassProps, SubTitle, Title } from '@o/ui'
 import React from 'react'
+
 import { AppProps } from '../types/AppProps'
 import { Icon } from '../views/Icon'
 
 export function Message({ title, icon, subTitle, subType }: AppProps) {
   return (
-    <View flex={1} alignItems="center" justifyContent="center" padding={20}>
-      {!!title && <Title size={title.length > 40 ? 1.4 : 2.2}>{title}</Title>}
-      {!!subTitle && (
-        <>
-          <SubTitle>{subTitle}</SubTitle>
-        </>
-      )}
-      {!!subType && (
-        <>
-          <SubTitle>{subType}</SubTitle>
-        </>
-      )}
-      <Space />
-      <PassProps size={88}>
-        {typeof icon === 'string' ? <Icon name={icon} /> : icon || null}
-      </PassProps>
-    </View>
+    <Col flex={1} pad="lg" space="lg" alignItems="center" justifyContent="center">
+      <Col space="sm">
+        {!!title && <Title size={title.length > 40 ? 1.4 : 2.2}>{title}</Title>}
+        {!!subTitle && <SubTitle>{subTitle}</SubTitle>}
+        {!!subType && <SubTitle>{subType}</SubTitle>}
+      </Col>
+      <>
+        <PassProps size={128}>
+          {typeof icon === 'string' ? <Icon name={icon} /> : icon || null}
+        </PassProps>
+      </>
+    </Col>
   )
 }

6f5dbcd9053a8e6cd30f9363b4dd82cd01310cec
natew <natewienert@gmail.com>
2019-05-26 09:44:40 -0700

    save

--- apps/postgres-app/src/api.node.ts
+++ apps/postgres-app/src/api.node.ts
@@ -25,6 +25,7 @@ export default (app: any) => {
         connection = await connect(appData)
         console.log('querying...')
         const result = await connection.query(query, parameters)
+        console.log('got result yo', result)
         return result
       } catch (err) {
         console.log('error in postgres', err)

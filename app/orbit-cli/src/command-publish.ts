c6884aa8de7e24ed4b6e1c0966f810c5607aa35c
natew <natewienert@gmail.com>
2019-05-27 19:45:50 -0700

    save

--- app/orbit-cli/src/command-publish.ts
+++ app/orbit-cli/src/command-publish.ts
@@ -1,3 +1,6 @@
+import commandExists from 'command-exists'
+import exec from 'execa'
+
 import { commandBuild } from './command-build'
 import { reporter } from './reporter'
 
@@ -12,4 +15,19 @@ export async function commandPublish(options: CommandPublishOptions) {
 
 async function publishApp(options: CommandPublishOptions) {
   console.log('publish to verdaccio', options)
+  await npmCommand(`publish --registry http://example.com`)
+}
+
+async function npmCommand(args: string) {
+  const cmd = await yarnOrNpm()
+  await exec(cmd, args.split(' '))
+}
+
+async function yarnOrNpm() {
+  const hasYarn = await commandExists('yarn')
+  const hasNpm = await commandExists('npm')
+  if (!hasYarn && !hasNpm) {
+    throw new Error(`Neither npm or yarn installed, need one of them to continue.`)
+  }
+  return hasYarn ? 'yarn' : 'npm'
 }

8f6359bb774890adabadf696862f3aa883ef5a81
natew <natewienert@gmail.com>
2019-06-05 18:03:41 -0700

    remove old stu

--- app/orbit-electron/src/helpers/devTools.ts
+++ app/orbit-electron/src/helpers/devTools.ts
@@ -5,7 +5,5 @@ export let devTools = null
 if (process.env.NODE_ENV === 'development') {
   const tools = require('@o/reactron/devtools')
 
-  // const devToolsReactNew = join(require.resolve('@o/react-devtools'), '..')
-
   devTools = [tools.mobx, tools.react /* devToolsReactNew */]
 }

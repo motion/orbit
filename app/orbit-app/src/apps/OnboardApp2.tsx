642c7b96960e0bea822f393728425436e990e879
natew <natewienert@gmail.com>
2019-06-10 19:50:16 -0700

    more onboard

--- app/orbit-app/src/apps/OnboardApp2.tsx
+++ app/orbit-app/src/apps/OnboardApp2.tsx
@@ -1,17 +1,40 @@
-import { command, useStore } from '@o/kit'
-import { CheckProxyCommand } from '@o/models'
-import { Col, Flow, gloss, Paragraph, Space, SubTitle, Text, Title, useCreateFlow, useOnMount, View } from '@o/ui'
+import { command, save } from '@o/kit'
+import { CheckProxyCommand, SetupProxyCommand, Space, SpaceModel } from '@o/models'
+import { Col, Flow, gloss, Paragraph, Space, SubTitle, Text, Title, useCreateFlow, useOnMount } from '@o/ui'
 import React from 'react'
 
+import { om } from '../om/om'
 import BlurryGuys from '../pages/OrbitPage/BlurryGuys'
 
+async function checkProxy() {
+  return await command(CheckProxyCommand)
+}
+
+async function validateProxy() {
+  return await command(SetupProxyCommand)
+}
+
+async function finishOnboard(space: Space) {
+  om.actions.router.showHomePage()
+  await save(SpaceModel, {
+    ...space,
+    onboarded: true,
+  })
+}
+
 export function OnboardApp() {
-  const onboardStore = useStore(OnboardStore)
   const flow = useCreateFlow()
 
-  useOnMount(async () => {
-    if (await onboardStore.checkProxy()) {
-      flow.next()
+  useOnMount(() => {
+    if (flow.index === 0) {
+      let tm = setTimeout(() => {
+        checkProxy().then(res => {
+          res && flow.next()
+        })
+      }, 1000)
+      return () => {
+        clearTimeout(tm)
+      }
     }
   })
 
@@ -20,15 +43,20 @@ export function OnboardApp() {
       <BlurryGuys />
       <Col width="80%" margin="auto" height="90%">
         <Flow useFlow={flow}>
-          <Flow.Step title="Proxy" validateFinished={() => command(CheckProxyCommand)}>
+          <Flow.Step title="Proxy" validateFinished={validateProxy}>
             {OnboardStepProxy}
           </Flow.Step>
           <Flow.Step title="Workspace">{OnboardStepProxy}</Flow.Step>
+          <Flow.Step validateFinished={finishOnboard}>final setp</Flow.Step>
         </Flow>
       </Col>
     </>
   )
 }
+
+const IntroPara = props => (
+  <Paragraph textAlign="left" alpha={0.9} size={1.2} sizeLineHeight={1.025} />
+)
 
 function OnboardStepProxy() {
   return (
@@ -38,21 +66,17 @@ function OnboardStepProxy() {
           <Text size={2.8} fontWeight={500}>
             Hello.
           </Text>
-          <View height={10} />
           <Text size={1.75} alpha={0.5}>
             Welcome to Orbit
           </Text>
-          <View height={30} />
-          <Text selectable textAlign="left" size={1.2} sizeLineHeight={1.025} alpha={0.9}>
-            Orbit is your team knowledge manager.
-            <Space />
-            It gives you easy access to <b>shortcuts</b>, <b>people</b>, and <b>search</b> within
-            your company without exposing any of your team data to us. To do so it runs privately
-            each persons computer.
-            <Space />
+          <IntroPara>Orbit is your team knowledge manager.</IntroPara>
+          It gives you easy access to <b>shortcuts</b>, <b>people</b>, and <b>search</b> within your
+          company without exposing any of your team data to us. To do so it runs privately each
+          persons computer.
+          <IntroPara>
             Orbit will set up a local proxy now to enable private sync and the access quick URLs you
             can access in your browser.
-          </Text>
+          </IntroPara>
         </Centered>
       )}
       {false === false && (
@@ -62,22 +86,21 @@ function OnboardStepProxy() {
             Orbit had a problem setting up a proxy on your machine. Feel free to get in touch with
             us if you are having issues:
           </SubTitle>
-          <Paragraph>
+          <IntroPara>
             <strong>
               <a href="mailto:help@tryorbit.com">help@tryorbit.com</a>
             </strong>
-          </Paragraph>
-          <Paragraph>
+          </IntroPara>
+          <IntroPara>
             <strong>Error message:</strong>
-          </Paragraph>
+          </IntroPara>
         </Centered>
       )}
       {false === true && (
-        <Centered>
+        <Centered space>
           <Text size={2.2} fontWeight={600}>
             Success!
           </Text>
-          <Space />
           <Text size={1.5} alpha={0.5}>
             Lets set you up...
           </Text>
@@ -93,50 +116,3 @@ const Centered = gloss(Col, {
   justifyContent: 'center',
   textAlign: 'center',
 })
-
-class OnboardStore {
-  acceptedMessage = ''
-  accepted = null
-  pendingMove = false
-
-  stateActions = {
-    0: async () => {
-      if (this.accepted !== true) {
-        try {
-          this.accepted = await command(SetupProxyCommand)
-        } catch (err) {
-          this.acceptedMessage = err
-        }
-        if (this.accepted) {
-          // show message for a sec
-          this.acceptedMessage = 'Successfully setup proxy'
-          await sleep(1500)
-        }
-      }
-    },
-    1: () => {},
-    2: async () => {
-      om.actions.router.showHomePage()
-      // save setting
-      const user = await loadOne(UserModel, {})
-      save(UserModel, {
-        ...user,
-        settings: {
-          ...user.settings,
-          hasOnboarded: true,
-        },
-      })
-    },
-  }
-
-  prevFrame = () => {
-    this.curFrame--
-  }
-
-  nextFrame = async () => {
-    this.pendingMove = true
-    await this.stateActions[this.curFrame]()
-    this.curFrame++
-    this.pendingMove = false
-  }
-}

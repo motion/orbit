acad66009c264a964b12168b2c4f739538e4b76f
natew <natewienert@gmail.com>
2019-06-26 16:26:57 -0700

    save

--- app/orbit-repl/src/dev.ts
+++ app/orbit-repl/src/dev.ts
@@ -6,16 +6,7 @@ Error.stackTraceLimit = Infinity
 // they dont work well (you can only access globals not see logs)
 // so lets avoid clutter unless absolutely wanted
 
-const {
-  DISABLE_WORKERS,
-  DEBUG_ELECTRON,
-  DEBUG_ELECTRON_MAIN = 'true',
-  DEBUG_ELECTRON_APPS,
-} = process.env
-
-const debugElectron = DEBUG_ELECTRON === 'true'
-const debugElectronMain = DEBUG_ELECTRON_MAIN === 'true'
-const debugElectronApps = DEBUG_ELECTRON_APPS === 'true'
+const { DISABLE_WORKERS } = process.env
 
 async function start() {
   const sessions = [
@@ -23,18 +14,18 @@ async function start() {
     { port: '9000' }, // desktop
 
     // electron + remote
-    (debugElectron || debugElectronMain) && { port: '9001' },
+    { port: '9001' },
     { port: '9002' },
 
     // syncers
     !DISABLE_WORKERS && { port: '9003' },
 
     // electron apps (look for a few)
-    (debugElectron || debugElectronApps) && { port: '9004' },
+    { port: '9004' },
     { port: '9005' },
-    (debugElectron || debugElectronApps) && { port: '9006' },
+    { port: '9006' },
     { port: '9007' },
-    (debugElectron || debugElectronApps) && { port: '9008' },
+    { port: '9008' },
     { port: '9009' },
   ].filter(Boolean)
 

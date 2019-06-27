be5f5b6e52db094b347b748581da92c799c0f514
natew <natewienert@gmail.com>
2019-06-27 12:16:33 -0700

    improve template

---
+++ app/example-workspace/workspace-app/main.tsx
@@ -0,0 +1,20 @@
+import { App, createApp } from '@o/kit'
+import React from 'react'
+
+export default createApp({
+  id: 'demo-app-flow',
+  name: 'App Demo: Flow',
+  icon: 'flow',
+  iconColors: ['rgb(255, 133, 27)', 'rgb(235, 123, 17)'],
+  app: () => (
+    <App>
+      <AppMain />
+    </App>
+  ),
+})
+
+function AppMain() {
+  return (
+
+  )
+}

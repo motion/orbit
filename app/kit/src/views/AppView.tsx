45cd4009e01adafbf06ef430b28ca1ffb8d8bace
natew <natewienert@gmail.com>
2019-07-01 10:31:08 -0700

    save

--- app/kit/src/views/AppView.tsx
+++ app/kit/src/views/AppView.tsx
@@ -1,10 +1,9 @@
 import { isEqual } from '@o/fast-compare'
 import { AppViewProps } from '@o/models'
-import { CenteredText, ErrorBoundary, ItemPropsProviderSmall, Loading, memoIsEqualDeep } from '@o/ui'
+import { ErrorBoundary, ItemPropsProviderSmall, Loading, memoIsEqualDeep } from '@o/ui'
 import { Contents } from 'gloss'
 import { capitalize } from 'lodash'
-import React, { createContext, forwardRef, Suspense, useContext, useEffect, useMemo, useRef } from 'react'
-import { findDOMNode } from 'react-dom'
+import React, { createContext, Suspense, useContext, useMemo, useRef } from 'react'
 
 import { useAppDefinition } from '../hooks/useAppDefinition'
 import { AppStore } from '../stores'
@@ -18,103 +17,67 @@ export type AppSubViewProps = {
   appStore?: AppStore
 }
 
-export type AppViewRef = {
-  hasView?: boolean
-}
-
-// !TODO make this nice
-function useHandleAppViewRef(ref: any, rootRef: any) {
-  useEffect(() => {
-    if (!ref) return
-    if (!rootRef.current) return
-    const domNode = findDOMNode(rootRef.current)
-    const hasView = !!domNode.firstChild
-    const current = { hasView }
-    if (typeof ref === 'function') {
-      ref(current)
-    } else {
-      // @ts-ignore
-      ref.current = current
-      return () => {
-        // @ts-ignore
-        ref.current = null
-      }
-    }
-  }, [ref, rootRef.current])
-}
-
 const ChildrenOnly = props => props.children
 
 const AppViewContext = createContext<Partial<AppSubViewProps>>({})
 
-export const AppView = memoIsEqualDeep(
-  forwardRef<AppViewRef, AppSubViewProps>(function AppView({ appProps, ...props }, ref) {
-    const prev = useContext(AppViewContext)
-    const rootRef = useRef<HTMLDivElement>(null)
-    const context = useMemo(() => ({ [capitalize(props.viewType)]: ChildrenOnly } as any), [
-      props.viewType,
-    ])
-    const definition = useAppDefinition(props.identifier)
+export const AppView = memoIsEqualDeep(function AppView({ appProps, ...props }) {
+  const prev = useContext(AppViewContext)
+  const rootRef = useRef<HTMLDivElement>(null)
+  const context = useMemo(() => ({ [capitalize(props.viewType)]: ChildrenOnly } as any), [
+    props.viewType,
+  ])
+  const definition = useAppDefinition(props.identifier)
 
+  const View = useMemo(() => {
     // prevent infinite loop of nesting, which can be relatively easy to do
     if (isEqual(prev, props)) {
       console.error(`Rendering the same view as a sub-view, preventing infinite loop.`)
       return null
     }
-
     if (!props.identifier) {
       console.log('props for error', props)
-      return <CenteredText>No identifier {props.identifier}</CenteredText>
+      return null
     }
-
-    let View = null
 
     if (!definition) {
       console.warn('no definition found', props)
-      return <CenteredText>No definition found for identifier {props.identifier}</CenteredText>
+      return null
     }
 
     if (props.viewType === 'setup' || props.viewType === 'settings') {
-      View = definition[props.viewType]
+      return definition[props.viewType]
     } else {
       const RenderApp = definition.app
-      View = next => (
+      return next => (
         <AppViewsContext.Provider value={context}>
           <RenderApp {...next} />
         </AppViewsContext.Provider>
       )
     }
+  }, [props.identifier, props.viewType, definition])
 
-    // handle ref
-    useHandleAppViewRef(ref, rootRef)
+  if (View === null) {
+    return null
+  }
 
-    if (!View) {
-      console.warn('no view', props, definition)
-      return null
-    }
-    if (typeof View !== 'function') {
-      console.warn('invalid view', View, props, appProps)
-      return null
-    }
+  const element = (
+    <AppViewContext.Provider value={props}>
+      <Contents ref={rootRef}>
+        <ErrorBoundary name={`App: ${props.identifier}`}>
+          <Suspense fallback={<Loading />}>
+            <View {...props} {...appProps} />
+          </Suspense>
+        </ErrorBoundary>
+      </Contents>
+    </AppViewContext.Provider>
+  )
 
-    const element = (
-      <AppViewContext.Provider value={props}>
-        <Contents ref={rootRef}>
-          <ErrorBoundary name={`App: ${props.identifier}`}>
-            <Suspense fallback={<Loading />}>
-              <View {...props} {...appProps} />
-            </Suspense>
-          </ErrorBoundary>
-        </Contents>
-      </AppViewContext.Provider>
-    )
+  // small rendering for index views
+  if (props.viewType === 'index') {
+    return <ItemPropsProviderSmall>{element}</ItemPropsProviderSmall>
+  }
 
-    // small rendering for index views
-    if (props.viewType === 'index') {
-      return <ItemPropsProviderSmall>{element}</ItemPropsProviderSmall>
-    }
-
-    // regular rendering for others
-    return element
-  }),
-)
+  // regular rendering for others
+  return element
+})
--- app/orbit-app/src/apps/orbitApps.tsx
+++ app/orbit-app/src/apps/orbitApps.tsx
@@ -1,14 +1,8 @@
-import {
-  __SERIOUSLY_SECRET,
-  AppDefinition,
-  configureKit,
-  createApp,
-  useAppDefinitions,
-} from '@o/kit'
+import { __SERIOUSLY_SECRET, AppDefinition, configureKit, createApp, useAppDefinitions } from '@o/kit'
 import { Desktop } from '@o/stores'
 import { Loading } from '@o/ui'
 import { reaction } from 'mobx'
-import { createElement } from 'react'
+import React from 'react'
 
 import { StoreContext } from '../StoreContext'
 import AppsApp from './apps/AppsApp'
@@ -27,6 +21,10 @@ async function updateDefinitions() {
   const rawApps = require('../../appDefinitions.js')
   console.log('rawApps', rawApps)
   dynamicApps = Object.keys(rawApps).map(simpleKey => rawApps[simpleKey].default)
+  //
+  // new idea:
+  // just number the apps and hardcode them tosomething like `@o/app-1` `@o-app2`
+  //
   // this doesn't work because dynamic require doesn't use manifest
   // TODO we need to do something like this
   // const apps = await command(AppGetWorkspaceAppsCommand)
@@ -53,7 +51,10 @@ const LoadingApp = createApp({
   name: 'Loading...',
   icon: '',
   id: 'loading',
-  app: () => createElement(Loading),
+  app: () => {
+    console.log('LOADING')
+    return <Loading />
+  },
 })
 
 // apps we use internally in orbit

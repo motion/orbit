a387546d247eb56a9693f4785c83bd300ec80336
natew <natewienert@gmail.com>
2019-07-05 11:46:47 -0700

    golf

--- projects/playground/src/TestCarousel.tsx
+++ projects/playground/src/TestCarousel.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Row, Title, View } from '@o/ui'
+import { animation, Button, Card, Geometry, Row, Title, View } from '@o/ui'
 import React from 'react'
 
 export const TestCarousel = () => {
@@ -11,7 +11,7 @@ export const TestCarousel = () => {
    * animation() is a helper, but desugars into props somewhat like:
    *    scrollLeft={spring(...)}
    *    animated
-   * it lets us control it using .set without being fully controlled
+   * lets us control it using .set without being fully controlled
    */
   return (
     <View width="100%" height="100%">
@@ -19,25 +19,25 @@ export const TestCarousel = () => {
       <Button onClick={() => carousel.set(cur => ({ scrollLeft: 100 + cur.scrollLeft }))}>
         scroll 100px right
       </Button>
-      <Row flex={1} scrollable {...carousel}>
+      <Row flex={1} scrollable="x" space={100} {...carousel}>
         {apps.map((app, index) => (
-          <Geometry key={index} leftIndex={index}>
+          <Geometry key={index}>
             {geometry => (
               <Card
                 onClick={() => {
                   carousel.set({
-                    scrollLeft: geometry.frame(),
+                    scrollLeft: geometry.offset(index),
                   })
                 }}
                 onDoubleClick={() => {
                   carousel.set(current => ({
-                    scrollLeft: geometry.frame(),
+                    scrollLeft: geometry.offset(index),
                     zoomed: !current.zoomed,
                   }))
                 }}
                 transform={{
-                  rotateY: () => (index - geometry.frame()) * 10,
-                  scale: () => (carousel.zoomed ? 1 : Math.abs(geometry.frame())),
+                  rotateY: () => (index - geometry.frame(index)) * 10,
+                  scale: () => (carousel.zoomed ? 1 : Math.abs(geometry.frame(index))),
                 }}
               >
                 <Title>{app.title}</Title>
@@ -50,5 +50,53 @@ export const TestCarousel = () => {
   )
 }
 
-const Geometry: any = null
-const animation: any = () => {}
+// alternate
+
+export const TestCarousel2 = () => {
+  const apps = [{ title: 'ok' }, { title: 'ok2' }, { title: 'ok3' }, { title: 'o4' }]
+  const carousel = animation(() => ({
+    scrollLeft: 0,
+    zoomed: false,
+  }))
+  /**
+   * animation() is a helper, but desugars into props somewhat like:
+   *    scrollLeft={spring(...)}
+   *    animated
+   * lets us control it using .set without being fully controlled
+   */
+  return (
+    <View width="100%" height="100%">
+      <Button onClick={() => carousel.set(cur => ({ zoomed: !cur.zoomed }))}>zoom</Button>
+      <Button onClick={() => carousel.set(cur => ({ scrollLeft: 100 + cur.scrollLeft }))}>
+        scroll 100px right
+      </Button>
+      <Row flex={1} scrollable="x" space={100} {...carousel}>
+        {apps.map((app, index) => (
+          <Geometry key={index}>
+            {geometry => (
+              <Card
+                onClick={() => {
+                  carousel.set({
+                    scrollLeft: geometry.offset(index),
+                  })
+                }}
+                onDoubleClick={() => {
+                  carousel.set(current => ({
+                    scrollLeft: geometry.offset(index),
+                    zoomed: !current.zoomed,
+                  }))
+                }}
+                transform={{
+                  rotateY: () => (index - geometry.frame(index)) * 10,
+                  scale: () => (carousel.zoomed ? 1 : Math.abs(geometry.frame(index))),
+                }}
+              >
+                <Title>{app.title}</Title>
+              </Card>
+            )}
+          </Geometry>
+        ))}
+      </Row>
+    </View>
+  )
+}

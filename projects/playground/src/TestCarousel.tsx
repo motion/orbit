84d1f250e98908cacfaa72e2bf5b0b9291ac134b
natew <natewienert@gmail.com>
2019-07-05 11:47:13 -0700

    save

--- projects/playground/src/TestCarousel.tsx
+++ projects/playground/src/TestCarousel.tsx
@@ -54,41 +54,42 @@ export const TestCarousel = () => {
 
 export const TestCarousel2 = () => {
   const apps = [{ title: 'ok' }, { title: 'ok2' }, { title: 'ok3' }, { title: 'o4' }]
-  const carousel = animation(() => ({
-    scrollLeft: 0,
-    zoomed: false,
-  }))
-  /**
-   * animation() is a helper, but desugars into props somewhat like:
-   *    scrollLeft={spring(...)}
-   *    animated
-   * lets us control it using .set without being fully controlled
-   */
+  const carousel = useRef<Row>(null)
   return (
     <View width="100%" height="100%">
-      <Button onClick={() => carousel.set(cur => ({ zoomed: !cur.zoomed }))}>zoom</Button>
-      <Button onClick={() => carousel.set(cur => ({ scrollLeft: 100 + cur.scrollLeft }))}>
+      <Button onClick={() => carousel.current.setProp('zoomed', !carousel.current.props.zoomed)}>
+        zoom
+      </Button>
+      <Button onClick={() => carousel.current.setProp('scrollLeft', cur => cur + 100)}>
         scroll 100px right
       </Button>
-      <Row flex={1} scrollable="x" space={100} {...carousel}>
+      {/* this parts a bit sketchy */}
+      <Row
+        ref={carousel}
+        flex={1}
+        scrollable="x"
+        space={100}
+        animated={{ scrollLeft: true, zoomed: true }}
+        defaultScrollLeft={0}
+        defaultZoomed={false}
+      >
         {apps.map((app, index) => (
-          <Geometry key={index}>
+          <Geometry key={index} frame={carousel}>
             {geometry => (
               <Card
                 onClick={() => {
-                  carousel.set({
-                    scrollLeft: geometry.offset(index),
-                  })
+                  carousel.current.setProp('scrollLeft', geometry.offset(index))
                 }}
                 onDoubleClick={() => {
-                  carousel.set(current => ({
+                  carousel.current.setProps(cur => ({
                     scrollLeft: geometry.offset(index),
-                    zoomed: !current.zoomed,
+                    zoomed: !cur.zoomed,
                   }))
                 }}
                 transform={{
                   rotateY: () => (index - geometry.frame(index)) * 10,
-                  scale: () => (carousel.zoomed ? 1 : Math.abs(geometry.frame(index))),
+                  scale: () =>
+                    carousel.current.props.zoomed ? 1 : Math.abs(geometry.frame(index)),
                 }}
               >
                 <Title>{app.title}</Title>

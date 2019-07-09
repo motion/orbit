bd7d6bade0f194b0bbb05022e8cdd0d6b63f6c7f
natew <natewienert@gmail.com>
2019-07-08 20:52:33 -0700

    save

--- app/orbit-app/src/pages/OrbitPage/OrbitDraggableOverlay.tsx
+++ app/orbit-app/src/pages/OrbitPage/OrbitDraggableOverlay.tsx
@@ -3,4 +3,5 @@ import { useCurrentDraggable } from '@o/ui'
 export const OrbitDraggableOverlay = () => {
   const { item, position } = useCurrentDraggable()
   console.log('render me bitch', item, position)
+  return null
 }

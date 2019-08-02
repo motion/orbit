62d78eae1cffa30bc0cdf83154b72a9344fd032d
natew <natewienert@gmail.com>
2019-08-02 09:38:23 -0700

    save

--- packages/mobx-bridge/src/websocket.ts
+++ packages/mobx-bridge/src/websocket.ts
@@ -3,6 +3,7 @@ if (typeof window !== 'undefined') {
   // @ts-ignore
   WebSocket = window.WebSocket
 } else {
-  WebSocket = eval(`require('html5-websocket')`)
+  // prevent it from being imported by webpack
+  WebSocket = eval(`require('ws')`)
 }
 export default WebSocket

5a70c93ca1f38d1d4100bccc32cd5783df6ad36c
natew <natewienert@gmail.com>
2019-08-09 15:30:29 -0700

    save

--- app/kit/src/EventSourceManager.ts
+++ app/kit/src/EventSourceManager.ts
@@ -1,28 +1,18 @@
 export class EventSourceManager {
-  source: EventSource
+  source: EventSource | null = null
   listeners: any[] = []
+
   constructor(private url: string) {
     this.start()
   }
+
   start = () => {
     this.source = new EventSource(this.url)
     this.source.onopen = this.handleOnline
     this.source.onerror = this.handleDisconnect
     this.source.onmessage = this.handleMessage
   }
-  private handleOnline() {
-    console.debug('[HMR] connected')
-  }
-  private handleMessage = event => {
-    for (var i = 0; i < this.listeners.length; i++) {
-      this.listeners[i](event)
-    }
-  }
-  private handleDisconnect() {
-    console.log('[HMR] disconnected')
-    this.source.close()
-    setTimeout(this.start, 1000)
-  }
+
   addMessageListener(fn) {
     this.listeners.push(fn)
   }
@@ -34,6 +24,20 @@ export class EventSourceManager {
     this.listeners = []
   }
   close() {
-    this.source.close()
+    this.source && this.source.close()
+  }
+
+  private handleOnline() {
+    console.debug('[HMR] connected')
+  }
+  private handleMessage = event => {
+    for (var i = 0; i < this.listeners.length; i++) {
+      this.listeners[i](event)
+    }
+  }
+  private handleDisconnect() {
+    console.log('[HMR] disconnected')
+    this.source && this.source.close()
+    setTimeout(this.start, 1000)
   }
 }

7b1035c5e4557dc0ac9a3014354869987a10b726
natew <natewienert@gmail.com>
2019-08-13 17:41:01 -0700

    fixing build

--- app/build-orbit/stage-app/index.js
+++ app/build-orbit/stage-app/index.js
@@ -1,7 +1,6 @@
 // enforce production env
 process.env.NODE_ENV = 'production'
 process.env.ORBIT_VERSION = JSON.parse(require('fs').readFileSync('./package.json', 'utf8')).version
-process.env.FIRST_RUN = 'true'
 
 console.log('Starting Orbit version', process.env.ORBIT_VERSION)
 

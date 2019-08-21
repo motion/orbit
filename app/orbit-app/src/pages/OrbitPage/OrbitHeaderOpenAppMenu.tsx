4dd4d3597f18442c92baaa8a30b4497daae824b9
natew <natewienert@gmail.com>
2019-08-20 20:53:07 -0700

    save

--- app/orbit-app/src/pages/OrbitPage/OrbitHeaderOpenAppMenu.tsx
+++ app/orbit-app/src/pages/OrbitPage/OrbitHeaderOpenAppMenu.tsx
@@ -1,12 +1,13 @@
-import { command, openItem, useStore } from '@o/kit'
+import { command, openItem, useReaction, useStore } from '@o/kit'
 import { AppDevCloseCommand, AppDevOpenCommand } from '@o/models'
 import { App } from '@o/stores'
 import { Icon, ListSeparator, MenuButton, Row, Toggle, useBanner } from '@o/ui'
-import React, { memo } from 'react'
+import React, { memo, useMemo } from 'react'
 
 import { om, useOm } from '../../om/om'
 import { paneManagerStore } from '../../om/stores'
 import { useAppsCarousel } from './OrbitAppsCarouselStore'
+import { appsDrawerStore } from './OrbitAppsDrawer'
 import { headerButtonProps } from './OrbitHeader'
 
 export const OrbitHeaderOpenAppMenu = memo(
@@ -14,6 +15,10 @@ export const OrbitHeaderOpenAppMenu = memo(
     const { state, effects } = useOm()
     const { appRole } = useStore(App)
     const banner = useBanner()
+    const appsCarousel = useAppsCarousel()
+    const disabled = useReaction(() => {
+      return appsCarousel.focusedApp.identifier === 'setupApp' || appsDrawerStore.isOpen
+    })
 
     const constantMenuItems = [
       <ListSeparator key={1000}>App</ListSeparator>,
@@ -30,8 +35,70 @@ export const OrbitHeaderOpenAppMenu = memo(
       },
     ].filter(Boolean)
 
-    const appsCarousel = useAppsCarousel()
     const { isOnOpenableApp } = appsCarousel
+
+    const items = useMemo(() => {
+      return [
+        ...(isOnOpenableApp
+          ? [
+              <ListSeparator key={100}>Develop</ListSeparator>,
+              {
+                title: 'Edit',
+                icon: 'edit',
+                onClick: async e => {
+                  e.stopPropagation()
+                  // toggle
+                  banner.set({
+                    type: 'info',
+                    message: `Updating...`,
+                    timeout: 1.5,
+                  })
+                  let reply: any = null
+                  setIsDeveloping(x => !x)
+                  if (isDeveloping === false) {
+                    reply = await command(AppDevOpenCommand, {
+                      type: 'workspace',
+                      appId: appsCarousel.focusedApp.id!,
+                      identifier: appsCarousel.focusedApp.identifier!,
+                    })
+                  } else {
+                    reply = await command(AppDevCloseCommand, {
+                      identifier: appsCarousel.focusedApp.identifier!,
+                    })
+                  }
+                  banner.set({
+                    type: reply.type,
+                    message: reply.message,
+                    timeout: 1.5,
+                  })
+                },
+                after: (
+                  <Row space>
+                    <Toggle checked={isDeveloping} />
+                    <Icon
+                      onClick={e => {
+                        e.stopPropagation()
+                        openItem(`file://Users/nw/motion/orbit`)
+                      }}
+                      name="code"
+                      tooltip="Open in VSCode"
+                    />
+                  </Row>
+                ),
+              },
+              {
+                title: 'Fork',
+                icon: 'fork',
+              },
+              {
+                title: 'Publish',
+                icon: 'export',
+              },
+            ]
+          : []),
+        ...constantMenuItems,
+      ]
+    }, [isOnOpenableApp, constantMenuItems])
 
     if (appRole === 'torn' || appRole === 'editing') {
       return (
@@ -48,69 +115,12 @@ export const OrbitHeaderOpenAppMenu = memo(
         onClick={effects.openCurrentApp}
         elevation={3}
         elevationShadowOpacity={0.2}
-        openIconProps={{
+        disabled={disabled}
+        opacity={disabled ? 0.5 : 1}
+        openIconProps={useMemo(() => {
           name: isDeveloping ? 'edit' : 'chevron-down',
-        }}
-        items={[
-          ...(isOnOpenableApp
-            ? [
-                <ListSeparator key={100}>Develop</ListSeparator>,
-                {
-                  title: 'Edit',
-                  icon: 'edit',
-                  onClick: async e => {
-                    e.stopPropagation()
-                    // toggle
-                    banner.set({
-                      type: 'info',
-                      message: `Updating...`,
-                      timeout: 1.5,
-                    })
-                    let reply: any = null
-                    setIsDeveloping(x => !x)
-                    if (isDeveloping === false) {
-                      reply = await command(AppDevOpenCommand, {
-                        type: 'workspace',
-                        appId: appsCarousel.focusedApp.id!,
-                        identifier: appsCarousel.focusedApp.identifier!,
-                      })
-                    } else {
-                      reply = await command(AppDevCloseCommand, {
-                        identifier: appsCarousel.focusedApp.identifier!,
-                      })
-                    }
-                    banner.set({
-                      type: reply.type,
-                      message: reply.message,
-                      timeout: 1.5,
-                    })
-                  },
-                  after: (
-                    <Row space>
-                      <Toggle checked={isDeveloping} />
-                      <Icon
-                        onClick={e => {
-                          e.stopPropagation()
-                          openItem(`file://Users/nw/motion/orbit`)
-                        }}
-                        name="code"
-                        tooltip="Open in VSCode"
-                      />
-                    </Row>
-                  ),
-                },
-                {
-                  title: 'Fork',
-                  icon: 'fork',
-                },
-                {
-                  title: 'Publish',
-                  icon: 'export',
-                },
-              ]
-            : []),
-          ...constantMenuItems,
-        ]}
+        }, [isDeveloping])}
+        items={items}
       >
         Open
       </MenuButton>

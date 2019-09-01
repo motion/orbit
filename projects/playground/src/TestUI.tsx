6cea44d3a6a6b2219a0c27d3706d122a1dabd5f8
natew <natewienert@gmail.com>
2019-08-31 18:33:11 -0700

    start on framer motion experiments

--- projects/playground/src/TestUI.tsx
+++ projects/playground/src/TestUI.tsx
@@ -1,16 +1,50 @@
 import { Button, CardSimple, Col, Row } from '@o/ui'
+import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion'
 import _ from 'lodash'
 import * as React from 'react'
 
-import { TestUIEditor } from './TestUI/TestUIEditor'
-
 export function TestUI() {
   return (
-    <Row flex={1} overflow="hidden" height="100%">
+    <Row overflow="hidden" height="100%" width={window.innerWidth * 10} perspective="1000px">
       {/* <TestUIPopovers /> */}
       {/* <TestUIGlossSpeed /> */}
-      <TestUIEditor />
+      {/* <TestUIEditor /> */}
+      <TestUIMotion />
     </Row>
+  )
+}
+
+export function TestUIMotion() {
+  const scrollXProgress = useMotionValue(0)
+
+  React.useEffect(() => {
+    function updateCallback() {
+      scrollXProgress.set(window.pageXOffset / window.innerWidth)
+    }
+    window.addEventListener('scroll', updateCallback, { passive: true })
+  }, [])
+
+  return (
+    <>
+      {[1, 2, 3, 4, 5].map(i => (
+        <Card key={i} index={i} total={5} scrollXProgress={scrollXProgress} />
+      ))}
+    </>
+  )
+}
+
+function Card(props: any) {
+  const cardPosition = props.index + 1 / props.total
+  const ry = useTransform(props.scrollXProgress, x => {
+    // console.log(x, cardPosition, cardPosition / (x + 1))
+    return (cardPosition / (x + 1)) * 10
+  })
+  const rotateY = useSpring(ry, { stiffness: 400, damping: 90 })
+  return (
+    <motion.div
+      style={{ width: window.innerWidth, height: window.innerHeight, background: 'green', rotateY }}
+      animate={{ scale: 0.7 }}
+    />
   )
 }
 

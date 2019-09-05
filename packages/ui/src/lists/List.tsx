7a8ecb92d47923363323b27db7aefdc1419c1a0b
natew <natewienert@gmail.com>
2019-09-04 22:22:28 -0700

    save

--- packages/ui/src/lists/List.tsx
+++ packages/ui/src/lists/List.tsx
@@ -1,7 +1,6 @@
 import { isDefined, selectDefined } from '@o/utils'
 import { pick } from 'lodash'
-import React, { forwardRef, memo, useCallback, useContext, useEffect, useImperativeHandle, useMemo, useRef } from 'react'
-import { HotKeys, HotKeysProps } from 'react-hotkeys'
+import React, { forwardRef, useCallback, useContext, useEffect, useImperativeHandle, useMemo, useRef } from 'react'
 
 import { Center } from '../Center'
 import { splitCollapseProps } from '../Collapsable'
@@ -73,8 +72,6 @@ function toListItemProps(props?: any): ListItemSimpleProps & { item?: any } {
 const PropsContext = createContextualProps<ListProps>()
 export const ListPassProps = PropsContext.PassProps
 export const useListProps = PropsContext.useProps
-
-export type HandleOrbitSelect = (index: number, extraData: any) => any
 
 const nullFn = () => null
 
@@ -313,36 +310,4 @@ function ListPlaceholder(props: ListProps) {
 // @ts-ignore
 List.accepts = {
   surfaceProps: true,
-}
-
-export const ListShortcuts = memo(({ keyMap, handlers, ...props }: Partial<HotKeysProps>) => {
-  const shortcutStore = useShortcutStore()
-
-  const innerHandlers = useMemo(() => {
-    const emit = key => {
-      shortcutStore.emit(key)
-    }
-    return {
-      up: () => emit('up'),
-      down: () => emit('down'),
-      ...handlers,
-    }
-  }, [handlers])
-
-  return (
-    <HotKeys
-      keyMap={{
-        down: 'down',
-        up: 'up',
-        ...keyMap,
-      }}
-      style={hotKeyStyle}
-      handlers={innerHandlers}
-      {...props}
-    />
-  )
-})
-
-const hotKeyStyle = {
-  flex: 'inherit',
 }

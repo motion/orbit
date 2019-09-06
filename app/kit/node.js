d19256b21a52e71b5cc949d5053432ed4e806067
natew <natewienert@gmail.com>
2019-09-06 12:23:07 -0700

    save

--- app/kit/node.js
+++ app/kit/node.js
@@ -1,8 +1,8 @@
 const m = require('./_/index')
-if (!global.require.__islaxied) {
+if (!global.require.__proxiedRequire) {
   const laxy = require('laxy')
   global.require = laxy(require)
-  global.require.__islaxied = true
+  global.require.__proxiedRequire = true
 }
 for (var p in m) {
   if (!exports.hasOwnProperty(p)) exports[p] = m[p]

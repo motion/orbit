5f2282b627467941b1df645ef3211eaca6fa25ce
natew <natewienert@gmail.com>
2019-09-06 19:54:12 -0700

    work towards electron 8 prod mode

--- app/telemetry/src/showAnalyticsNotification.ts
+++ app/telemetry/src/showAnalyticsNotification.ts
@@ -1,23 +1,23 @@
-const boxen = require(`boxen`)
+// // const boxen = require(`boxen`)
 
-const defaultConfig = {
-  padding: 1,
-  borderColor: `blue`,
-  borderStyle: `double`,
-}
+// const defaultConfig = {
+//   padding: 1,
+//   borderColor: `blue`,
+//   borderStyle: `double`,
+// }
 
-const defaultMessage =
-  `Orbit has started collecting anonymous usage analytics\n` +
-  `to help improve Orbit for all users.\n` +
-  `\n` +
-  `If you'd like to opt-out, you can use \`orbit telemetry --disable\`\n` +
-  `To learn more, checkout https://orbit.dev/telemetry`
+// const defaultMessage =
+//   `Orbit has started collecting anonymous usage analytics\n` +
+//   `to help improve Orbit for all users.\n` +
+//   `\n` +
+//   `If you'd like to opt-out, you can use \`orbit telemetry --disable\`\n` +
+//   `To learn more, checkout https://orbit.dev/telemetry`
 
-/**
- * Analytics notice for the end-user
- * @param {Object} config - The configuration that boxen accepts. https://github.com/sindresorhus/boxen#api
- * @param {string} message - Message shown to the end-user
- */
-export const showAnalyticsNotification = (config = defaultConfig, message = defaultMessage) => {
-  console.log(boxen(message, config))
-}
+// /**
+//  * Analytics notice for the end-user
+//  * @param {Object} config - The configuration that boxen accepts. https://github.com/sindresorhus/boxen#api
+//  * @param {string} message - Message shown to the end-user
+//  */
+// export const showAnalyticsNotification = (config = defaultConfig, message = defaultMessage) => {
+//   // console.log(boxen(message, config))
+// }

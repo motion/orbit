79112505704b0be9cd59633d2fb65f72ddc5dc08
natew <natewienert@gmail.com>
2019-09-23 15:57:51 -0700

    save

--- projects/site/src/views/PillButtonDark.tsx
+++ projects/site/src/views/PillButtonDark.tsx
@@ -5,7 +5,7 @@ import { PillButton } from './PillButton'
 
 export function PillButtonDark({ children, ...props }: ButtonProps) {
   return (
-    <PillButton letterSpacing={2} background="#000" {...props}>
+    <PillButton letterSpacing={2} background="rgba(0,0,0,0.5)" {...props}>
       <span
         className="clip-text"
         style={{

8d93346f1d785ac2ca394714611f1f255a10f68d
natew <natewienert@gmail.com>
2019-09-28 02:05:42 -0700

    save

--- packages/ui/src/progress/Loading.tsx
+++ packages/ui/src/progress/Loading.tsx
@@ -1,19 +1,18 @@
 import React, { memo } from 'react'
 
 import { Center } from '../Center'
+import { Space } from '../Space'
 import { Spinner } from '../Spinner'
 import { SimpleText } from '../text/SimpleText'
-import { Col } from '../View/Col'
 
 export type LoadingProps = { message?: string }
 
 export const Loading = memo((props: LoadingProps) => {
   return (
     <Center>
-      <Col space>
-        <Spinner />
-        {!!props.message && <SimpleText>{props.message}</SimpleText>}
-      </Col>
+      <Spinner />
+      <Space />
+      {!!props.message && <SimpleText>{props.message}</SimpleText>}
     </Center>
   )
 })

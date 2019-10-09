17d62348c66b6f98f858f0ddfd2fda48feeae1d2
natew <natewienert@gmail.com>
2019-10-09 14:03:30 -0700

    save

--- packages/gloss-webpack/src/utils/ast/parse.ts
+++ packages/gloss-webpack/src/utils/ast/parse.ts
@@ -14,6 +14,7 @@ export function parse(code: string | Buffer): any {
       'decorators-legacy',
       'typescript',
       'optionalChaining',
+      'nullishCoalescingOperator',
     ],
     sourceType: 'module',
   })

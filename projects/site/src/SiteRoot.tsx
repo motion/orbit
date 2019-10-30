b3e8b7ddf989529f4d42613b366e676141347ed9
natew <natewienert@gmail.com>
2019-10-30 10:33:10 -0700

    save

--- projects/site/src/SiteRoot.tsx
+++ projects/site/src/SiteRoot.tsx
@@ -1,45 +1,44 @@
-//!
-import { ProvideUI, Stack } from '@o/ui'
-
-//
-const React = require('react')
-const { themes } = require('./themes')
-export const SiteRoot = () => {
-  return (
-    <ProvideUI themes={themes} activeTheme="dark">
-      <Stack flex={1} overflow="hidden" className="content" sm-padding={0} padding={[0, 0, 0, 24]}>
-        ok
-      </Stack>
-    </ProvideUI>
-  )
-}
-// import { ErrorBoundary, ProvideUI } from '@o/ui'
-// import React, { StrictMode, Suspense } from 'react'
-// import { Router, View } from 'react-navi'
-
-// import { Layout } from './Layout'
-// import { Navigation } from './Navigation'
-// import { SiteStoreContext } from './SiteStore'
-// import { themes } from './themes'
-
+// //!
+// import { ProvideUI, Stack } from '@o/ui'
+// //
+// const React = require('react')
+// const { themes } = require('./themes')
 // export const SiteRoot = () => {
 //   return (
-//     <ProvideUI themes={themes} activeTheme="home">
-//       <ErrorBoundary name="Site Root">
-//         <SiteStoreContext.Provider>
-//           {/* this key helps HMR for lazy imports... but it breaks scroll position */}
-//           <Router
-//             // key={process.env.NODE_ENV === 'development' ? Math.random() : 0}
-//             navigation={Navigation}
-//           >
-//             <Layout>
-//               <Suspense fallback={null}>
-//                 <View hashScrollBehavior="smooth" />
-//               </Suspense>
-//             </Layout>
-//           </Router>
-//         </SiteStoreContext.Provider>
-//       </ErrorBoundary>
+//     <ProvideUI themes={themes} activeTheme="dark">
+//       <Stack flex={1} overflow="hidden" className="content" sm-padding={0} padding={[0, 0, 0, 24]}>
+//         ok
+//       </Stack>
 //     </ProvideUI>
 //   )
 // }
+import { ErrorBoundary, ProvideUI } from '@o/ui'
+import React, { StrictMode, Suspense } from 'react'
+import { Router, View } from 'react-navi'
+
+import { Layout } from './Layout'
+import { Navigation } from './Navigation'
+import { SiteStoreContext } from './SiteStore'
+import { themes } from './themes'
+
+export const SiteRoot = () => {
+  return (
+    <ProvideUI themes={themes} activeTheme="home">
+      <ErrorBoundary name="Site Root">
+        <SiteStoreContext.Provider>
+          {/* this key helps HMR for lazy imports... but it breaks scroll position */}
+          <Router
+            // key={process.env.NODE_ENV === 'development' ? Math.random() : 0}
+            navigation={Navigation}
+          >
+            <Layout>
+              <Suspense fallback={null}>
+                <View hashScrollBehavior="smooth" />
+              </Suspense>
+            </Layout>
+          </Router>
+        </SiteStoreContext.Provider>
+      </ErrorBoundary>
+    </ProvideUI>
+  )
+}

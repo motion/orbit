c1831eaa0a316113d50ccecab5b33d6767e71b3d
natew <natewienert@gmail.com>
2019-11-04 16:27:12 -0800

    save

--- projects/site/src/views/IntroText.tsx
+++ projects/site/src/views/IntroText.tsx
@@ -5,26 +5,38 @@ import markSolid from '../public/images/mark-solid.svg'
 import { Text } from './Text'
 
 export const IntroText = (props: TextProps) => {
-  const words = `${props.children}`.split(' ')
-  const allButLast = words.slice(0, words.length - 1).join(' ')
-  const last = words[words.length - 1]
   return (
     <Text size={1.45} sizeLineHeight={1.35} fontWeight={400} width="100%">
-      {allButLast}{' '}
-      <span style={{ display: 'inline-block' }}>
-        {last}
-        <Image
-          src={markSolid}
-          display="inline"
-          width={16}
-          height={16}
-          transform={{
-            y: -2,
-          }}
-          margin={[-2, 8]}
-          userSelect="none"
-        />
-      </span>
+      {wordsWithBrandMark(props.children)}
     </Text>
   )
 }
+
+export const wordsWithBrandMark = (text: any) => {
+  const words = `${text}`.split(' ')
+  const allButLast = words.slice(0, words.length - 1).join(' ')
+  const last = words[words.length - 1]
+  return (
+    <>
+      {allButLast}{' '}
+      <span style={{ display: 'inline-block' }}>
+        {last}
+        <BrandMarkInline />
+      </span>
+    </>
+  )
+}
+
+export const BrandMarkInline = () => (
+  <Image
+    src={markSolid}
+    display="inline"
+    width={16}
+    height={16}
+    transform={{
+      y: -2,
+    }}
+    margin={[-2, 8]}
+    userSelect="none"
+  />
+)
